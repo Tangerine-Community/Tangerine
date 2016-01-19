acc45be55f95c97d5ca2b730efed46e611b60fcc
Josh Anderson <fetmar@gmail.com>
2016-01-18 20:18:44 -0500

    check for node/npm separately, use exported bash variable

--- server-init.sh
+++ server-init.sh
@@ -1,11 +1,25 @@
 # robbert
 
+# apt-get update
+if ! $updated_recently; then
+  sudo apt-get update
+  export updated_recently=TRUE
+fi
+
 # node
+which_node=`which_node`
+if [ ! -z "$which_node" ]; then
+  echo "node already installed"
+else
+  sudo apt-get install nodejs nodejs-legacy -y
+fi
+
+# npm
 which_npm=`which npm`
 if [ ! -z "$which_npm" ]; then
   echo "npm already installed"
-else
-  sudo apt-get install nodejs nodejs-legacy npm -y
+elif [ ! -z "$which_npm" ]; then
+  sudo apt-get install npm -y
 fi
 
 npm install
