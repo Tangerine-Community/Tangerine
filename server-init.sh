40badf105a83cd1aa0e56084cabdb892a2816282
R.J. Steinert <rj@rjsteinert.com>
2016-02-12 12:49:51 -0500

    Add Dockerfile, simplify server-init.sh, and add start.sh that always runs a couchapp push

--- server-init.sh
+++ server-init.sh
@@ -1,44 +1,20 @@
-#!/usr/bin/env bash -v
+#!/bin/bash
 # robbert
 
 set -v # set verbose
 
-# apt-get update
-if ! $updated_recently; then
-  sudo apt-get update
-  export updated_recently=TRUE
-fi
+sudo apt-get update
+export updated_recently=TRUE
+sudo apt-get install curl -y
 
-# node
-if [ ! -z "`which node`" ]; then
-  echo "node already installed"
-else
-  curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-  sudo apt-get install -y nodejs
-fi
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 npm install
 
-# curl
-if [ ! -z "`which curl`" ]; then
-  echo "curl already installed"
-else
-  sudo apt-get install curl -y
-fi
 
-# couchapp
-if [ ! -z "`which couchapp`" ]; then
-  echo "couchapp already installed"
-else
-  sudo apt-get install python-dev -y
-  curl -O https://bootstrap.pypa.io/get-pip.py
-  sudo python get-pip.py
-  sudo pip install couchapp
-fi
+sudo apt-get install python-dev -y
+curl -O https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo pip install couchapp
 
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $dir
-npm install
-
-cd $dir/couchapp
-couchapp push
---
+++ start.sh
@@ -0,0 +1,4 @@
+cd couchapp
+couchapp push
+cd ..
+npm start
