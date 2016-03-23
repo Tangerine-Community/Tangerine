bb8c27b00cd2f2760025089ee1817ac1a7cefe64
Chris E. Kelley <chris@vetula.com>
2016-03-23 22:26:54 +0100

    using env var instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ couchdb -k
 couchdb -b
 service nginx start
 echo "We will relax while the couch gets ready."
-while true; do nc -vz 127.0.0.1 5984 > /dev/null && break; done
+while true; do nc -vz $T_COUCH_HOST 5984 > /dev/null && break; done
 echo "TS_URL = $TS_URL"
 echo "Sending http://$T_ADMIN:$T_PASS@$T_COUCH_HOST:$T_COUCH_PORT/_users/org.couchdb.user:$T_USER1"
 curl -HContent-Type:application/json -vXPUT "http://$T_ADMIN:$T_PASS@$T_COUCH_HOST:$T_COUCH_PORT/_users/org.couchdb.user:$T_USER1" --data-binary '{"_id": "'"org.couchdb.user:$T_USER1"'","name": "'"$T_USER1"'","roles": [],"type": "user","password": "'"$T_USER1_PASSWORD"'"}'
