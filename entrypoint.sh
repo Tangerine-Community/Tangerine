c4d0f5e467dddc93318ed28f307d4d3bd89b47cb
R.J. Steinert <rj@rjsteinert.com>
2016-03-28 14:51:30 +0000

    Add a couchapp push of the _users design doc from robbert on entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,8 @@ cd /root/Tangerine-server/editor/app
 sed "s/\INSERT_HOSTNAME/"$TS_URL"/g" _docs/configuration.template > _docs/configuration.json
 sed "s/\INSERT_HOSTNAME/"$TS_URL"/g" _docs/settings.template > _docs/settings.json
 couchapp push
+cd /root/Tangerine-server/robbert/couchapp
+couchapp push
 #updated=$(curl -s http://$T_ADMIN:$T_PASS@$T_COUCH_HOST:$T_COUCH_PORT/tangerine/settings | sed -En "s/local.tangerinecentral.org/$TS_URL/p")
 #curl -HContent-Type:application/json -vXPUT "http://$T_ADMIN:$T_PASS@$T_COUCH_HOST:$T_COUCH_PORT/tangerine/settings" --data-binary $updated
 cd /root/Tangerine-server
