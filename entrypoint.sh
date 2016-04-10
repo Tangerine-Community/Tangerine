52c975a3907f8d79c78a5652d500034af885c6d7
R.J. Steinert <rj@rjsteinert.com>
2016-04-10 11:16:29 -0400

    #25 Remove bash break lines because they are not working on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #sudo mkdir /var/run/couchdb
+set -v
 sudo chown -R couchdb /var/run/couchdb
 echo "Setting up new admin for this TSI - T_ADMIN = $T_ADMIN T_NEW_ADMIN = $T_NEW_ADMIN TS_URL = $TS_URL"
 sudo -E sh -c 'echo "$T_NEW_ADMIN = $T_NEW_ADMIN_PASS" >> /etc/couchdb/local.ini'
@@ -12,9 +13,7 @@ echo "TS_URL = $TS_URL"
 echo "Sending http://$T_ADMIN:$T_PASS@$T_COUCH_HOST:$T_COUCH_PORT/_users/org.couchdb.user:$T_USER1"
 curl -HContent-Type:application/json -vXPUT "http://$T_ADMIN:$T_PASS@$T_COUCH_HOST:$T_COUCH_PORT/_users/org.couchdb.user:$T_USER1" --data-binary '{"_id": "'"org.couchdb.user:$T_USER1"'","name": "'"$T_USER1"'","roles": [],"type": "user","password": "'"$T_USER1_PASSWORD"'"}'
 cd /root/Tangerine-server/editor/app
-sed "s#INSERT_HOSTNAME#"$TS_URL"#g" _docs/configuration.template \ 
-  | sed "s#INSERT_TREE_URL#"$T_TREE_URL"#g" \
-  > _docs/configuration.json
+sed "s#INSERT_HOSTNAME#"$TS_URL"#g" _docs/configuration.template | sed "s#INSERT_TREE_URL#"$T_TREE_URL"#g" > _docs/configuration.json
 sed "s#INSERT_HOSTNAME#"$TS_URL"#g" _docs/settings.template > _docs/settings.json 
 couchapp push
 cd /root/Tangerine-server/robbert/couchapp
