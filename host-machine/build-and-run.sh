e8cc47ed0ad9b8dcdc1f4cc3dc0e16fc8bfe132b
R.J. Steinert <rj@rjsteinert.com>
2016-05-05 17:11:17 +0000

    Make build-and-run faster by using docker kill instead of docker stop which our entrypoints dont respond to

--- host-machine/build-and-run.sh
+++ host-machine/build-and-run.sh
@@ -1,4 +1,6 @@
 set -v
-docker build -t tangerine/tangerine-server:test ../
-./create-tangerine-server-container.sh test
+docker build -t tangerine/tangerine-server:local ../
+docker kill tangerine-server-container
+docker rm tangerine-server-container
+./create-tangerine-server-container.sh local 
 docker logs -f tangerine-server-container
