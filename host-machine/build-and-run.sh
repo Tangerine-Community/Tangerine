de8ac261de8ce2271bf502b7397cc634b42fa341
R.J. Steinert <rj@rjsteinert.com>
2016-05-23 13:31:46 +0000

    Add the run command directl in build-and-run for testing purposes

--- host-machine/build-and-run.sh
+++ host-machine/build-and-run.sh
@@ -2,5 +2,20 @@ set -v
 docker build -t tangerine/tangerine-server:local ../
 docker kill tangerine-server-container
 docker rm tangerine-server-container
-./run-container.sh local 
+source ./config.sh
+echo $T_ADMIN
+docker run -d \
+  --name tangerine-server-container \
+  --env "T_PROTOCOL=$T_PROTOCOL" \
+  --env "T_ADMIN=$T_ADMIN" \
+  --env "T_PASS=$T_PASS" \
+  --env "T_USER1=$T_USER1" \
+  --env "T_USER1_PASSWORD=$T_USER1_PASSWORD" \
+  --env "T_TREE_HOSTNAME=$T_TREE_HOSTNAME" \
+  --env "T_TREE_URL=$T_TREE_URL" \
+  --env "T_HOST_NAME=$T_HOST_NAME" \
+  -p 80:80 \
+  -p 5984:5984 \
+  --volume $T_VOLUMES/tangerine-server/couchdb/:/var/lib/couchdb \
+  tangerine/tangerine-server:local
 docker logs -f tangerine-server-container
