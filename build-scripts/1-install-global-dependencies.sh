43da707882ed1fbd84ce9425b4d87993076528f0
R.J. Steinert <rj@rjsteinert.com>
2016-06-28 16:38:13 -0400

    Incorporate tree and client into Dockerfile, build scripts, and ecosystem.json so tree starts on entrypoint

--- build-scripts/1-install-global-dependencies.sh
+++ build-scripts/1-install-global-dependencies.sh
@@ -66,6 +66,29 @@ cp /tangerine-server/tangerine-env-vars.sh /etc/profile.d/
 # get top to work
 echo -e "\nexport TERM=xterm" >> ~/.bashrc
 
+# Install jd7
+apt-get -y install default-jdk
+
+# Installs Android SDK
+cd /opt && \
+    wget -q $ANDROID_SDK_URL && \
+    tar -xzf $ANDROID_SDK_FILENAME && \
+    rm $ANDROID_SDK_FILENAME && \
+    echo y | android update sdk --no-ui -a --filter tools,platform-tools,$ANDROID_API_LEVELS,build-tools-$ANDROID_BUILD_TOOLS_VERSION,extra-android-support,extra-android-m2repository
+
+# Installs Cordova
+# Forces a platform add in order to preload libraries
+npm update && \
+    npm install -g npm && \
+    npm install -g cordova && \
+    cd /tmp && \
+    cordova create fakeapp && \
+    cd /tmp/fakeapp && \
+    cordova platform add android@5.X.X && \
+    cordova plugin add cordova-plugin-crosswalk-webview --variable XWALK_VERSION="19+" && \
+    cordova build android && \
+    cd
+
 # Install Couchdb
 apt-get -y install software-properties-common
 apt-add-repository -y ppa:couchdb/stable
