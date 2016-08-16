6214ef20f091b76398266347f31cad9050bfb2d5
R.J. Steinert <rj@rjsteinert.com>
2016-08-15 21:29:25 -0400

    Add a develop.sh script, set some sensible defaults in config.defaults.sh, and git ignore the data directory

--- config.defaults.sh
+++ config.defaults.sh
@@ -1,10 +1,6 @@
-
 #
 # Required to change
 #
-
-# T_HOST_NAME is the URL without protocol (like http://) you will be accessing your Tangerine server at.
-T_HOST_NAME='xxx.x.x.x'
 
 # T_USER1 is the username of the first user you will log in as. It is also the super user that has all permissions. 
 T_USER1="user1"
@@ -18,12 +14,14 @@ T_PASS="password"
 # Optional to change
 #
 
-# TANGERINE_SERVER_VERSION is the version that the start.sh and uprgrade.sh script will use when installing Tangerine-server.
+# TANGERINE_SERVER_VERSION is the version that the start.sh and uprgrade.sh script will use when installing Tangerine.
 TANGERINE_VERSION="latest"
-# If you are not on an Ubuntu system, change this to be a path of somewhere else to store data.
-T_VOLUMES="/home/ubuntu/data/"
+
+# T_HOST_NAME is the URL without protocol (like http://) you will be accessing your Tangerine server at.
+T_HOST_NAME='127.0.0.1'
+
+# Directory to store data 
+T_VOLUMES=$(pwd)/data
+
 # If you have set up SSL on your server, you must change this to "https".
 T_PROTOCOL="http"
-# If you would like to run your own Tree server, you can change that here.
-T_TREE_HOSTNAME="/"
-T_TREE_URL="/tree"
---
+++ develop.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+source ./config.defaults.sh
+if [ -f "./config.sh" ]
+then
+  source ./config.sh
+fi
+
+docker pull tangerine/tangerine:$TANGERINE_VERSION
+docker run \
+  -p 80:80 \
+  --env "T_ADMIN=$T_ADMIN" \
+  --env "T_PASS=$T_PASS" \
+  --env "T_USER1=$T_USER1" \
+  --env "T_USER1_PASSWORD=$T_USER1_PASSWORD" \
+  --env "T_HOST_NAME=$T_HOST_NAME" \
+  --env "T_PROTOCOL=$T_PROTOCOL" \
+  --volume $T_VOLUMES/couchdb/:/var/lib/couchdb \
+  --volume $T_VOLUMES/apks/:/tangerine-server/tree/apks \
+  --volume $(pwd)/editor/src/js:/tangerine-server/editor/src/js \
+  --volume $(pwd)/client/src/js:/tangerine-server/client/src/js \
+  tangerine/tangerine:$TANGERINE_VERSION
