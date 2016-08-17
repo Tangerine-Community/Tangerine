5a398b2af39ef30d35274e34529e36637625d84b
R.J. Steinert <rj@rjsteinert.com>
2016-08-17 18:00:52 +0000

    Remove upgrade.sh and install.sh because they don't map well conceptually

--- start.sh
+++ start.sh
@@ -1,3 +1,27 @@
 #!/usr/bin/env bash
 
-docker start tangerine-container
+set -v
+source ./config.defaults.sh
+if [ -f "./config.sh" ]
+then
+  source ./config.sh
+fi
+
+
+docker pull tangerine/tangerine:$TANGERINE_VERSION
+docker stop tangerine-container 
+docker rm tangerine-container 
+docker run -d \
+  --name tangerine-container \
+  --env "T_PROTOCOL=$T_PROTOCOL" \
+  --env "T_ADMIN=$T_ADMIN" \
+  --env "T_PASS=$T_PASS" \
+  --env "T_USER1=$T_USER1" \
+  --env "T_USER1_PASSWORD=$T_USER1_PASSWORD" \
+  --env "T_TREE_HOSTNAME=$T_TREE_HOSTNAME" \
+  --env "T_TREE_URL=$T_TREE_URL" \
+  --env "T_HOST_NAME=$T_HOST_NAME" \
+  -p 80:80 \
+  --volume $T_VOLUMES/couchdb/:/var/lib/couchdb \
+  --volume $T_VOLUMES/apks/:/tangerine-server/tree/apks \
+  tangerine/tangerine:$TANGERINE_VERSION
