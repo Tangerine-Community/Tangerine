19ca46cbfba2af6bc17e419a763541bcff081abf
R.J. Steinert <rj@rjsteinert.com>
2016-09-29 14:55:02 +0000

    Remove heavy logging from Tree make route

--- tree/routes/makeApk.js
+++ tree/routes/makeApk.js
@@ -65,7 +65,6 @@ const makeApk = function(req, res) {
     const groupNameRaw = req.params.group;
     const hostname = req.params[0];
     const docs = req.body.docs
-    console.log("docs: " + JSON.stringify(docs));
     const emptyGroup = !groupNameRaw || groupNameRaw == ''
     if (emptyGroup) {
         return res
