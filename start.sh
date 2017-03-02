8238caa8a0f34a5e44d9adc8c516c70bd72babb0
R.J. Steinert <rj@rjsteinert.com>
2017-03-02 15:44:41 -0500

    Automate SSL proxying with docker-nginx-proxy-letsencrypt and nginx-proxy containers starting on start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,24 @@ then
   source ./config.sh
 fi
 
+docker stop nginx-proxy
+docker rm nginx-proxy
+docker run -d -p 80:80 -p 443:443 \
+  --name nginx-proxy \
+  -v $(pwd)/data/certs:/etc/nginx/certs:ro \
+  -v /etc/nginx/vhost.d \
+  -v /usr/share/nginx/html \
+  -v /var/run/docker.sock:/tmp/docker.sock:ro \
+  jwilder/nginx-proxy
+
+docker stop letsencrypt-nginx-proxy-companion
+docker rm letsencrypt-nginx-proxy-companion
+docker run -d \
+  --name letsencrypt-nginx-proxy-companion \ 
+  -v $(pwd)/data/certs:/etc/nginx/certs:rw \
+  --volumes-from nginx-proxy \
+  -v /var/run/docker.sock:/var/run/docker.sock:ro \
+  jrcs/letsencrypt-nginx-proxy-companion
 
 docker pull tangerine/tangerine:$TANGERINE_VERSION
 docker stop tangerine-container 
@@ -18,7 +36,8 @@ docker run -d \
   --env "T_USER1=$T_USER1" \
   --env "T_USER1_PASSWORD=$T_USER1_PASSWORD" \
   --env "T_HOST_NAME=$T_HOST_NAME" \
-  -p 80:80 \
+  --env "VIRTUAL_HOST=$T_HOST_NAME" \
+  --expose 80 \
   --volume $(pwd)/data/couchdb/:/var/lib/couchdb \
   --volume $(pwd)/data/log/couchdb/:/var/log/couchdb \
   --volume $(pwd)/data/log/nginx/:/var/log/nginx \
