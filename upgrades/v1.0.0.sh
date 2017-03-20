d5d95a2b1af1c331a5b6dc40f271d40419e455ff
R.J. Steinert <rj@rjsteinert.com>
2017-03-20 22:06:02 +0000

    Break upgrades out into two upgrade scripts

--- upgrades/v1.0.0.sh
+++ upgrades/v1.0.0.sh
@@ -4,3 +4,4 @@ echo ""
 echo ""
 echo "Push design doc to all group databases"
 curl http://$T_ADMIN:$T_PASS@$T_COUCH_HOST:$T_COUCH_PORT/_all_dbs | jq '.' | grep 'group-' | sed -e 's/\"//g' | sed -e 's/,//' | xargs -I {} couchapp push http://$T_ADMIN:$T_PASS@$T_COUCH_HOST:$T_COUCH_PORT/{}
+
---
+++ upgrades/v2.0.0.sh
@@ -0,0 +1,6 @@
+echo "Running updates for 1.0.0"
+echo ""
+echo ""
+echo ""
+echo "How the Access Control list changed. Add manager role to necessary users and overrwrite the acl document in the tangerine database."
+# @TODO
