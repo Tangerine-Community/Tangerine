8b890ca2a52b24317408cbb3bf979dbd84de67ac
R.J. Steinert <rj@rjsteinert.com>
2017-03-28 14:08:29 +0000

    Add a tangerine deploy-globals command to update all groups settings and confiration docs with current globals

--- cli/tangerine.js
+++ cli/tangerine.js
@@ -6,4 +6,5 @@ program
   .command('pull-assessments <url>', 'install one or more packages')
   .command('pull-workflows <url>', 'install one or more packages')
   .command('push-backup <url>', 'search with optional query')
+  .command('deploy-globals <url>', 'search with optional query')
   .parse(process.argv);
