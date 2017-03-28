1f2b69f724f6dc9cea1dcff983b845f2dd84f207
R.J. Steinert <rj@rjsteinert.com>
2017-03-28 00:44:14 +0000

    #252 Upgrade 0.x schema for Location Subtest to 2.x

--- upgrades/v2.0.0-helper.js
+++ upgrades/v2.0.0-helper.js
@@ -8,38 +8,129 @@ let HttpStatus = require('http-status-codes');
 
 let fs = require('fs')
 let unirest = require('unirest');
-let crypto = require('crypto');
 
 const JSON_OPTS = {
   'Content-Type' : 'application/json',
   'Accept'       : 'application/json'
 };
 
-console.log( "How the Access Control list works has changed. Add manager role to necessary users and overrwrite the acl document in the tangerine database.")
-
-var tangerineDb = `${Conf.protocol}${Conf.auth}${Conf.serverUrl}/tangerine`
-var userDb = `${Conf.protocol}${Conf.auth}${Conf.serverUrl}/_users`
-
+const tangerineDb = `${Conf.protocol}${Conf.auth}${Conf.serverUrl}/tangerine`
+const userDb = `${Conf.protocol}${Conf.auth}${Conf.serverUrl}/_users`
+const couchUrl = `${Conf.protocol}${Conf.auth}${Conf.serverUrl}`
 
 var dbAclDoc = {}
 var fileAclDoc = {}
 var usersToUpdateRole = []
+var groupDbs = []
+var locationSubtests = [] 
 
-var upgrade = function upgrade() {
-  const self = this;
-  return (new Promise(function getAclDocInDb(resolve, reject) {
-    unirest.get( tangerineDb + '/acl' )
-      .end( function onDbCreateResponse(response) {
-        if (response.status === HttpStatus.OK ) {
-          dbAclDoc = JSON.parse(response.body)
-          return resolve();
-        } 
-        else {
-          return reject(response);
-        }
-      })
+var upgrade = function() {
+  return (new Promise(function startUpgrade(resolve, reject) {
+    console.log("Starting upgrade...")
+    resolve()
   }))
+  .then(function getGroupDbs(resolve, reject) {
+    return new Promise(function getGroupDbsPromise(resolve, reject) {
+      console.log('Getting all Group databases.')
+      unirest.get( couchUrl + '/_all_dbs' )
+        .end( function onGroupDbsUrlGet(response) {
+          var dbs = JSON.parse(response.body)
+          console.log(JSON.stringify(dbs))
+          dbs.forEach(function(db) {
+            if (db.indexOf('group-') !== -1) {
+              groupDbs.push(db)
+            }
+          })
+          console.log('groupDbs:')
+          console.log(groupDbs.length)
+          resolve()
+        })
+    })
+  })
+  .then(function getAllLocationSubtests(resolve, reject) {
+    return new Promise(function getAllLocationSubtestsPromise(resolve, reject) {
+      console.log('Getting location subtests.')
+      var i = 0
+      var getLocationSubtestsInOneDatabase = function getLocationSubtestsInOneDatabase() {
+        var db = groupDbs[i]
+        unirest.get( `${couchUrl}/${db}/_design/ojai/_view/byCollection?include_docs=true&keys=["subtest"]`  )
+          .end(function onGetAllSubtests(response) {
+            var obj = JSON.parse(response.body) 
+            var rows = obj.rows
+            if (rows.length > 0) {
+              rows.forEach(function forEachSubtest(row) {
+                if (row.doc.prototype == 'location') { 
+                  locationSubtests.push({ doc: row.doc,  
+                                          path: `/${db}/${row.doc._id}` })
+                }
+              })
+            }
+            if ((i + 1) == groupDbs.length) {
+              console.log('locationSubtests:')
+              console.log(JSON.stringify(locationSubtests.length))
+              return resolve()
+            }
+            else {
+              i++
+              return getLocationSubtestsInOneDatabase()
+            }
+          })
+      }
+      getLocationSubtestsInOneDatabase()
+    })
+  })
+  .then(function updateLocationSubtests(resolve, reject) {
+    return new Promise(function updateLocationSubtestsPromise(resolve, reject) {
+      console.log('Updating all location subtests.')
+      var i = 0
+      var doneUpdatingLocationSubtest = function() {
+        if ((i + 1) == locationSubtests.length) {
+          console.log('All done updating location subtests.')
+          return resolve()
+        }
+        else {
+          i++
+          updateLocationSubtest()
+        }
+      }
+      var updateLocationSubtest = function updateLocationSubtest() {
+        console.log('Updating one location subtest.')
+        var locationSubtest = locationSubtests[i]
+        if (locationSubtest.doc.hasOwnProperty('locationCols')) {
+          console.log('Location subtest already has locationCols. Skipping.')
+          return doneUpdatingLocationSubtest() 
+        }
+        locationSubtest.doc.locationCols = locationSubtest.doc.levels
+        console.log('Saving updated Location Subtest.')
+        unirest.put(couchUrl + locationSubtest.path)
+          .headers({ 'Content-Type' : 'application/json'})
+          .json(locationSubtest.doc)
+          .end(function onLocationSubtestUpdate(response) {
+            if (response.status !== 201) throw new Error(`Updating location subtest failed with status of ${response.status}`) 
+            console.log('Done saving updated location subtest.')
+            doneUpdatingLocationSubtest()
+          })
+      }
+      updateLocationSubtest()
+    })
+  })
+  .then(function getAclDocInDb(resolve, reject) {
+    return new Promise(function getAclDocInDbPromise(resolve, reject){
+      console.log('Updating Access Control List')
+      unirest.get( tangerineDb + '/acl' )
+        .end( function onDbCreateResponse(response) {
+          if (response.status === HttpStatus.OK ) {
+            dbAclDoc = JSON.parse(response.body)
+            return resolve();
+          } 
+          else {
+            return reject(response);
+          }
+        })
+    })
+  })
   .then(function getFileAcl() {
+    console.log( "How the Access Control list works has changed. Add manager role to necessary users and overrwrite the acl document in the tangerine database.")
     return new Promise(function getFileAclPromise(resolve, reject){
       fs.readFile( '/tangerine-server/documents-for-new-groups/acl.json', 'utf-8', function(err, data) { 
         if (err) { 
@@ -103,8 +194,4 @@ var upgrade = function upgrade() {
     })
   })
 }
-
 upgrade()
-  .catch(function(res) {
-     console.log(res) 
-  })
