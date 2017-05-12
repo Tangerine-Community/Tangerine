54a79ffb54a41110db144cea624d35fe8479d819
R.J. Steinert <rj@rjsteinert.com>
2017-05-12 13:48:06 -0400

    Make build-and-run.sh aware of config.sh along with container name variable and base the build image name on the current branch

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-docker build -t tangerine/tangerine:local .
-docker kill tangerine-container
-docker rm tangerine-container
-./start.sh local
-docker logs -f tangerine-container
+source ./config.defaults.sh
+if [ -f "./config.sh" ]; then
+  source ./config.sh
+else
+  echo "You have no config.sh. Copy config.defaults.sh to config.sh, change the passwords and try again." && exit 1;
+fi
+
+IMAGE_NAME=$(git rev-parse --abbrev-ref HEAD);
+docker build -t tangerine/tangerine:$IMAGE_NAME .
+docker kill $T_CONTAINER_NAME 
+docker rm $T_CONTAINER_NAME
+./start.sh $IMAGE_NAME 
+docker logs -f $T_CONTAINER_NAME
