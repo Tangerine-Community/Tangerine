d0d2a1a30c4f87ad71521e1eb40fe831d55afb88
R.J. Steinert <rj@rjsteinert.com>
2017-05-16 12:01:54 -0400

    Add the Angular Material Component library to client v3 and use the MdInputContainer for the binder demo

--- client-v3/src/app/app.module.ts
+++ client-v3/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MdInputModule} from '@angular/material';
+import 'hammerjs';
 
 import { NgBinderModule } from './ng-binder/ng-binder.module';
 
@@ -15,6 +18,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule, MdCheckboxModule, MdInputModule,
     NgBinderModule
   ],
   providers: [],
