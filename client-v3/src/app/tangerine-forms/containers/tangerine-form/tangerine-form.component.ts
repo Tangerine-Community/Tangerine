5a6a64b10d258d07712af4d83c9ff32473b673d3
R.J. Steinert <rj@rjsteinert.com>
2017-06-28 05:56:06 -0400

    Auto generate a UUID on new TangerineFormSession, share a model between cards in a form and demo that

--- client-v3/src/app/tangerine-forms/containers/tangerine-form/tangerine-form.component.ts
+++ client-v3/src/app/tangerine-forms/containers/tangerine-form/tangerine-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, Input, Output, EventEmitter, ViewChildren, ElementRef, QueryList } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, Output, EventEmitter, ViewChildren, ElementRef, QueryList, ContentChildren } from '@angular/core';
 import { Store, provideStore } from '@ngrx/store';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
@@ -32,7 +32,7 @@ export class TangerineFormComponent implements OnInit, AfterViewInit {
   // Or send a Tangerine Session in.
   @Input() tangerineFormModel: TangerineFormSession = new TangerineFormSession();
   // Latch onto the children Cards so we can listen for their events.
-  @ViewChildren(TangerineFormCardComponent) tangerineFormCardChildren: QueryList<TangerineFormCardComponent>;
+  @ContentChildren(TangerineFormCardComponent) tangerineFormCardChildren: QueryList<TangerineFormCardComponent>;
   // Config may come through the element.
   private internalEl: any;
 
@@ -42,59 +42,19 @@ export class TangerineFormComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    let inlineConfig = this.internalEl.nativeElement.innerHTML;
-    inlineConfig = inlineConfig.substring(inlineConfig.lastIndexOf('START-CONFIG'), inlineConfig.lastIndexOf('END-CONFIG'));
-    if (inlineConfig) {
-      inlineConfig = inlineConfig.split('\n');
-      inlineConfig.splice(0, 2);
-      inlineConfig.splice(inlineConfig.length - 2, inlineConfig.length);
-      inlineConfig = safeLoad(inlineConfig.join('\n'));
-      Object.assign(this.tangerineForm, inlineConfig);
-    }
-    console.log(this.tangerineForm);
-    // TODO: Subscribe by TangerineFormSession.id
-    // Pull down state changes.
-    this.store.select('tangerineFormSession')
-      .subscribe((tangerineFormSession: TangerineFormSession) => {
-        this.tangerineFormModel = tangerineFormSession;
-      });
 
-    this.tangerineForm.cards.forEach((card) => {
-      card.model = this.tangerineFormModel;
-    });
-
-    // Start a new Tangerine Form Session.
-    this.store.dispatch({type: 'TANGERINE_FORM_SESSION_START', payload: this.tangerineForm});
-
-    // Push state changes up When Cards change.
-    /*
-    this.form.valueChanges.subscribe(model => {
-      if (model) {
-        this.store.dispatch({
-          type: 'FORM_UPDATE',
-          payload: {
-            status: this.form.status,
-            model
-          }
-        });
-      }
-    });
-    */
   }
 
+  // TODO: Should be ngAfterContentInit?
   ngAfterViewInit() {
+    this.tangerineFormCardChildren.setDirty();
     console.log(this.tangerineFormCardChildren);
     this.tangerineFormCardChildren.forEach((tangerineFormCardComponent, index, cards) => {
+      tangerineFormCardComponent.tangerineFormCard.model = this.tangerineFormModel;
       tangerineFormCardComponent.change.subscribe((tangerineFormCard) => {
-        this.store.dispatch({type: 'TANGERINE_FORM_CARD_CHANGE', payload: tangerineFormCard.model});
+        Object.assign(this.tangerineFormModel, tangerineFormCard.model);
       });
     });
-    /*
-    this.tangerineFormCardChildren.change.subscribe((tangerineFormCard) => {
-      console.log(tangerineFormCard);
-    });
-    */
-
   }
 
 }
--- client-v3/src/app/tangerine-forms/models/tangerine-form-session.ts
+++ client-v3/src/app/tangerine-forms/models/tangerine-form-session.ts
@@ -1,4 +1,9 @@
 import { TangerineFormCard } from './tangerine-form-card';
+import { UUID } from 'angular2-uuid';
 
 export class TangerineFormSession {
+    _info: any;
+    constructor() {
+        this._info = {id: UUID.UUID()};
+    }
 }
