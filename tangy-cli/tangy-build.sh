b60cd75584bdf586e1e5aad2780a12d9d1a399e0
R.J. Steinert <rj@rjsteinert.com>
2017-06-30 03:34:56 -0400

    Fix build process for tangy build

--- tangy-cli/tangy-build.sh
+++ tangy-cli/tangy-build.sh
@@ -4,9 +4,9 @@ SOURCE_DIR=$PWD
 APP_DIR="$SOURCE_DIR/code/src/app"
 DIST_DIR="$SOURCE_DIR/code/dist"
 
-if [ ! -d "./app" ]; then
-  ng new code 
-fi
+# Create the new app.
+ng new --routing code 
+
 # Get all MD file paths.
 index=0
 for x in `find ./ * | grep -v "code\/" | grep -v "\.\/\/" | grep ".md"`; do
@@ -41,6 +41,7 @@ do
   echo "Installing module: $FS_NAME to $MODULE_PATH"
   ng generate module --routing $FS_NAME
   MODULE_NAME=`cat "$APP_DIR/$FS_NAME/$FS_NAME.module.ts" | tail -n 1 | awk '{print $3}'`
+  COMPONENT_NAME=`echo "$MODULE_NAME" | sed -e "s/Module/Component/"`
   # ES6 import module into app.module.ts.
   echo "import { $MODULE_NAME } from './$FS_NAME/$FS_NAME.module';" > tmp
   cat "$APP_DIR/app.module.ts" >> tmp
@@ -53,9 +54,25 @@ do
   # Move HTML file to Component.
   cd $SOURCE_DIR 
   mv "$HTML_PATH" "$MODULE_PATH/$FS_NAME/$FS_NAME.component.html"
-  # Edit routing to import and declare path for Component.
+  # Edit routing to ES6 import Component.
+  echo "import { $COMPONENT_NAME } from './$FS_NAME/$FS_NAME.component';" > tmp
+  cat "$MODULE_PATH/$FS_NAME-routing.module.ts" >> tmp
+  mv tmp "$MODULE_PATH/$FS_NAME-routing.module.ts"
+  # Edit routing to declare route or Component.
   echo "MD_PATH IS $MD_PATH"
-  cat "$MODULE_PATH/$FS_NAME-routing.module.ts" | sed -e "s#const routes# const routes \: Routes \ = \[ \{ path\: '$MD_PATH', component\: $MODULE_NAME \}\] \/\/#" > tmp
+  cat "$MODULE_PATH/$FS_NAME-routing.module.ts" | sed -e "s#const routes# const routes \: Routes \ = \[ \{ path\: '$MD_PATH', component\: $COMPONENT_NAME \}\] \/\/#" > tmp
   mv tmp "$MODULE_PATH/$FS_NAME-routing.module.ts"
 	index=$(($index+1))
 done
+
+# Build the Angular App.
+cd "$SOURCE_DIR/code" 
+ng build
+# Move the dist directory to home.
+cd ..
+mv ./code/dist ./public
+# Cleanup
+# rm -rf code
+# Start the server.
+cd public
+hs
