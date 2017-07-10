57a2e232f7c86a9251ad96141ab598151dc70d81
R.J. Steinert <rj@rjsteinert.com>
2017-07-10 11:47:24 -0400

    Wire up new tanginer-form component spec files

--- client-v3/src/app/tangerine-forms/components/tangerine-form-links/tangerine-form-links.component.spec.ts
+++ client-v3/src/app/tangerine-forms/components/tangerine-form-links/tangerine-form-links.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
 import { TangerineFormLinksComponent } from './tangerine-form-links.component';
+import { TangerineFormsModule } from '../../tangerine-forms.module';
 
 describe('TangerineFormLinksComponent', () => {
   let component: TangerineFormLinksComponent;
@@ -8,7 +10,7 @@ describe('TangerineFormLinksComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TangerineFormLinksComponent ]
+      imports: [ TangerineFormsModule, RouterTestingModule  ]
     })
     .compileComponents();
   }));
