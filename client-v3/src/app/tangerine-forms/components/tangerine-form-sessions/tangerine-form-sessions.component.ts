73a7f96e7ddae27dff17d3627d017ce21e5b51ed
R.J. Steinert <rj@rjsteinert.com>
2017-07-10 01:58:17 -0400

    Add a CSV generator to tangerine-form-sessions component

--- client-v3/src/app/tangerine-forms/components/tangerine-form-sessions/tangerine-form-sessions.component.ts
+++ client-v3/src/app/tangerine-forms/components/tangerine-form-sessions/tangerine-form-sessions.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TangerineFormSessionsService } from '../../services/tangerine-form-sessions.service';
 import { TangerineFormSession } from '../../models/tangerine-form-session';
+import * as Csv from 'csv-file-creator';
 
 @Component({
   selector: 'tangerine-form-sessions',
@@ -20,6 +21,27 @@ export class TangerineFormSessionsComponent implements OnInit {
 
   async ngOnInit() {
     this.sessions = await this.service.getAll(this.formId);
+  }
+
+  downloadCsv() {
+    // TODO: Flatten all sessions.
+    // Get unique keys.
+    let keys = [];
+    this.sessions.forEach((session) => {
+      keys = keys.concat(Object.keys(session.model).filter(function (item) {
+        return keys.indexOf(item) < 0;
+      }));
+    });
+    const rows = [keys];
+    this.sessions.forEach((session) => {
+      const row = [];
+      keys.forEach((key) => {
+        row.push(session.model[key]);
+      });
+      rows.push(row);
+    });
+
+    Csv(this.formId + '.csv', rows);
   }
 
 }
