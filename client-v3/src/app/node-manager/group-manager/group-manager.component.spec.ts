b91771a3d94397667f97164c697edeb0e1efb163
R.J. Steinert <rj@rjsteinert.com>
2017-07-10 11:48:37 -0400

    Uncomment failing group manager spec

--- client-v3/src/app/node-manager/group-manager/group-manager.component.spec.ts
+++ client-v3/src/app/node-manager/group-manager/group-manager.component.spec.ts
@@ -27,7 +27,7 @@ describe('GroupManagerComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-  /* TODO: Failing due to breaking changes in Tangerine Form Component's refactoring.
+  // TODO: Failing due to breaking changes in Tangerine Form Component's refactoring.
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
@@ -43,9 +43,8 @@ describe('GroupManagerComponent', () => {
     fixture.detectChanges();
     const inputElements = fixture.debugElement.queryAll(By.css('input'));
     // let model = tangerineFormSession.sections[0].pages[0].model
-    expect(inputElements[0].nativeElement.value).toEqual("Rambo");
+    expect(inputElements[0].nativeElement.value).toEqual('Rambo');
   });
-  */
 
 
 });
