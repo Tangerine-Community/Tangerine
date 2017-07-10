e2dc26d006ecf445c75b5e27161e046a774d9328
R.J. Steinert <rj@rjsteinert.com>
2017-07-10 02:08:59 -0400

    Flatten session objects so we have more than models in CSV output

--- client-v3/src/app/tangerine-forms/components/tangerine-form-sessions/tangerine-form-sessions.component.ts
+++ client-v3/src/app/tangerine-forms/components/tangerine-form-sessions/tangerine-form-sessions.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { TangerineFormSessionsService } from '../../services/tangerine-form-sessions.service';
 import { TangerineFormSession } from '../../models/tangerine-form-session';
 import * as Csv from 'csv-file-creator';
-
+declare const Buffer;
 @Component({
   selector: 'tangerine-form-sessions',
   templateUrl: './tangerine-form-sessions.component.html',
@@ -25,23 +25,52 @@ export class TangerineFormSessionsComponent implements OnInit {
 
   downloadCsv() {
     // TODO: Flatten all sessions.
+    const flatSessions = [];
+    this.sessions.forEach((session) => {
+      flatSessions.push(this.flatten(session));
+    });
     // Get unique keys.
     let keys = [];
-    this.sessions.forEach((session) => {
-      keys = keys.concat(Object.keys(session.model).filter(function (item) {
+    flatSessions.forEach((session) => {
+      keys = keys.concat(Object.keys(session).filter(function (item) {
         return keys.indexOf(item) < 0;
       }));
     });
     const rows = [keys];
-    this.sessions.forEach((session) => {
+    flatSessions.forEach((session) => {
       const row = [];
       keys.forEach((key) => {
-        row.push(session.model[key]);
+        row.push(session[key]);
       });
       rows.push(row);
     });
-
     Csv(this.formId + '.csv', rows);
+  }
+
+  // Flatten a deep object.
+  // From https://gist.github.com/penguinboy/762197#gistcomment-2130571
+  flatten(object: object, separator = '.'): object {
+    const isValidObject = (value: {}): boolean => {
+        if (!value) {
+            return false;
+        }
+
+        const isArray = Array.isArray(value);
+        const isBuffer = Buffer.isBuffer(value);
+        const isΟbject = Object.prototype.toString.call(value) === '[object Object]';
+        const hasKeys = !!Object.keys(value).length;
+
+        return !isArray && !isBuffer && isΟbject && hasKeys;
+    };
+
+    const walker = (child: {}, path: Array<string> = []): Object => {
+        return Object.assign({}, ...Object.keys(child).map(key => isValidObject(child[key])
+            ? walker(child[key], path.concat([key]))
+            : { [path.concat([key]).join(separator)]: child[key] })
+        );
+    };
+
+    return Object.assign({}, walker(object));
   }
 
 }
