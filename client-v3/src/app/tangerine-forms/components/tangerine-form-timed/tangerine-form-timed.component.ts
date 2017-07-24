de8174660a32de8ce6c75c58cb4789676dbb9898
R.J. Steinert <rj@rjsteinert.com>
2017-07-24 01:58:20 -0400

    Protect against double timer running in tangerine-form-timer and hide countdown form variable

--- client-v3/src/app/tangerine-forms/components/tangerine-form-timed/tangerine-form-timed.component.ts
+++ client-v3/src/app/tangerine-forms/components/tangerine-form-timed/tangerine-form-timed.component.ts
@@ -13,6 +13,8 @@ export class TangerineFormTimedComponent implements OnInit {
   countdown = 0;
   timer: any;
 
+  countdownMode = false;
+
   constructor(private elementRef: ElementRef) { }
 
   ngOnInit() {
@@ -20,22 +22,26 @@ export class TangerineFormTimedComponent implements OnInit {
   }
 
   clickedStart() {
-    const countdownInputEl = this.elementRef.nativeElement.querySelector('#' + this.id + '_countdown');
-    console.log('starting timer');
-    this.timer = setInterval(() => {
-      if (this.countdown > 0) {
-        this.countdown--;
-        countdownInputEl.dispatchEvent(new Event('change', {bubbles: true}));
-      } else {
-        clearInterval(this.timer);
-        countdownInputEl.dispatchEvent(new Event('change', {bubbles: true}));
-        this.timeIsUp();
-      }
-    }, 1000);
+    if (this.countdownMode === false) {
+      this.countdownMode = true;
+      const countdownInputEl = this.elementRef.nativeElement.querySelector('#' + this.id + '_countdown');
+      console.log('starting timer');
+      this.timer = setInterval(() => {
+        if (this.countdown > 0) {
+          this.countdown--;
+          countdownInputEl.dispatchEvent(new Event('change', {bubbles: true}));
+        } else {
+          clearInterval(this.timer);
+          countdownInputEl.dispatchEvent(new Event('change', {bubbles: true}));
+          this.timeIsUp();
+        }
+      }, 1000);
+    }
   }
 
   clickedStop() {
     clearInterval(this.timer);
+      this.countdownMode = false;
   }
 
   timeIsUp() {
