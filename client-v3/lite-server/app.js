165226db5695d69bd2a9f2282f0d86bfcf661a28
R.J. Steinert <rj@rjsteinert.com>
2017-07-24 23:24:38 -0400

    Standardize database api to /api/db endpoint

--- client-v3/lite-server/app.js
+++ client-v3/lite-server/app.js
@@ -10,13 +10,19 @@ var config = read.sync('./config.yml');
 const DB_URL = `${config.protocol}${config.domain}:${config.port}${config.dbServerEndpoint}`
 const DB_ADMIN_URL = `${config.protocol}${config.admin.username}:${config.admin.password}@${config.domain}:${config.port}${config.dbServerEndpoint}`
 
-app.use(config.dbServerEndpoint, require('express-pouchdb')(PouchDB.defaults({prefix: './db/'})));
-fauxton.use(require('express-pouchdb')(PouchDB.defaults({prefix: './db/'})));
 
-app.listen(config.port);
-fauxton.listen(5984);
-
+// Set up the app server.
+// Claim a route for the database at `/api/db`. We may have other APIs under `/api/*` in the future.
+app.use('/api/db', require('express-pouchdb')(PouchDB.defaults({prefix: './db/'})));
+// Use the compiled Angular app in dist at the root of the server.
 app.use(express.static(path.join(__dirname, '../dist')));
+// Claim port 80 for the app.
+app.listen(80);
+
+// Set up PouchDB Server.
+// Claim a second port so we can use Fauxton DB Admin at the root level, otherwise it chokes.
+fauxton.use(require('express-pouchdb')(PouchDB.defaults({prefix: './db/'})));
+fauxton.listen(5984);
 
 async function setup() {
   
@@ -29,14 +35,16 @@ async function setup() {
     console.log("We already have admins."); 
   }
 
-  // Set up the app database.
-  try {
-    await http.put(`${DB_ADMIN_URL}/${config.appDatabase}`);
-    console.log("App database created.");
-  }
-  catch (err) {
-    console.log("We already have an app database."); 
-  }
-
+  // Set up databases.
+  config.databases.forEach(async (database) => {
+    try {
+      await http.put(`${DB_ADMIN_URL}/${database}`);
+      console.log(`${database} database created.`);
+    }
+    catch (err) {
+      console.log(`We have a ${database} database.`); 
+    }
+  })
+  
 }
 setup();
