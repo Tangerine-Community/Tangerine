1aff5484f52f9b7b63a7815b79dc5bcd2371e5ca
R.J. Steinert <rj@rjsteinert.com>
2017-07-26 16:38:40 -0400

    git ignore dat files and persist the dat directory between publishes

--- client-v3/scripts/publish-to-dat-web.sh
+++ client-v3/scripts/publish-to-dat-web.sh
@@ -2,4 +2,9 @@
 ng build --aot --prod
 sw-precache --verbose --config=sw-precache-config.js
 cd dist
+if [ -d ../dat ]; then
+  mv ../dat ./.dat
+fi
+gtimeout 7 ../node_modules/.bin/dat share 
+cp -r .dat ../dat
 ../node_modules/.bin/dat share
