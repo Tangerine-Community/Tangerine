087c7a2fe6a7b4f082bc3a088c4d9d7ab886caf4
R.J. Steinert <rj@rjsteinert.com>
2017-07-27 15:27:09 -0400

    Add md transition buttons to timed form

--- client-v3/src/app/app.module.ts
+++ client-v3/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCheckboxModule, MdInputModule, MdToolbarModule, MdSidenavModule } from '@angular/material';
+import { MdButtonModule, MdButtonToggleModule, MdCheckboxModule, MdInputModule, MdToolbarModule, MdSidenavModule } from '@angular/material';
 import 'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -27,7 +27,7 @@ export { AppComponent }
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    MdButtonModule, MdCheckboxModule, MdInputModule, MdToolbarModule, MdSidenavModule,
+    MdButtonModule, MdCheckboxModule, MdInputModule, MdToolbarModule, MdSidenavModule, MdButtonToggleModule,
     TangerineFormsModule,
     NodeManagerModule,
     PagesModule,
