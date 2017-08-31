ea5ebd14feb681c660c7a4bd0c894bb84e94a876
Evans D <edianga@rti.org>
2017-08-31 10:44:50 +0300

    Added edit-profile component

---
+++ client-v3/src/app/core/auth/edit-profile/edit-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.css']
+})
+export class EditProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
