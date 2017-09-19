383ee1cbf41294829a84518b6afcb48163ece7cb
R.J. Steinert <rj@rjsteinert.com>
2017-09-19 20:35:02 +0000

    Refactor start.sh and config.defaults.sh to allow configurable ports and tag

--- config.defaults.sh
+++ config.defaults.sh
@@ -1,7 +1,12 @@
+# required in config.sh
 T_HOST_NAME='localhost'
 T_PROTOCOL="http"
 T_USER1="user1"
 T_USER1_PASSWORD="password"
 T_ADMIN="admin"
 T_PASS="password"
+
+# optional
+T_TAG=""
 T_CONTAINER_NAME="tangerine-container"
+T_PORT_MAPPING="-p 80:80 -p 5984:5984"
