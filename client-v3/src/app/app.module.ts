08280bd937301fddea9f080290fffc168badfa9c
Evans D <edianga@rti.org>
2017-10-04 12:24:42 +0300

    Added choose user profile and case management

--- client-v3/src/app/app.module.ts
+++ client-v3/src/app/app.module.ts
@@ -1,27 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCheckboxModule, MdInputModule, MdToolbarModule, MdSidenavModule, MdMenuModule } from '@angular/material';
 import 'hammerjs';
+
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {
+    MdButtonModule,
+    MdCheckboxModule,
+    MdInputModule,
+    MdMenuModule,
+    MdSidenavModule,
+    MdToolbarModule,
+} from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CaseManagementModule } from './case-management/case-management.module';
+import { AuthModule } from './core/auth/auth.module';
+import { ChooseUserProfileComponent } from './core/auth/choose-user-profile/choose-user-profile.component';
+import { SyncRecordsModule } from './core/sync-records/sync-records.module';
 import { WindowRef } from './core/window-ref.service';
-
 import { NodeManagerModule } from './node-manager/node-manager.module';
 import { TangerineFormsModule } from './tangerine-forms/tangerine-forms.module';
-import { AuthModule } from './core/auth/auth.module';
-
-import { SyncRecordsModule } from './core/sync-records/sync-records.module';
 
 export { AppComponent }
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ChooseUserProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +40,8 @@ export { AppComponent }
     AuthModule,
     SyncRecordsModule,
     NodeManagerModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CaseManagementModule
   ],
   providers: [WindowRef],
   bootstrap: [AppComponent]
