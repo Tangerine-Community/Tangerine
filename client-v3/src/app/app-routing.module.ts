9743e64c54c8282e415330ac246f6a3089286c3d
Evans D <edianga@rti.org>
2017-10-04 12:20:03 +0300

    Addded choose-user-profile component to app-routing module

--- client-v3/src/app/app-routing.module.ts
+++ client-v3/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ChooseUserProfileComponent } from './core/auth/choose-user-profile/choose-user-profile.component';
 
 const routes: Routes = [
-
-  { path: '**', redirectTo: '' }// redirects to '' in case no route is matched
+  { path: 'test', component: ChooseUserProfileComponent },
+  { path: '**', redirectTo: 'home' }// redirects to '' in case no route is matched
 ];
 
 @NgModule({
