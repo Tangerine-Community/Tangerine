9495fd413257b6146031142c848d42ab87c5e8e8
Evans D <edianga@rti.org>
2017-10-04 23:21:17 +0300

    added Login gurad to sync records routing module

--- client-v3/src/app/core/sync-records/sync-recods-routing.module.ts
+++ client-v3/src/app/core/sync-records/sync-recods-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { SyncRecordsComponent } from './sync-records/sync-records.component';
+import { RouterModule } from '@angular/router';
+
+import { LoginGuard } from '../auth/_guards/login-guard.service';
 import { UploadGuardService } from '../auth/_guards/upload-guard.service';
+import { SyncRecordsComponent } from './sync-records/sync-records.component';
+
 const routes = [{
   path: 'sync-records',
   component: SyncRecordsComponent,
-  canActivate: [UploadGuardService]
+  canActivate: [LoginGuard, UploadGuardService]
 }];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
