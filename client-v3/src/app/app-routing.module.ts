1dad877dcf4a66a63f1e931abf0c3d588d7d2065
Evans D <edianga@rti.org>
2017-10-04 12:21:33 +0300

    Removed choose-user-profile component from app-routing- Component not currently used

--- client-v3/src/app/app-routing.module.ts
+++ client-v3/src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ChooseUserProfileComponent } from './core/auth/choose-user-profile/choose-user-profile.component';
-
 const routes: Routes = [
-  { path: 'test', component: ChooseUserProfileComponent },
   { path: '**', redirectTo: 'home' }// redirects to '' in case no route is matched
 ];
 
