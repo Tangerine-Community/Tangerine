985e1d497aab3af877cb2ea43365ba2f968da8f5
Evans D <edianga@rti.org>
2017-10-06 11:41:20 +0300

    Added case management as default route and included it into app module

--- client/shell/src/app/app-routing.module.ts
+++ client/shell/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CaseManagementComponent } from './case-management/case-management.component';
+import { LoginGuard } from './core/auth/_guards/login-guard.service';
+
 const routes: Routes = [
-  { path: '**', redirectTo: 'tangy-forms' }// redirects to '' in case no route is matched
+  { path: '**', redirectTo: 'home' }, // redirects to '' in case no route is matched
+  { path: 'home', component: CaseManagementComponent, canActivate: [LoginGuard] }
 ];
 
 @NgModule({
