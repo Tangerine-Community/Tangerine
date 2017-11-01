3dd3c5c09bd69ca3869f97db599d0e61f22193e1
Evans D <edianga@rti.org>
2017-11-01 09:44:12 +0300

    Added form playing from form list

--- client/shell/src/app/tangy-forms/tangy-forms-player/tangy-forms-player.component.ts
+++ client/shell/src/app/tangy-forms/tangy-forms-player/tangy-forms-player.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { CaseManagementService } from '../../case-management/_services/case-management.service';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tangy-forms-player',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tangy-forms-player.component.css']
 })
 export class TangyFormsPlayerComponent implements OnInit {
+  formUrl;
+  formIndex: number;
+  /**
+   * @Todo 1 create form input
+   * @Todo Create responseID input
+   */
+  constructor(private caseManagementService: CaseManagementService, private route: ActivatedRoute) {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.formIndex = +params['formIndex'] || 0;
+      this.getForm(this.formIndex);
+    });
+  }
+
+  async getForm(index = 0) {
+    try {
+      const form = await this.caseManagementService.getFormList();
+      if (!(index >= form.length)) {
+        this.formUrl = '/tangy-forms/index.html?form=/content/' + form[index]['src'];
+      } else {
+        console.error('Item not Found');
+      }
+    } catch (error) {
+      console.error('Could not load list of Forms');
+    }
   }
 
 }
