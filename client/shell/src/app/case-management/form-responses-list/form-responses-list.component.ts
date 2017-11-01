4c55da22bc113e38ebb3d0c39737fcd49525f4e6
Evans D <edianga@rti.org>
2017-11-01 19:50:51 +0300

    removed unused code

--- client/shell/src/app/case-management/form-responses-list/form-responses-list.component.ts
+++ client/shell/src/app/case-management/form-responses-list/form-responses-list.component.ts
@@ -7,30 +7,9 @@ import 'rxjs/add/observable/of';
   templateUrl: './form-responses-list.component.html',
   styleUrls: ['./form-responses-list.component.css']
 })
-
-
 export class FormResponsesListComponent implements OnInit {
-  dataSource;
-  displayedColumns = ['date', 'class', 'subject', 'stream'];
   constructor() { }
 
   ngOnInit() {
-    this.dataSource = new FormDataSource();
   }
-
-
-}
-
-export interface FormResponse {
-  date?: Date;
-  class: string;
-  subject: string;
-  stream?: string;
-}
-
-const data: FormResponse[] = [{
-  class: '1',
-  subject: 'English'
-}];
-
 }
