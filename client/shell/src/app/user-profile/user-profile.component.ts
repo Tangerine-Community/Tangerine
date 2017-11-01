8b9b696dfe73e4ead27545b850da736d035eec25
Evans D <edianga@rti.org>
2017-11-01 16:59:51 +0300

    Added fix to Iframe scrolling to scroll with parent window

--- client/shell/src/app/user-profile/user-profile.component.ts
+++ client/shell/src/app/user-profile/user-profile.component.ts
@@ -1,20 +1,39 @@
+import { UserService } from '../core/auth/_services/user.service';
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,AfterViewInit, Renderer,ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent implements OnInit, AfterViewInit {
 
   formUrl;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private userService: UserService, private renderer: Renderer) { }
 
   ngOnInit() {
+    this.renderer.listenGlobal('window', 'scroll', (evt) => {
+      frames['ifr'].document.documentElement.scrollTop = window.pageYOffset;
+      frames['ifr'].document.body.scrollTop = window.pageYOffset;
+      window.dispatchEvent(new Event('resize'));
+    });
+    this.renderer.listenGlobal('window', 'resize', (evt) => {
+      document.body.style.height = frames['ifr'].document.body.offsetHeight + parseInt(document.getElementById('tangy_iframe_container').style.top) + parseInt(document.getElementById('tangy_iframe_container').style.bottom) + 'px'
+    });
     this.getForm();
   }
-  getForm() {
+  async getForm() {
+    const userDB = await this.userService.getUserDatabase();
+    const responseId = await this.userService.getUserProfileId();
+    // console.log(userDB);
+    this.formUrl =
+      `/tangy-forms/index.html?form=/content/user-profile/form.html&database=${userDB}&response-id=${responseId}`;
+      
+  }
 
+  ngAfterViewInit(){
+    document.body.style.height = frames['ifr'].document.body.offsetHeight + parseInt(document.getElementById('tangy_iframe_container').style.top) + parseInt(document.getElementById('tangy_iframe_container').style.bottom) + 'px';
+    
   }
 }
