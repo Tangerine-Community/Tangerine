21a4131a6a4e50c6c704a1e1c1f12f27a737281c
Evans D <edianga@rti.org>
2017-11-01 09:34:38 +0300

    Added case management component

--- client/shell/src/app/case-management/case-management.component.ts
+++ client/shell/src/app/case-management/case-management.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/Rx';
+import { CaseManagementService } from './_services/case-management.service';
 
 @Component({
   selector: 'app-case-management',
@@ -6,12 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./case-management.component.css']
 })
 export class CaseManagementComponent implements OnInit {
-  constructor() {
-
+  notYetVisitedLocations;
+  alreadyVistedLocations;
+  result;
+  isVisited = true;
+  searchTextValue$;
+  constructor(private caseManagementService: CaseManagementService) {
+    this.searchTextValue$ = new Subject();
   }
 
-  ngOnInit() {
-
+  async ngOnInit() {
+    this.searchTextValue$.debounceTime(500).distinctUntilChanged().subscribe(searchText => {
+      this.searchLocation(searchText);
+    });
+    this.getMyLocations();
+  }
+  async getMyLocations() {
+    try {
+      this.result = await this.caseManagementService.getMyLocationVisits();
+      this.notYetVisitedLocations = this.filterLocationsByVisitStatus(this.result, !this.isVisited);
+      this.alreadyVistedLocations = this.filterLocationsByVisitStatus(this.result, this.isVisited);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  filterLocationsByVisitStatus(data, isVisited) {
+    return data.filter((item) => {
+      return (isVisited && item.visits > 0) || (!isVisited && item.visits < 1);
+    });
+  }
+  onSearchBoxKeup(searchText) {
+    this.searchTextValue$.next(searchText);
+  }
+  searchLocation(locationName) {
+    this.notYetVisitedLocations =
+      this.filterRecordsBySearchTerm(this.filterLocationsByVisitStatus(this.result, !this.isVisited), locationName);
+    this.alreadyVistedLocations =
+      this.filterRecordsBySearchTerm(this.filterLocationsByVisitStatus(this.result, this.isVisited), locationName);
   }
 
+  filterRecordsBySearchTerm(records, filterText) {
+    if (Array.isArray(records)) {
+      return records.filter(data => {
+        const location = data.location.toLowerCase();
+        return location.includes(filterText.toLowerCase());
+      });
+    } else {
+      console.error('Could not load records');
+    }
+  }
 }
