be7307b34127da361c4bbca6aea579a5c2a5c5d8
R.J. Steinert <rj@rjsteinert.com>
2017-11-06 09:28:39 -0500

    Add in support for listening to profile iframes ALL_ITEMS_CLOSED event and then navigating to home route

--- client/shell/src/app/user-profile/user-profile.component.ts
+++ client/shell/src/app/user-profile/user-profile.component.ts
@@ -1,20 +1,34 @@
 import { UserService } from '../core/auth/_services/user.service';
-import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, AfterContentInit, ElementRef } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent implements OnInit, AfterContentInit {
 
+  @ViewChild('iframe') iframe: ElementRef;
   formUrl;
-  constructor(private route: ActivatedRoute, private userService: UserService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService
+  ) { }
+
 
   ngOnInit() {
     this.getForm();
   }
+
+  ngAfterContentInit() {
+    this.iframe.nativeElement.addEventListener('ALL_ITEMS_CLOSED', () => {
+      // navigate to homescreen
+      this.router.navigate(['/home']);
+    });
+  }
+
   async getForm() {
     const userDB = await this.userService.getUserDatabase();
     const responseId = await this.userService.getUserProfileId();
