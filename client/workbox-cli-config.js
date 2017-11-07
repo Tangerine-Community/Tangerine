1bb561faa4f17f620e14b41ef3f8ab1fb9bc3783
R.J. Steinert <rj@rjsteinert.com>
2017-11-07 16:39:55 -0500

    skipWaiting in service worker to deactivate previously enabled service worker, allows new files paths to be accessed

--- client/workbox-cli-config.js
+++ client/workbox-cli-config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  "maximumFileSizeToCacheInBytes": 15000000,
+  "maximumFileSizeToCacheInBytes": 9915000000,
   "globDirectory": "build/",
   "globPatterns": [
     "**/*.{md,html,json,png,txt,xml,ico,svg,jpg,gif,js,css}"
   ],
   "swDest": "build/sw.js",
+  "skipWaiting": true,
   "globIgnores": [
     "../workbox-cli-config.js",
     "release-uuid.txt"
