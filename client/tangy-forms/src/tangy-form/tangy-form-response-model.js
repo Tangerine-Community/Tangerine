b724c1127b74d3a258300d80e970355616cf0c9f
R.J. Steinert <rj@rjsteinert.com>
2017-11-16 00:28:31 -0500

    Add ability to mark form as complete and fix progress calculation

--- client/tangy-forms/src/tangy-form/tangy-form-response-model.js
+++ client/tangy-forms/src/tangy-form/tangy-form-response-model.js
@@ -2,15 +2,20 @@ class TangyFormResponseModel {
   constructor(props) {
     this._id = ''
     this.collection = 'TangyFormResponse'
-    this.formId = ''
+    // Placeholders for where element.getProps() info will go.
+    this.form = {}
+    this.items = []
+    this.inputs = []
+    // States.
+    complete: false,
+    // Focus indexes.
+    // @TODO: We can probably get rid of these indexes.
     this.focusIndex = 0
     this.nextFocusIndex = 1 
     this.previousFocusIndex =  -1
+    // Info.
     this.startDatetime = (new Date()).toLocaleString(),
     this.startUnixtime = Date.now(),
-    this.log = []
-    this.items = []
-    this.inputs = []
     this.uploadDatetime = ''
     Object.assign(this, props)
   }
