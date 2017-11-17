7e86aa3071374988c6edd2a6a7fa526bcfe92aa5
Evans D <edianga@rti.org>
2017-11-17 14:48:19 +0300

    Added Create Profile Guard to top level routes

--- client/shell/src/app/app-routing.module.ts
+++ client/shell/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateProfileGuardService } from 'app/user-profile/create-profile-guard.service';
 
 import { CaseManagementComponent } from './case-management/case-management.component';
 import { AppSettings } from './config/app-settings';
@@ -7,7 +8,7 @@ import { LoginGuard } from './core/auth/_guards/login-guard.service';
 
 const routes: Routes = [
   { path: '**', redirectTo: AppSettings.HOME_URL }, // redirects to '' in case no route is matched
-  { path: 'home', component: CaseManagementComponent, canActivate: [LoginGuard] }
+  { path: 'home', component: CaseManagementComponent, canActivate: [LoginGuard, CreateProfileGuardService] }
 ];
 
 @NgModule({
--- client/shell/src/app/user-profile/create-profile-guard.service.ts
+++ client/shell/src/app/user-profile/create-profile-guard.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { CanActivate } from '@angular/router/src/interfaces';
 
 @Injectable()
-export class CreateProfileGuardService {
+export class CreateProfileGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const isProfileComplete = true;
+    if (isProfileComplete) {
+      return true;
+    }
+    this.router.navigate(['/manage-user-profile'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
 
 }
