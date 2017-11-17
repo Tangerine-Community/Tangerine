ecdd88d9c09fd89718211a857cc2b44cb590a760
Evans D <edianga@rti.org>
2017-11-17 14:48:19 +0300

    Added Create Profile Guard to top level routes

--- client/shell/src/app/core/sync-records/sync-records-routing.module.ts
+++ client/shell/src/app/core/sync-records/sync-records-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateProfileGuardService } from 'app/user-profile/create-profile-guard.service';
 
 import { LoginGuard } from '../auth/_guards/login-guard.service';
 import { SyncRecordsComponent } from './sync-records/sync-records.component';
@@ -7,7 +8,7 @@ import { SyncRecordsComponent } from './sync-records/sync-records.component';
 const routes: Routes = [{
   path: 'sync-records',
   component: SyncRecordsComponent,
-  canActivate: [LoginGuard]
+  canActivate: [LoginGuard, CreateProfileGuardService]
 }];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- client/shell/src/app/user-profile/user-profile.module.ts
+++ client/shell/src/app/user-profile/user-profile.module.ts
@@ -1,12 +1,14 @@
-import { SharedModule } from '../shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatListModule } from '@angular/material';
 
+import { SharedModule } from '../shared/shared.module';
+import { CreateProfileGuardService } from './create-profile-guard.service';
 import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { UserProfileComponent } from './user-profile.component';
 
 @NgModule({
+  providers: [CreateProfileGuardService],
   imports: [
     CommonModule,
     UserProfileRoutingModule,
