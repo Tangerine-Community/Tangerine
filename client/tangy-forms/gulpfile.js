3f1a8ebb3040302b634e471df187256222b923ba
Chris E. Kelley <chris@vetula.com>
2017-11-21 20:39:04 +0100

    copy the whole fs when building the babel-compiled app

--- client/tangy-forms/gulpfile.js
+++ client/tangy-forms/gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var babel = require("gulp-babel");
 
-
 // gulp.task("default", function () {
 //   return watch('src/polydemo-app.js', { ignoreInitial: false })
 //     // .pipe(babel())
@@ -10,14 +9,24 @@ var babel = require("gulp-babel");
 //     .pipe(gulp.start('build'))
 // });
 
+gulp.task("init", function () {
+  gulp.src('../tangy-forms/**/*')
+    .pipe(gulp.dest('../tangy-forms-build/'));
+})
 
 gulp.task("build", function () {
-  return gulp.src(['src/tangy-form/tangy-form.js', 'src/tangy-form/tangy-form-item.js', 'src/tangy-input/tangy-input.js'])
+  return gulp.src(['../tangy-forms/src/tangy-form/tangy-form.js', '../tangy-forms/src/tangy-form/tangy-form-item.js'])
     .pipe(babel())
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('../tangy-forms-build/src/tangy-form/'));
 });
 
-gulp.task('default', ['build'], function(){
-  gulp.watch(['**/*.js','!**/foo.js'], ['build']);
+gulp.task("build2", function () {
+  return gulp.src(['../tangy-forms/src/tangy-input/tangy-input.js'])
+    .pipe(babel())
+    .pipe(gulp.dest('../tangy-forms-build/src/tangy-input/'));
+});
+
+gulp.task('default', ['init', 'build', 'build2'], function() {
+  gulp.watch(['../tangy-forms/src/tangy-form/tangy-form.js', '../tangy-forms/src/tangy-input/tangy-input.js','../tangy-forms/src/tangy-input/tangy-input.js'], ['build']);
   gulp.watch(['.babelrc'], ['build']);
 });
