9f3e4ea74a99b58ebd80f4af681bfe403bd2f219
Evans D <edianga@rti.org>
2017-11-21 18:01:41 +0300

    Added sanity check to redirect the user when the route in the app-config.json is not matched

--- client/shell/src/app/shared/_components/redirect-to-default-route/redirect-to-default-route.component.ts
+++ client/shell/src/app/shared/_components/redirect-to-default-route/redirect-to-default-route.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { AppConfigService } from '../../_services/app-config.service';
 
@@ -10,11 +10,23 @@ import { AppConfigService } from '../../_services/app-config.service';
 })
 export class RedirectToDefaultRouteComponent implements OnInit {
 
-  constructor(private router: Router, private appConfigService: AppConfigService) { }
+  constructor(private router: Router, private appConfigService: AppConfigService, private activatedRoute: ActivatedRoute) {
+  }
 
   async ngOnInit() {
-    const home_url = '/case-management' && await this.appConfigService.getDefaultURL();
-    this.router.navigate([home_url]);
+    const defaultUrl = '/case-management';
+    const home_url = await this.appConfigService.getDefaultURL();
+    this.router.navigate([home_url]).then(data => {
+      /**
+       * When the user has supplied a route that cannot be matched from the
+       * app-config.json redirect the user to the default url
+       * It checks if the  current route after the  first router.navigate
+       * call is still the register route and redirects to the default url
+       */
+      if (this.router.url === '/redirect') {
+        this.router.navigate([defaultUrl]);
+      }
+    });
   }
 
 }
