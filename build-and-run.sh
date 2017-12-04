3c68f4c0bd5550f92322a2bc7786127953d26547
R.J. Steinert <rj@rjsteinert.com>
2017-12-04 14:35:55 -0500

    Standardize on local-v3 naming for build and run

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,4 +1,2 @@
-docker kill tangerine-v3
-docker rm tangerine-v3
-docker build -t tangerine/tangerine:v3 .
-docker run -d -p 80:80 --name tangerine-v3 tangerine/tangerine:v3
+docker build -t tangerine/tangerine:local-v3 .
+./start.sh
