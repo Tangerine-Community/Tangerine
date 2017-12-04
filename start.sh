01f1720cc1fb9fdb628163fa664d6c9ff9fcecee
R.J. Steinert <rj@rjsteinert.com>
2017-12-04 13:49:25 -0500

    Update start script for running with custom content

--- start.sh
+++ start.sh
@@ -7,14 +7,9 @@ else
   echo "You have no config.sh. Copy config.defaults.sh to config.sh, change the passwords and try again." && exit 1;
 fi
 
-# Allow to specify Tangerine Version as parameter in ./star.sh, other wise use the most recent tag.
-if [ "$1" = "" ]; then
-  TAG=$(git describe --tags --abbrev=0)
-else
-  TAG=$1
-fi
-echo "Pulling $TAG"
-docker pull tangerine/tangerine:$TAG
+echo "Pulling $T_TAG"
+docker pull tangerine/tangerine:$T_TAG
+
 NO_SUCH_CONTAINER=$(docker inspect $T_CONTAINER_NAME | grep "No such object")
 echo $NO_SUCH_CONTAINER
 if [ "$NO_SUCH_CONTAINER" = "" ]; then
@@ -23,20 +18,10 @@ if [ "$NO_SUCH_CONTAINER" = "" ]; then
   echo "Removing $T_CONTAINER_NAME"
   docker rm $T_CONTAINER_NAME > /dev/null 
 fi
-echo "Running $T_CONTAINER_NAME at version $TAG"
+
+echo "Running $T_CONTAINER_NAME at version $T_TAG"
 docker run -d \
   --name $T_CONTAINER_NAME \
-  --env "NODE_ENV=production" \
-  --env "T_VERSION=$TANGERINE_VERSION" \
-  --env "T_PROTOCOL=$T_PROTOCOL" \
-  --env "T_ADMIN=$T_ADMIN" \
-  --env "T_PASS=$T_PASS" \
-  --env "T_USER1=$T_USER1" \
-  --env "T_USER1_PASSWORD=$T_USER1_PASSWORD" \
-  --env "T_HOST_NAME=$T_HOST_NAME" \
   -p 80:80 \
-  --volume $(pwd)/data/couchdb/:/var/lib/couchdb \
-  --volume $(pwd)/data/logs/pm2/:/tangerine-server/logs \
-  --volume $(pwd)/data/logs/couchdb/couchdb.log:/var/log/couchdb/couchdb.log \
-  --volume $(pwd)/data/media_assets/:/tangerine-server/client/media_assets/ \
-  tangerine/tangerine:$TAG
+  --volume $T_CONTENT_PATH:/tangerine/client/build/content \
+  tangerine/tangerine:$T_TAG
