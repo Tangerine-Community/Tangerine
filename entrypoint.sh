a3d1dc7dc322a13c0bf8acfd2e48676f54bf2b17
R.J. Steinert <rj@rjsteinert.com>
2017-12-04 13:48:50 -0500

    Add indexig of release to entrypoint, not dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,14 @@
-cd client/build
+
+# Generate release in case of new content.
+cd /tangerine/client/ \
+  && ./node_modules/.bin/workbox generate:sw \
+  && UUID=$(./node_modules/.bin/uuid) \
+  && mv build/sw.js build/$UUID.js \
+  && echo $UUID > build/release-uuid.txt \
+  && echo "Release with UUID of $UUID"
+
+# Host static assets in build directory.
+cd /tangerine/client/build
 ../node_modules/.bin/http-server -c-1 -p 80
+
+
