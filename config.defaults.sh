1d248dade184c48c261b3fb4758141f6b49840dd
R.J. Steinert <rj@rjsteinert.com>
2017-12-04 13:49:25 -0500

    Update start script for running with custom content

--- config.defaults.sh
+++ config.defaults.sh
@@ -1,7 +1,4 @@
-T_HOST_NAME='localhost'
-T_PROTOCOL="http"
-T_USER1="user1"
-T_USER1_PASSWORD="password"
-T_ADMIN="admin"
-T_PASS="password"
+T_CONTENT_PATH="client/content"
 T_CONTAINER_NAME="tangerine-container"
+T_TAG="v3-local"
+
