263c233eaf634a4f360281b68a3b097d5f138d91
R.J. Steinert <rj@rjsteinert.com>
2017-12-05 14:16:59 -0500

    Development and build and run scripts for v3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,29 @@
 
-# Generate release in case of new content.
-cd /tangerine/client/ \
-  && ./node_modules/.bin/workbox generate:sw \
-  && UUID=$(./node_modules/.bin/uuid) \
-  && mv build/sw.js build/$UUID.js \
-  && echo $UUID > build/release-uuid.txt \
-  && echo "Release with UUID of $UUID"
+if [ "$DEBUG" != "" ]; then
 
-# Host static assets in build directory.
-mkdir /tangerine/server/db
-cd /tangerine/server
-node index.js
+  echo "Starting entrypoint in DEBUG mode."
+
+  mkdir /tangerine/server/db
+
+  cd /tangerine/server/
+  node index.js &
+
+  cd /tangerine/client/shell
+  ./node_modules/.bin/ng build --base-href /tangerine/ --watch
+else
+
+  echo "Starting entrypoint in PRODUCTION mode."
+
+  # Generate release in case of new content.
+  cd /tangerine/client/ \
+    && ./node_modules/.bin/workbox generate:sw \
+    && UUID=$(./node_modules/.bin/uuid) \
+    && mv build/sw.js build/$UUID.js \
+    && echo $UUID > build/release-uuid.txt \
+    && echo "Release with UUID of $UUID"
+
+  # Host static assets in build directory.
+  mkdir /tangerine/server/db
+  cd /tangerine/server
+  node index.js
+fi
--- server/index.js
+++ server/index.js
@@ -10,16 +10,36 @@ var config = read.sync('./config.yml');
 const DB_URL = `${config.protocol}${config.domain}:${config.port}${config.dbServerEndpoint}`
 const DB_ADMIN_URL = `${config.protocol}${config.admin.username}:${config.admin.password}@${config.domain}:${config.port}${config.dbServerEndpoint}`
 
+// Database at /db/*
 app.use(config.dbServerEndpoint, require('express-pouchdb')(PouchDB.defaults({prefix: './db/'})));
-fauxton.use(require('express-pouchdb')(PouchDB.defaults({prefix: './db/'})));
 
+// Content at /content/*
+app.use('/content', express.static(path.join(__dirname, '../client/build/content')));
+
+// If we are in DEBUG mode, then glue together various dev folders into a structure that reperesents the paths of what they would be built else 
+// mount the client build folder and don't worry about it.
+if (process.env.DEBUG) {
+  // Shell at /tangerine/*
+  app.use('/tangerine', express.static(path.join(__dirname, '../client/shell/dist')));
+  // Tangy Forms at /tangy-forms/*
+  app.use('/tangy-forms', express.static(path.join(__dirname, '../client/tangy-forms')));
+  // App updater at /*
+  app.use('/', express.static(path.join(__dirname, '../client/app-updater')));
+} else {
+  app.use('/', express.static(path.join(__dirname, '../client/build')));
+}
+
+
+// Bind the app to port 80.
 app.listen(config.port);
+
+// Setup fauxton on port 5984 at root path because it will only work there due to some hardcoded expectations in the Fauxton JS.
+fauxton.use(require('express-pouchdb')(PouchDB.defaults({prefix: './db/'})));
 fauxton.listen(5984);
 
-app.use(express.static(path.join(__dirname, '../client/build')));
-
+// Setup database.
 async function setup() {
-  
+
   // Set up the admin user.
   try {
     await http.put(`${DB_URL}/_config/admins/${config.admin.username}`, `"${config.admin.password}"`, {headers:{}});
