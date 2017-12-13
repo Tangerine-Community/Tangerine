1faf1003887382ce87466a563d31a58f6b7401b9
R.J. Steinert <rj@rjsteinert.com>
2017-12-13 16:34:58 -0500

    Basic homebase server index

--- server/index.js
+++ server/index.js
@@ -6,57 +6,24 @@ var path = require('path');
 var app = express();
 var fauxton = express();
 var config = read.sync('./config.yml');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-const DB_URL = `${config.protocol}${config.domain}:${config.port}${config.dbServerEndpoint}`
-const DB_ADMIN_URL = `${config.protocol}${config.admin.username}:${config.admin.password}@${config.domain}:${config.port}${config.dbServerEndpoint}`
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+  }
+));
 
-// Database at /db/*
-app.use(config.dbServerEndpoint, require('express-pouchdb')(PouchDB.defaults({prefix: './db/'})));
-
-// Content at /content/*
-app.use('/content', express.static(path.join(__dirname, '../client/build/content')));
-
-// If we are in DEBUG mode, then glue together various dev folders into a structure that reperesents the paths of what they would be built else 
-// mount the client build folder and don't worry about it.
-if (process.env.DEBUG) {
-  // Shell at /tangerine/*
-  app.use('/tangerine', express.static(path.join(__dirname, '../client/shell/dist')));
-  // Tangy Forms at /tangy-forms/*
-  app.use('/tangy-forms', express.static(path.join(__dirname, '../client/tangy-forms')));
-  // App updater at /*
-  app.use('/', express.static(path.join(__dirname, '../client/app-updater')));
-} else {
-  app.use('/', express.static(path.join(__dirname, '../client/build')));
-}
-
-
-// Bind the app to port 80.
+app.use('/', express.static(path.join(__dirname, '../client/homebase')));
 app.listen(config.port);
-
-// Setup fauxton on port 5984 at root path because it will only work there due to some hardcoded expectations in the Fauxton JS.
-fauxton.use(require('express-pouchdb')(PouchDB.defaults({prefix: './db/'})));
-fauxton.listen(5984);
-
-// Setup database.
-async function setup() {
-
-  // Set up the admin user.
-  try {
-    await http.put(`${DB_URL}/_config/admins/${config.admin.username}`, `"${config.admin.password}"`, {headers:{}});
-    console.log("Admin created.");
-  }
-  catch (err) {
-    console.log("We already have admins."); 
-  }
-
-  // Set up the app database.
-  try {
-    await http.put(`${DB_ADMIN_URL}/tangerine`);
-    console.log("Todos database created.");
-  }
-  catch (err) {
-    console.log("We already have an app database."); 
-  }
-
-}
-setup();
