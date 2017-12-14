bd223f7cd2ed4474a5a701cbfc7cbeb9d2d86ba0
R.J. Steinert <rj@rjsteinert.com>
2017-12-14 16:32:07 +0000

    Auth0 and google Auth support

--- server/index.js
+++ server/index.js
@@ -1,14 +1,89 @@
-var http = require('axios');
-var read = require('read-yaml');
-var PouchDB = require('pouchdb');
-var express = require('express');
-var path = require('path');
-var app = express();
-var fauxton = express();
-var config = read.sync('./config.yml');
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var read = require('read-yaml')
+var PouchDB = require('pouchdb')
+var express = require('express')
+var passport = require('passport')
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+var path = require('path')
 
+//var User = require('./User')
+
+var config = read.sync('/tangerine/server/config.yml')
+
+var app = express()
+
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now())
+  console.log(Object.keys(req))
+  next()
+})
+//app.use(express.static('public'));
+app.use('/', express.static(path.join(__dirname, '../client/homebase')))
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  console.log(user)
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  console.log(user)
+  done(null, user);
+});
+
+/*
+ * Auth0 Auth
+ */
+var Auth0Strategy = require('passport-auth0'),
+    passport = require('passport');
+
+var strategy = new Auth0Strategy({
+   domain:       process.env.AUTH0_DOMAIN,
+   clientID:     process.env.AUTH0_CLIENT_ID,
+   clientSecret: process.env.AUTH0_CLIENT_SECRET,
+   callbackURL:  process.env.T_URL + '/auth0/callback'
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+app.get('/auth0/callback',
+  passport.authenticate('auth0', { failureRedirect: '/login' }),
+  function(req, res) {
+    if (!req.user) {
+      throw new Error('user null');
+    }
+    res.redirect("/");
+  }
+);
+
+app.get('/auth0/login',
+  passport.authenticate('auth0', {}), function (req, res) {
+  res.redirect("/");
+});
+
+/*
+ * Google Auth
+ */
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+const GOOGLE_AUTH_CALLBACK_URL = process.env.T_URL + "/auth/google/callback"
 // Use the GoogleStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Google
@@ -16,14 +91,35 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://www.example.com/auth/google/callback"
+    callbackURL: GOOGLE_AUTH_CALLBACK_URL 
   },
   function(accessToken, refreshToken, profile, done) {
-       User.findOrCreate({ googleId: profile.id }, function (err, user) {
-         return done(err, user);
-       });
+     /*
+     User.findOrCreate({ googleId: profile.id }, function (err, user) {
+       return done(err, user)
+     })
+     */
+     return done(null, { name: "test", id: profile.id, profile: profile })
   }
-));
+))
 
-app.use('/', express.static(path.join(__dirname, '../client/homebase')));
-app.listen(config.port);
+// GET /auth/google
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Google authentication will involve
+//   redirecting the user to google.com.  After authorization, Google
+//   will redirect the user back to this application at /auth/google/callback
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }))
+
+// GET /auth/google/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/')
+  })
+
+app.listen(config.port)
