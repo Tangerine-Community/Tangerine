7f9aeb8fb9ca032379e66230eb55605bb5c224db
R.J. Steinert <rj@rjsteinert.com>
2017-12-22 01:09:11 +0000

    Fix some paths in client-v3 apk build script

--- client-v3/build.sh
+++ client-v3/build.sh
@@ -1,10 +1,12 @@
+set -x
+
 CONTENT_PATH="$1"
 
 rm -r /tangerine-server/client-v3/build
 mkdir /tangerine-server/client-v3/build
 
 cd /tangerine-server/client-v3/shell
-./node_modules/.bin/ng build --base-href "/android_assets/www/tangerine"
+./node_modules/.bin/ng build --base-href "/android_asset/www/tangerine/"
 mv dist /tangerine-server/client-v3/build/tangerine
 
 cd /tangerine-server/client-v3/tangy-forms
@@ -13,11 +15,14 @@ mv dist /tangerine-server/client-v3/build/tangy-forms
 
 cp /tangerine-server/client-v3/android/index.html /tangerine-server/client-v3/build/
 
-cp "$CONTENT_PATH" /tangerine-server/client-v3/build/content
+# cp "$CONTENT_PATH" /tangerine-server/client-v3/build/content
+cp /tangerine-server/client-v3/content /tangerine-server/client-v3/build/content
 
-# mv /tangerine-server/client/www /tangerine-server/client/www-tmp
-# mv /tangerine-server/client-v3/build /tangerine-server/client/www
+mv /tangerine-server/client/www /tangerine-server/client/www-tmp
+cp -r /tangerine-server/client-v3/build /tangerine-server/client/www
 
-# cd /tangerine-server/client
-# ./node_modules/.bin/cordova run build
+cd /tangerine-server/client
+./node_modules/.bin/cordova build android
+rm -r /tangerine-server/client/www
+mv /tangerine-server/client/www-tmp /tangerine-server/client/www
 
