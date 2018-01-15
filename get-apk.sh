f3c41d2249f2b4716d2398ad8d93a80ee86d8590
R.J. Steinert <rj@rjsteinert.com>
2018-01-15 15:07:19 -0500

    Remove some options from script that runs apk for v3 that were causing an issue when nodejs was CLI promting for input

--- get-apk.sh
+++ get-apk.sh
@@ -1,4 +1,4 @@
-docker exec -it tangerine-container /tangerine-server/client-v3/build.sh
+docker exec tangerine-container /tangerine-server/client-v3/build.sh
 docker exec -it tangerine-container mv /tangerine-server/client/platforms/android/build/outputs/apk/android-armv7-debug.apk /tangerine-server/tree/apks/v3.apk
 echo "You will find your APK at $(pwd)/data/apks/v3.apk"
 
