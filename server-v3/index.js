decdc1e6d6e4f362247b8ad384a315c28f84ed6a
Chris E. Kelley <chris@vetula.com>
2018-01-23 14:53:15 +0100

    removed unused code for editor

--- server-v3/index.js
+++ server-v3/index.js
@@ -2,14 +2,11 @@
 
 // const http = require('axios')
 const read = require('read-yaml')
-// const PouchDB = require('pouchdb')
 const express = require('express')
 const path = require('path')
 const app = express()
-// const fauxton = express()
 const fs = require('fs-extra')
 const config = read.sync('./config.yml')
-const editor = require('./editor.js')()
 const sanitize = require('sanitize-filename');
 const cheerio = require('cheerio');
 // for json parsing in recieved requests
@@ -21,39 +18,7 @@ const DB_ADMIN_URL = `${config.protocol}${config.admin.username}:${config.admin.
 
 app.use(bodyParser.json()); // use json
 
-// Database at /db/*
-// app.use(config.dbServerEndpoint, require('express-pouchdb')(PouchDB.defaults({prefix: './db/'})))
-
-// Content at /content/*
-// app.use('/content', express.static(path.join(__dirname, config.contentRoot)))
-
-// If we are in DEBUG mode, then glue together various dev folders into a structure that reperesents the paths of what they would be built else 
-// mount the client build folder and don't worry about it.
-// if (process.env.DEBUG) {
-//   // Shell at /tangerine/*
-//   app.use('/tangerine', express.static(path.join(__dirname, '../client/shell/dist')));
-//   // Tangy Forms at /tangy-forms/*
-//   app.use('/tangy-forms', express.static(path.join(__dirname, '../client/tangy-forms/dist')));
-//   // App updater at /*
-//   app.use('/', express.static(path.join(__dirname, '../client/app-updater')));
-// } else {
-//   app.use('/', express.static(path.join(__dirname, '../client/build')));
-// }
-
-// editor
-
-// app.use('/tangy-editor', express.static(path.join(__dirname, '../client-v3/tangy-forms/editor')));
-
 console.log("launching server.")
-
-app.get('/editor/project/listAll', function (req, res, next) {
-  var dirs = editor.listProjects().then(function (result) {
-    console.log("listAll: " + JSON.stringify(result)); // "Stuff worked!"
-    res.send(result);
-  }, function (err) {
-    console.log(err); // Error: "It broke"
-  });
-});
 
 async function saveFormsJson(formParameters, project) {
   console.log("formParameters: " + JSON.stringify(formParameters))
@@ -271,7 +236,6 @@ app.post('/item/save', async function (req, res) {
     }
     // console.log('html after: ' + $.html())
     let form = $.html()
-    // await editor.saveForm(formPath, form);
     console.log('now outputting ' + formPath)
     await fs.outputFile(formPath, form)
       .then(() => {
@@ -284,7 +248,6 @@ app.post('/item/save', async function (req, res) {
       })
   }
   // Save the item
-  // let resp =  await editor.saveItem(formPath, itemFilename, itemHtmlText)
   let itemPath = formPath.substring(0, formPath.lastIndexOf("/")) + sep + itemFilename;
   // console.log("Saving item at : " + itemPath)
   await fs.outputFile(itemPath, itemHtmlText)
@@ -306,32 +269,4 @@ app.post('/item/save', async function (req, res) {
 // Bind the app to port 80.
 app.listen(config.port);
 
-// Setup fauxton on port 5984 at root path because it will only work there due to some hardcoded expectations in the Fauxton JS.
-// fauxton.use(require('express-pouchdb')(PouchDB.defaults({prefix: './db/'})));
-// fauxton.listen(5984);
-
-// Setup database.
-// async function setup() {
-//
-//   // Set up the admin user.
-//   try {
-//     await http.put(`${DB_URL}/_config/admins/${config.admin.username}`, `"${config.admin.password}"`, {headers: {}});
-//     console.log("Admin created.");
-//   }
-//   catch (err) {
-//     console.log("We already have admins.");
-//   }
-//
-//   // Set up the app database.
-//   try {
-//     await http.put(`${DB_ADMIN_URL}/tangerine`);
-//     console.log("Todos database created.");
-//   }
-//   catch (err) {
-//     console.log("We already have an app database.");
-//   }
-//
-// }
-//
-// setup();
 
