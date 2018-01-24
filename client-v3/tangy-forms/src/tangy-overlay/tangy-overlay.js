58e007814f8ac23d1b3c13e44912b5ce0105c38f
R.J. Steinert <rj@rjsteinert.com>
2018-01-24 13:57:30 -0500

    Add icons, fab, and static content abilities to tangy-overlay

--- client-v3/tangy-forms/src/tangy-overlay/tangy-overlay.js
+++ client-v3/tangy-forms/src/tangy-overlay/tangy-overlay.js
@@ -68,12 +68,12 @@ export class TangyOverlay extends PolymerElement {
     }
 
     </style>
-    <div id="media-button" >
-        <paper-button raised on-click="open">View Media Overlay</paper-button>
+
+    <div id="media-button" on-click="open" >
     </div>
     
     <div id="overlay">
-        <div id="lightbox"on-click="off">
+        <div id="lightbox" on-click="off">
         </div>
     </div>
     <br/>
@@ -99,6 +99,10 @@ export class TangyOverlay extends PolymerElement {
       overlayContent: {
         type: String,
         value: '',
+      },
+      icon: {
+        type: String,
+        value: 'icons:open-with'
       }
     };
   }
@@ -108,28 +112,34 @@ export class TangyOverlay extends PolymerElement {
     super.connectedCallback();
     // Set up the store.
     this.store = window.tangyFormStore
-    // this.url = "/content/assets/cat.png"
+    this.$['media-button'].innerHTML = `
+      <paper-fab icon="${this.icon}" raised on-click="open"></paper-fab>
+    `
     if (this.position === 'top-right') {
       this.shadowRoot.getElementById('media-button').className = 'media-button-top-right'
     } else if (this.position === 'top-left') {
       this.shadowRoot.getElementById('media-button').className = 'media-button-top-left'
     } else if (this.position === 'bottom-right') {
       this.shadowRoot.getElementById('media-button').className = 'media-button-bottom-right'
-    } else {
+    } else if (this.position === 'bottom-left') {
       this.shadowRoot.getElementById('media-button').className = 'media-button-bottom-left'
     }
   }
 
   open(ev) {
-    let getValue = this.getValue.bind(this)
-    let newOverlayContent = ''
-    eval(`
-      function go() {
-        ${this.onOpen}
-      }
-      newOverlayContent = go()
-    `)
-    this.$.lightbox.innerHTML = newOverlayContent
+    if (this.onOpen) {
+      let getValue = this.getValue.bind(this)
+      let newOverlayContent = ''
+      eval(`
+        function go() {
+          ${this.onOpen}
+        }
+        newOverlayContent = go()
+      `)
+      this.$.lightbox.innerHTML = newOverlayContent
+    } else {
+      this.$.lightbox.innerHTML = this.innerHTML 
+    }
     this.on()
   }
 
