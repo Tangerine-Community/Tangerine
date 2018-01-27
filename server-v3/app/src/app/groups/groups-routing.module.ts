0c078edd1fe1de145156045e5c4df119348d885b
R.J. Steinert <rj@rjsteinert.com>
2018-01-26 19:55:32 -0500

    Refactor groups component to understand tangerine usage api, add group component for viewing editor of a group, and add a new-group component for creating new groups

--- server-v3/app/src/app/groups/groups-routing.module.ts
+++ server-v3/app/src/app/groups/groups-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard }                from '../auth-guard.service';
 
 import { GroupsComponent }    from './groups.component';
+import { GroupComponent }    from './group/group.component';
+import { NewGroupComponent }    from './new-group/new-group.component';
 const groupsRoutes: Routes = [
   //{ path: 'projects',  component: GroupsComponent },
   { path: 'projects', component: GroupsComponent, canActivate: [AuthGuard] },
+  { path: 'new-group', component: NewGroupComponent, canActivate: [AuthGuard] },
+  { path: 'group/:id', component: GroupComponent, canActivate: [AuthGuard] },
 ];
 @NgModule({
   imports: [
