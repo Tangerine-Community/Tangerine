216ac978ac80ff3976330b1137f7d89699329906
R.J. Steinert <rj@rjsteinert.com>
2018-01-31 21:21:00 -0500

    Add db persitence and a route for generating test data

--- start.sh
+++ start.sh
@@ -44,6 +44,7 @@ RUN_OPTIONS="
   --env \"T_HOST_NAME=$T_HOST_NAME\" \
   $T_PORT_MAPPING \
   --volume $(pwd)/data/client/apks:/tangerine/client/releases/apks/ \
+  --volume $(pwd)/data/db:/tangerine/db/ \
   --volume $(pwd)/data/client/pwas:/tangerine/client/releases/pwas/ \
   --volume $(pwd)/data/client/content/groups:/tangerine/client/content/groups \
   --volume $(pwd)/server/index.js:/tangerine/server/index.js \
