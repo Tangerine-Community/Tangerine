98ca4bae9b4a1aee7c8c9240766ccc1386606fd1
Evans D <edianga@rti.org>
2018-02-05 22:08:30 +0300

    Sync  indicates that it has succeeded or not

--- client/shell/src/app/core/sync-records/sync-records/sync-records.component.ts
+++ client/shell/src/app/core/sync-records/sync-records/sync-records.component.ts
@@ -8,35 +8,38 @@ import { SyncingService } from '../_services/syncing.service';
   styleUrls: ['./sync-records.component.css']
 })
 export class SyncRecordsComponent implements OnInit {
-  isLoading = false;
+  isSyncSuccesful: boolean = undefined;
+  syncStatus: string = '';
   docsNotUploaded;
   docsUploaded;
   syncPercentageComplete;
   constructor(private syncingService: SyncingService) { }
 
   async ngOnInit() {
+    this.calculateUploadProgress();
+  }
+
+  async calculateUploadProgress() {
     const result = await this.syncingService.getDocsNotUploaded();
     this.docsNotUploaded = result ? result.length : 0;
     this.docsUploaded = await this.syncingService.getNumberOfFormsLockedAndUploaded();
     this.syncPercentageComplete =
       ((this.docsUploaded / (this.docsNotUploaded + this.docsUploaded)) * 100);
   }
-
   async pushAllRecords() {
-    this.toggleIsLoading();
+    this.isSyncSuccesful = undefined;
     try {
       const result = await this.syncingService.pushAllrecords();
       if (result) {
-        this.toggleIsLoading();
+        this.isSyncSuccesful = true;
+        this.calculateUploadProgress();
       }
     } catch (error) {
       console.error(error);
-      this.toggleIsLoading();
+      this.isSyncSuccesful = false;
+      this.calculateUploadProgress();
     }
   }
 
 
-  toggleIsLoading() {
-    this.isLoading = !this.isLoading;
-  }
 }
