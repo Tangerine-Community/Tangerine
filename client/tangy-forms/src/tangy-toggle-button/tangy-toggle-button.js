485451ecd8808560f3cc9e071b227afaec6d4459
R.J. Steinert <rj@rjsteinert.com>
2018-02-09 11:49:10 -0500

    Use CSS variables to style tangy toggle buttons

--- client/tangy-forms/src/tangy-toggle-button/tangy-toggle-button.js
+++ client/tangy-forms/src/tangy-toggle-button/tangy-toggle-button.js
@@ -26,7 +26,7 @@ class TangyToggleButton extends Element {
         display: none;
       }
       :host([pressed]) {
-        background-color: #3c5b8d;;
+        background-color: var(--primary-color);;
         color: #FFF;
         background-image: url('data:image/svg+xml;utf-8,<svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>');
         background-repeat: no-repeat;
@@ -34,7 +34,7 @@ class TangyToggleButton extends Element {
       }
       
       :host([highlighted]) {
-        border-color: #f26f10;
+        border-color: var(--accent-color);
       }
       :host([required]:not([disabled])) label::before  { 
         content: "*"; 
