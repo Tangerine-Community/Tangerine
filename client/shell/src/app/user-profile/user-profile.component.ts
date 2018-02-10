efe8f0d4ecfe6fbb98c989e5193ce3e473561ee4
R.J. Steinert <rj@rjsteinert.com>
2018-02-10 15:33:22 -0500

    Hide new response button on user profile using parameters

--- client/shell/src/app/user-profile/user-profile.component.ts
+++ client/shell/src/app/user-profile/user-profile.component.ts
@@ -34,9 +34,9 @@ export class UserProfileComponent implements OnInit, AfterContentInit {
     let tangyFormService = new TangyFormService({databaseName: userDbName});
     let profileDocs = await tangyFormService.getResponsesByFormId('user-profile') 
     if (profileDocs.length > 0) {
-      this.formUrl = `../tangy-forms/index.html#form_src=../content/user-profile/form.html&database_name=${userDbName}&response_id=${profileDocs[0]._id}`;
+      this.formUrl = `../tangy-forms/index.html#form_src=../content/user-profile/form.html&hide_new_response_button=true&database_name=${userDbName}&response_id=${profileDocs[0]._id}`;
     } else {
-      this.formUrl = `../tangy-forms/index.html#form_src=../content/user-profile/form.html&database_name=${userDbName}`;
+      this.formUrl = `../tangy-forms/index.html#form_src=../content/user-profile/form.html&hide_new_response_button=true&database_name=${userDbName}`;
     }
     // This protects against binding again an element that does not yet exist because the
     // the this.formUrl property was just set, the *ngIf="formUrl" on iframe will be in the
