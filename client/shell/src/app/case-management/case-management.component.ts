24fa014cb61eec32b859a1e8c8b0c6ea4b2a8816
Evans D <edianga@rti.org>
2018-02-28 16:17:37 +0300

    Added Driildwown  Navigation to the case component

--- client/shell/src/app/case-management/case-management.component.ts
+++ client/shell/src/app/case-management/case-management.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
+import { ActivatedRoute } from '@angular/router';
 import { CaseManagementService } from './_services/case-management.service';
 import 'rxjs/add/operator/map';
 @Component({
@@ -12,12 +13,32 @@ export class CaseManagementComponent implements OnInit, AfterViewInit {
   alreadyVistedLocations;
   result;
   isVisited = true;
+  currentLocationId;
+  parentPath = '';
+  locationLevels = [
+    "county",
+    "subcounty",
+    "zone",
+    "school"
+  ];
+  currentLevelIndex = -1;
 
   @ViewChild('search') search: ElementRef;
-  constructor(private caseManagementService: CaseManagementService) {
+  constructor(
+    private caseManagementService: CaseManagementService,
+    private route: ActivatedRoute
+  ) {
   }
 
   async ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.currentLocationId = params['currentLocationId'] ? params['currentLocationId'] : 0;
+      this.getMyLocations();
+      this.parentPath += this.currentLevelIndex <= -1 ? '' : `${this.locationLevels[this.currentLevelIndex]}=${this.currentLocationId}&`;
+      // this.parentPath + = this.locationLevels[this.currentLevelIndex]
+      this.currentLevelIndex++
+      this.locationLevels.length - 1 === this.currentLevelIndex ? this.parentPath = `'${this.parentPath.slice(0, -1)}'` : null;
+    });
     this.getMyLocations();
   }
 
@@ -35,7 +56,7 @@ export class CaseManagementComponent implements OnInit, AfterViewInit {
   }
   async getMyLocations() {
     try {
-      this.result = await this.caseManagementService.getMyLocationVisits();
+      this.result = await this.caseManagementService.getMyLocationVisits(this.currentLocationId);
       this.notYetVisitedLocations = this.filterLocationsByVisitStatus(this.result, !this.isVisited);
       this.alreadyVistedLocations = this.filterLocationsByVisitStatus(this.result, this.isVisited);
     } catch (error) {
