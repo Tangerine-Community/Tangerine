103be4049d7c33deb2d7a8f2ecce310dd1b98e9e
Mel Michael <mel.michael@andela.com>
2018-02-28 12:01:23 +0100

    Test out reporting code

--- server/reporting/config.js
+++ server/reporting/config.js
@@ -2,6 +2,6 @@ const Settings = require('../Settings');
 let config = {};
 
 config.base_db = `http://${Settings.T_ADMIN}:${Settings.T_PASS}@${Settings.T_COUCH_HOST}/db/tangerine`;
-config.result_db = `http://${Settings.T_ADMIN}:${Settings.T_PASS}@${Settings.T_COUCH_HOST}/db/tmp-result`;
+config.result_db = `http://${Settings.T_ADMIN}:${Settings.T_PASS}@${Settings.T_COUCH_HOST}/db/tangerine-result`;
 
 module.exports = config;
