884c46c5fce64af81c7bbcf1fef3780221e9d67c
Evans D <edianga@rti.org>
2018-03-02 12:36:50 +0300

    Added Queries and views for incomplete responses

--- client/shell/src/app/case-management/incomplete-observations/incomplete-observations.component.ts
+++ client/shell/src/app/case-management/incomplete-observations/incomplete-observations.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CaseManagementService } from '../_services/case-management.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-incomplete-observations',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./incomplete-observations.component.css']
 })
 export class IncompleteObservationsComponent implements OnInit {
-
-  constructor() { }
+  observations;
+  constructor(private caseManagementService: CaseManagementService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      const locationId = params['locationId'];
+      this.getObservations(locationId);
+    });
   }
-
+  async getObservations(locationId: string) {
+    const result = await this.caseManagementService.getIncompleteResponsesByLocationId(locationId);
+    const formList = await this.caseManagementService.getFormList();
+    this.observations = result.map((observation) => {
+      const index = formList.findIndex(c => c.id === observation.doc.form['id']);
+      return {
+        formTitle: formList[index]['title'],
+        startDatetime: observation.doc.startDatetime,
+        formIndex: index,
+        _id: observation.doc._id,
+      };
+    });
+  }
 }
