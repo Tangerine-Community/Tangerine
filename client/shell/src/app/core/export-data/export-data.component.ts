4567146e2756ca2c0c2484ec4c6539d1f0560397
Evans D <edianga@rti.org>
2018-03-27 11:30:47 +0300

    Added Component and module export-data

---
+++ client/shell/src/app/core/export-data/export-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-export-data',
+  templateUrl: './export-data.component.html',
+  styleUrls: ['./export-data.component.css']
+})
+export class ExportDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/shell/src/app/core/export-data/export-data.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExportDataComponent } from './export-data.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ExportDataComponent]
+})
+export class ExportDataModule { }
