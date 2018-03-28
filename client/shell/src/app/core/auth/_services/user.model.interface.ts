bf063023c0339bec8f0f852dcfa31d22f44091ac
Evans D <edianga@rti.org>
2018-03-28 17:43:10 +0300

    Created ability to recover user account when password is fogotten

--- client/shell/src/app/core/auth/_services/user.model.interface.ts
+++ client/shell/src/app/core/auth/_services/user.model.interface.ts
@@ -2,5 +2,6 @@ export interface User {
     username: string;
     password: string;
     confirmPassword?: string;
-    email: string;
+    securityQuestionResponse: string;
+    hashSecurityQuestion: boolean;
 }
