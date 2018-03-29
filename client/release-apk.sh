8ad38edee677bc86e208f16a28904e44b74e3c9c
Chris E. Kelley <chris@vetula.com>
2018-03-29 23:41:25 +0200

    Added support for cordova-hot-code-push in tangy groups

--- client/release-apk.sh
+++ client/release-apk.sh
@@ -2,8 +2,10 @@
 
 SECRET="$1"
 CONTENT_PATH="$2"
+QA_DIRECTORY="/tangerine/client/QA/apks/$SECRET"
+RELEASES_DIRECTORY="/tangerine/client/releases/apks/$SECRET"
 
-if [ "$SECRET" = "" ] || [ "$CONTENT_PATH" = "" ]; then
+if [ "$SECRET" = "" ] || [ "$CONTENT_PATH" = "" ] || [ "QA_DIRECTORY" = "" ] || [ "RELEASES_DIRECTORY" = "" ]; then
   echo ""
   echo "RELEASE APK"
   echo "A command for releasing an APK using a secret URL."
@@ -11,32 +13,46 @@ if [ "$SECRET" = "" ] || [ "$CONTENT_PATH" = "" ]; then
   echo "./release-apk.sh <secret> <content path>"
   echo ""
   echo "Usage:"
-  echo "  ./release-apk.sh a4uw93 ./content/group-a"
+  echo "  ./release-apk.sh a4uw93 ./content/groups/group-a"
   echo ""
   echo "Then visit https://foo.tangerinecentral.org/releases/apk/a4uw93.apk"
 fi
 
 #cp -r /tangerine/client/builds/apk /.tmp-apk
-rm -rf /.tmp-apk/www/content
-cp -r $CONTENT_PATH /.tmp-apk/www/content
-cp -r ./content/assets /.tmp-apk/www/content
+
+if [ ! -d "$QA_DIRECTORY" ]; then
+  # Control will enter here if $QA_DIRECTORY doesn't exist.
+  cp -R /.tmp-apk $QA_DIRECTORY
+fi
+
+rm -rf $QA_DIRECTORY/www/content
+cp -r $CONTENT_PATH $QA_DIRECTORY/www/content
+cp -r ./content/assets $QA_DIRECTORY/www/content
 
 echo "Refreshing the shell - this is dev use only"
 
-rm -rf /.tmp-apk/www/shell
-cp -r /tangerine/client/builds/apk/www/shell/ /.tmp-apk/www/shell/
-rm -rf /.tmp-apk/www/tangy-forms
+rm -rf $QA_DIRECTORY/www/shell
+cp -r /tangerine/client/builds/apk/www/shell/ $QA_DIRECTORY/www/shell/
+rm -rf $QA_DIRECTORY/www/tangy-forms
 #cp -r tangy-forms/dist builds/apk/www/tangy-forms
 cp /tangerine/client/tangy-forms/src/index.html /tangerine/client/builds/apk/www/tangy-forms/index.html
 cp /tangerine/client/tangy-forms/dist/module.bundle.js /tangerine/client/builds/apk/www/tangy-forms/
 cp /tangerine/client/tangy-forms/dist/module.bundle.js.map /tangerine/client/builds/apk/www/tangy-forms/
-cp -r /tangerine/client/builds/apk/www/tangy-forms/ /.tmp-apk/www/tangy-forms/
+cp -r /tangerine/client/builds/apk/www/tangy-forms/ $QA_DIRECTORY/www/tangy-forms/
 
-cd /.tmp-apk
+cd $QA_DIRECTORY
 echo "RELEASE APK: running Cordova build."
 cordova -v --no-telemetry
 cordova build --no-telemetry android
-#cp /.tmp-apk/platforms/android/app/build/outputs/apk/armv7/debug/app-armv7-debug.apk /tangerine/client/releases/apks/$SECRET.apk
-cp /.tmp-apk/platforms/android/app/build/outputs/apk/debug/app-debug.apk /tangerine/client/releases/apks/$SECRET.apk
-echo "Released apk for $SECRET"
-#rm -r /.tmp-apk
+if [ ! -d "$RELEASES_DIRECTORY" ]; then
+# Control will enter here if $RELEASES_DIRECTORY doesn't exist.
+    mkdir $RELEASES_DIRECTORY
+else
+    rm -r $RELEASES_DIRECTORY/www
+fi
+
+cp -R $QA_DIRECTORY/www $RELEASES_DIRECTORY/www
+cp $QA_DIRECTORY/platforms/android/app/build/outputs/apk/debug/app-debug.apk $RELEASES_DIRECTORY/$SECRET.apk
+
+echo "Released apk for $SECRET at $RELEASES_DIRECTORY"
+
