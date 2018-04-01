cd70aa6e88b4c17a287c2779261d40723e3ba4f1
R.J. Steinert <rj@rjsteinert.com>
2018-04-01 19:17:08 -0400

    #755 FORM_RESPONSE_COMPLETE actions adds new locked attribute to tangy-form-item elements and they wont firehook when locked

--- client/tangy-forms/src/tangy-form/tangy-form-reducer.js
+++ client/tangy-forms/src/tangy-form/tangy-form-reducer.js
@@ -51,6 +51,7 @@ const tangyFormReducer = function (state = initialState, action) {
             props.open = false
             props.hideButtons = false
           }
+          props.locked = true
           props.hideBackButton = true
           props.hideNextButton = true
           props.inputs = item.inputs.map(input => {
