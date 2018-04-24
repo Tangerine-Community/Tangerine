0a7cb5bb688fe64a785a603cc77d7b911835aaa9
R.J. Steinert <rj@rjsteinert.com>
2018-04-24 10:44:53 -0400

    Remove home page used on hub and other cruft

--- editor/src/app/app-routing.module.ts
+++ editor/src/app/app-routing.module.ts
@@ -2,10 +2,9 @@ import { NgModule }              from '@angular/core';
 import { RouterModule, Routes }  from '@angular/router';
 import {SupportComponent} from './support/support.component';
 import { AuthGuard }                from './auth-guard.service';
-//import { RegComponent }   from './reg/reg.component';
+
 const appRoutes: Routes = [
   { path: 'support', component: SupportComponent, canActivate: [AuthGuard] },
-  //{ path: '**', component: PageNotFoundComponent }
 ];
 @NgModule({
   imports: [
