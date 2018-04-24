c4ab2abd325d5cc3a72c1eae8ce7fe2c352467de
Mel Michael <mel.michael@andela.com>
2018-04-24 12:39:53 +0100

    Add v2 reporting code

---
+++ server/reporting/controllers/generate_csv.js
@@ -0,0 +1,103 @@
+/**
+ * This file generates a CSV file.
+ *
+ * Module: generateCSV
+ */
+
+/**
+ * Module dependencies
+ */
+
+const _ = require('lodash');
+const chalk = require('chalk');
+const Excel = require('exceljs');
+const PouchDB = require('pouchdb');
+
+/**
+ * Local dependency.
+ */
+
+const dbQuery = require('./../utils/dbQuery');
+const dbConfig = require('./../config');
+
+/**
+ * Generates a CSV file.
+ *
+ * Example:
+ *
+ *    POST /generate_csv/:id
+ *
+ *  where id refers to the id of the generated document in the result database.
+ *
+ *  The request object must contain the result database url.
+ *      {
+ *        "result_db_url": "http://admin:password@test.tangerine.org/database_name"
+ *      }
+ *
+ * Response:
+ *
+ *  Returns an object containing a success message.
+ *
+ * @param req - HTTP request object
+ * @param res - HTTP response object
+ */
+
+exports.generate = (req, res) => {
+  let resultDb = req.body.result_db || req.params.db_name;
+  resultDb = resultDb.includes('http') ? resultDb : dbConfig.db_url + resultDb + '-result';
+
+  const RESULT_DB = new PouchDB(resultDb);
+  const resultId = req.params.id;
+  const resultYear = req.params.year;
+  let resultMonth = req.params.month;
+  resultMonth = resultMonth ? resultMonth : false;
+  resultMonth = resultMonth ? resultMonth[0].toUpperCase() + resultMonth.substr(1, 2) : false;
+
+  let queryId = resultMonth && resultYear ? `${resultId}_${resultYear}_${resultMonth}` : resultId;
+
+  RESULT_DB.get(resultId, resultDb)
+    .then(async(colHeaders) => {
+      const result = await dbQuery.getProcessedResults(queryId, resultDb);
+      generateCSV(colHeaders, result, res);
+    })
+    .catch((err) => res.send(err));
+}
+
+/**
+ * This function creates a CSV file.
+ *
+ * @param {Object} columnData – column headers
+ * @param {Array} resultData – the result data.
+ * @param {Object} res – response object.
+ *
+ * @returns {Object} – generated response
+ */
+
+const generateCSV = function(columnData, resultData, res) {
+  const FILENAME = columnData.name.replace(/\s/g, '_');
+  let workbook = new Excel.Workbook();
+  workbook.creator = 'Tangerine';
+
+  let excelSheet = workbook.addWorksheet('Tangerine Sheet', {
+    views: [{ xSplit: 1 }],
+    pageSetup: { paperSize: 9, orientation: 'landscape' }
+  });
+
+  // Add column headers and define column keys
+  excelSheet.columns = columnData.column_headers;
+
+  // Add rows by key-value using the column keys
+  _.each(resultData, row => {
+    excelSheet.addRow(row.doc.processed_results);
+  });
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+  res.setHeader('Content-Disposition', `attachment; filename=${FILENAME}.xlsx`);
+  workbook.xlsx.write(res).then(function(data) {
+    console.log(chalk.green(`✓ You have successfully created ${FILENAME}.xlsx file at ${new Date()}`));
+    res.end();
+  });
+}
+
+exports.generateCSV = generateCSV;
---
+++ server/reporting/controllers/workflow.js
@@ -0,0 +1,154 @@
+/**
+ * This file generates the metadata for a workflow.
+ * It also exposes the createWorkflowHeaders module.
+ */
+
+/**
+ * Module dependencies
+ */
+
+const _ = require('lodash');
+const PouchDB = require('pouchdb');
+
+/**
+ * Local modules
+ */
+
+const createColumnHeaders = require('./assessment').createColumnHeaders;
+const dbQuery = require('./../utils/dbQuery');
+
+/**
+ * Retrieves all workflow collections in the database.
+ *
+ * Example:
+ *
+ *    POST /workflow
+ *
+ *  The request object must contain the database url
+ *       {
+ *         "db_url": "http://admin:password@test.tangerine.org/database_name"
+ *       }
+ *
+ * Response:
+ *
+ *  Returns an Array of objects of workflow collections.
+ *    [
+ *    	{
+ *        "id": "a1234567890",
+ *        "key": "assessment",
+ *        "value": {
+ *        	"r": "1-b123"
+ *        },
+ *        "doc": {
+ *        	"_id": "a1234567890",
+ *        	"_rev": "1-b123",
+ *        	"name": "After Testing",
+ *        	"assessmentId": "a1234567890",
+ *        	"collection": "workflow"
+ *        }
+ *      },
+ *      ...
+ *    ]
+ *
+ * @param req - HTTP request object
+ * @param res - HTTP response object
+ */
+
+exports.all = (req, res) => {
+  const GROUP_DB = new PouchDB(req.body.base_db);
+  GROUP_DB.query('ojai/byCollection', { key: 'workflow', include_docs: true })
+    .then((data) => res.json({ count: data.rows.length, workflows: data.rows }))
+    .catch((err) => res.send(err));
+}
+
+/**
+ * Generates headers for a workflow.
+ *
+ * Example:
+ *
+ *    POST /workflow/headers/:id
+ *  where id refers to the id of the workflow document.
+ *
+ *  The request object must contain the main database url and a
+ *  result database url where the generated headers will be saved.
+ *     {
+ *       "db_url": "http://admin:password@test.tangerine.org/database_name"
+ *       "another_db_url": "http://admin:password@test.tangerine.org/result_database_name"
+ *     }
+ *
+ * Response:
+ *
+ *   Returns an Object indicating the data has been saved.
+ *      {
+ *        "ok": true,
+ *        "id": "a1234567890",
+ *        "rev": "1-b123"
+ *       }
+ *
+ * @param req - HTTP request object
+ * @param res - HTTP response object
+ */
+
+exports.generateHeader = (req, res) => {
+  const workflowId = req.params.id;
+  const baseDb = req.body.base_db;
+  const resultDb = req.body.result_db;
+  const GROUP_DB = new PouchDB(workflowId);
+
+  GROUP_DB.get(workflowId, baseDb)
+    .then(async(doc) => {
+      let colHeaders = await createWorkflowHeaders(doc, baseDb);
+      const saveResponse = await dbQuery.saveHeaders(colHeaders, workflowId, resultDb);
+      console.log(saveResponse);
+      res.json(colHeaders);
+    })
+    .catch((err) => res.send(err));
+}
+
+
+/*****************************
+ *     APPLICATION MODULE    *
+ *****************************
+ */
+
+
+/**
+ * This function creates headers for a workflow.
+ *
+ * @param {string} docId - worklfow id of the document.
+ *
+ * @returns {Array} - generated headers for csv.
+ */
+
+const createWorkflowHeaders = async function(data, baseDb) {
+  let workflowHeaders = [];
+  let workflowItems = [];
+  let messageCount = 0;
+  let count = 0;
+
+  workflowHeaders.push(data.name);  // Add workflow name. Needed for csv file name
+
+  for (item of data.children) {
+    item.workflowId = data._id;
+    let isProcessed = _.filter(workflowItems, { typesId: item.typesId });
+    // this part is needed to avoid processing duplicates.
+    let isCurriculumProcessed = item.type === 'curriculum' & !isProcessed.length;
+
+    if (item.type === 'assessment' || isCurriculumProcessed) {
+      let assessmentHeaders = await createColumnHeaders(item, count, baseDb);
+      workflowHeaders.push(assessmentHeaders);
+      count++;
+    }
+    if (item.type === 'message') {
+      let messageSuffix = messageCount > 0 ? `_${messageCount}` : '';
+      workflowHeaders.push({ header: `message${messageSuffix}`, key: `${data._id}.message${messageSuffix}` });
+      messageCount++;
+    }
+    workflowItems.push(item);
+  }
+  workflowHeaders = _.flatten(workflowHeaders);
+
+  return workflowHeaders;
+}
+
+exports.createWorkflowHeaders = createWorkflowHeaders;
