35c71fd38b7e99338c637f5ad3da878dd07a9295
Mel Michael <mel.michael@andela.com>
2018-05-04 12:52:24 +0100

    Change timestamps format to unix

--- reporting/controllers/result.js
+++ reporting/controllers/result.js
@@ -263,7 +263,7 @@ const generateResult = async function(collections, count = 0, baseDb) {
         }
         if (doc.prototype === 'complete') {
           let endTimestamp = convertToTimeZone(doc.data.end_time, groupTimeZone);
-          result[`${collectionId}.end_time${assessmentSuffix}`] = moment(endTimestamp).format('hh:mm');
+          result[`${collectionId}.end_time${assessmentSuffix}`] = endTimestamp;
         }
       }
     }
@@ -428,7 +428,7 @@ function processDatetimeResult(body, subtestCount, groupTimeZone) {
     [`${body.subtestId}.month${suffix}`]: body.data.month,
     [`${body.subtestId}.day${suffix}`]: body.data.day,
     [`${body.subtestId}.assess_time${suffix}`]: body.data.time,
-    [`${body.subtestId}.timestamp_${subtestCount.timestampCount}`]: moment(timestamp).format('hh:mm')
+    [`${body.subtestId}.timestamp_${subtestCount.timestampCount}`]: timestamp
   }
   return datetimeResult;
 }
@@ -449,7 +449,7 @@ function processConsentResult(body, subtestCount, groupTimeZone) {
 
   consentResult = {
     [`${body.subtestId}.consent${suffix}`]: body.data.consent,
-    [`${body.subtestId}.timestamp_${subtestCount.timestampCount}`]: moment(timestamp).format('hh:mm')
+    [`${body.subtestId}.timestamp_${subtestCount.timestampCount}`]: timestamp
   };
   return consentResult;
 }
@@ -470,7 +470,7 @@ function processIDResult(body, subtestCount, groupTimeZone) {
 
   idResult = {
     [`${body.subtestId}.id${suffix}`]: body.data.participant_id,
-    [`${body.subtestId}.timestamp_${subtestCount.timestampCount}`]: moment(timestamp).format('hh:mm')
+    [`${body.subtestId}.timestamp_${subtestCount.timestampCount}`]: timestamp
   };
   return idResult;
 }
@@ -501,7 +501,7 @@ function processSurveyResult(body, subtestCount, groupTimeZone) {
       surveyResult[`${body.subtestId}.${doc}`] = value;
     }
   }
-  surveyResult[`${body.subtestId}.timestamp_${subtestCount.timestampCount}`] = moment(timestamp).format('hh:mm');
+  surveyResult[`${body.subtestId}.timestamp_${subtestCount.timestampCount}`] = timestamp;
 
   return surveyResult;
 }
@@ -541,7 +541,7 @@ function processGridResult(body, subtestCount, groupTimeZone, assessmentSuffix) 
 
   let fluencyRate = Math.round(correctSum / (1 - body.data.time_remain / body.data.time_allowed));
   gridResult[`${subtestId}.fluency_rate${assessmentSuffix}`] = fluencyRate;
-  gridResult[`${subtestId}.timestamp_${subtestCount.timestampCount}`] = moment(timestamp).format('hh:mm');
+  gridResult[`${subtestId}.timestamp_${subtestCount.timestampCount}`] = timestamp;
 
   return gridResult;
 }
@@ -568,7 +568,7 @@ function processGpsResult(doc, subtestCount, groupTimeZone) {
   gpsResult[`${doc.subtestId}.altitudeAccuracy${suffix}`] = doc.data.altAcc;
   gpsResult[`${doc.subtestId}.heading${suffix}`] = doc.data.heading;
   gpsResult[`${doc.subtestId}.speed${suffix}`] = doc.data.speed;
-  gpsResult[`${doc.subtestId}.timestamp_${subtestCount.timestampCount}`] = moment(timestamp).format('hh:mm');
+  gpsResult[`${doc.subtestId}.timestamp_${subtestCount.timestampCount}`] = timestamp;
 
   return gpsResult;
 }
@@ -591,7 +591,7 @@ function processCamera(body, subtestCount, groupTimeZone) {
 
   cameraResult[`${body.subtestId}.${varName}_photo_captured${suffix}`] = body.data.imageBase64;
   cameraResult[`${body.subtestId}.${varName}_photo_url${suffix}`] = body.data.imageBase64;
-  cameraResult[`${body.subtestId}.timestamp_${subtestCount.timestampCount}`] = moment(timestamp).format('hh:mm');
+  cameraResult[`${body.subtestId}.timestamp_${subtestCount.timestampCount}`] = timestamp;
 
   return cameraResult;
 }
