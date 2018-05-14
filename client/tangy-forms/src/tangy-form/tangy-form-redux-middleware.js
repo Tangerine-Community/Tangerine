1f5e21599330f3b52170b8aefb3dc7ebf74ba58d
Evans D <edianga@rti.org>
2018-05-14 10:49:54 +0300

    Fixed Typo

--- client/tangy-forms/src/tangy-form/tangy-form-redux-middleware.js
+++ client/tangy-forms/src/tangy-form/tangy-form-redux-middleware.js
@@ -18,7 +18,7 @@ const tangyReduxMiddlewareCrashReporter = store => next => action => {
   try {
     return next(action)
   } catch (err) {
-    console.error('Caught an exception!'), err)
+    console.error('Caught an exception!', err)
     console.log(action)
     console.log(store.getState())
     // throw err
