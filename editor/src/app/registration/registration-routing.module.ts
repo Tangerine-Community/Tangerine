badc698209a8d721ef33750d8dc641cbf6f383f5
Evans D <edianga@rti.org>
2018-06-20 18:07:25 +0300

    Removed reference to deleted component

--- editor/src/app/registration/registration-routing.module.ts
+++ editor/src/app/registration/registration-routing.module.ts
@@ -1,24 +1,20 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfirmationComponent } from './confirmation.component';
+import { PasswordFormComponent } from './password-form.component';
+import { PasswordChangeFormComponent } from './pwchange-form.component';
+import { RegistrationFormComponent } from './registration-form.component';
+import { UsernameFormComponent } from './username-form.component';
+import { VerifyComponent } from './verify.component';
 
-import { RegistrationFormComponent }    from './registration-form.component';
-import { LoginFormComponent }    from './login-form.component';
-import { ConfirmationComponent }  from './confirmation.component';
-import { PasswordFormComponent }  from './password-form.component';
-import { PasswordChangeFormComponent }  from './pwchange-form.component';
-import { UsernameFormComponent }  from './username-form.component';
-import { VerifyComponent }  from './verify.component';
-import { RegistrationService } from './services/registration.service';
-import { AuthGuard }                from '../auth-guard.service';
 
 const registrationRoutes: Routes = [
-  { path: 'registration',  component: RegistrationFormComponent },
-  { path: 'login',  component: LoginFormComponent },
-  { path: 'confirmation',  component: ConfirmationComponent },
-  { path: 'password',  component: PasswordFormComponent },
-  { path: 'password-reset/:token',  component: PasswordChangeFormComponent },
-  { path: 'verify',  component: VerifyComponent },
-  { path: 'username',  component: UsernameFormComponent }
+  { path: 'registration', component: RegistrationFormComponent },
+  { path: 'confirmation', component: ConfirmationComponent },
+  { path: 'password', component: PasswordFormComponent },
+  { path: 'password-reset/:token', component: PasswordChangeFormComponent },
+  { path: 'verify', component: VerifyComponent },
+  { path: 'username', component: UsernameFormComponent }
 
 ];
 @NgModule({
--- editor/src/app/registration/registration.module.ts
+++ editor/src/app/registration/registration.module.ts
@@ -1,23 +1,22 @@
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegistrationRoutingModule } from './registration-routing.module';
 
-import {ValidationModule} from '../validation/validation.module';
+import { ValidationModule } from '../validation/validation.module';
 
-import { RegistrationFormComponent }  from './registration-form.component';
-import { LoginFormComponent }  from './login-form.component';
-import { ConfirmationComponent }  from './confirmation.component';
-import { PasswordFormComponent }  from './password-form.component';
-import { PasswordChangeFormComponent }  from './pwchange-form.component';
-import { UsernameFormComponent }  from './username-form.component';
-import { VerifyComponent }  from './verify.component';
+import { RegistrationFormComponent } from './registration-form.component';
+import { ConfirmationComponent } from './confirmation.component';
+import { PasswordFormComponent } from './password-form.component';
+import { PasswordChangeFormComponent } from './pwchange-form.component';
+import { UsernameFormComponent } from './username-form.component';
+import { VerifyComponent } from './verify.component';
 import { RegistrationService } from './services/registration.service';
 import { CountriesService } from './services/countries.service';
 import { StatesService } from './services/states.service';
-import {UtilsModule} from "../utils/utils.module";
-import {ProfileService} from './services/profile.service';
+import { UtilsModule } from '../utils/utils.module';
+import { ProfileService } from './services/profile.service';
 
 
 
@@ -30,8 +29,8 @@ import {ProfileService} from './services/profile.service';
     ValidationModule,
     UtilsModule
   ],
-  declarations: [RegistrationFormComponent, LoginFormComponent, PasswordFormComponent, PasswordChangeFormComponent, 
+  declarations: [RegistrationFormComponent, PasswordFormComponent, PasswordChangeFormComponent,
     ConfirmationComponent, UsernameFormComponent, VerifyComponent],
-  providers: [ RegistrationService, CountriesService, StatesService, ProfileService ],
+  providers: [RegistrationService, CountriesService, StatesService, ProfileService],
 })
 export class RegistrationModule { }
