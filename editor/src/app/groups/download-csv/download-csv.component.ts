f1ac0fb22b872cd9fd081df1183cb36655e2d136
Evans D <edianga@rti.org>
2018-06-21 12:23:10 +0300

    Added Feature downloading CSV

--- editor/src/app/groups/download-csv/download-csv.component.ts
+++ editor/src/app/groups/download-csv/download-csv.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GroupsService } from '../services/groups.service';
+import { ActivatedRoute } from '../../../../node_modules/@angular/router';
+import { setInterval } from 'timers';
 
 @Component({
   selector: 'app-download-csv',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./download-csv.component.css']
 })
 export class DownloadCsvComponent implements OnInit {
+  stateUrl;
+  downloadUrl;
+  groupName;
+  formId;
+  isDownloadComplete;
+  errorDownloadingFile;
+  result;
+  constructor(private groupsService: GroupsService, private route: ActivatedRoute) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.groupName = params['groupName'];
+      this.formId = params['formId'];
+    });
+    await this.getData();
+  }
+  async getData() {
+    try {
+      const result: any = await this.groupsService.downloadCSV(this.groupName, this.formId);
+      this.stateUrl = result.stateUrl;
+      this.downloadUrl = result.downloadUrl;
+      // TODO call download status immediately then after every few second, Probably use RXJS to ensure we only use the latest values
+      setInterval(async () => { await this.checkDownloadStatus(); }, 5000);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
+  async checkDownloadStatus() {
+    try {
+      this.result = await this.groupsService.checkCSVDownloadStatus(this.stateUrl);
+      this.isDownloadComplete = this.result.complete;
+    } catch (error) {
+      this.errorDownloadingFile = true;
+      console.log(error);
+    }
+  }
 }
