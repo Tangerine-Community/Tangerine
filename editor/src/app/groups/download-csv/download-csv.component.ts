658140286916c98bd428f12395f7a9231f56a154
Evans D <edianga@rti.org>
2018-06-21 10:42:22 +0300

    Initialized download csv component

---
+++ editor/src/app/groups/download-csv/download-csv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-download-csv',
+  templateUrl: './download-csv.component.html',
+  styleUrls: ['./download-csv.component.css']
+})
+export class DownloadCsvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
