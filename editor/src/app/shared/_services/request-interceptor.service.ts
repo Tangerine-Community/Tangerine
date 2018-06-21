d9cab3660070c65f72dab352a08948cdd92479a6
Evans D <edianga@rti.org>
2018-06-21 14:20:03 +0300

    Added generation of APKs and PWAS

--- editor/src/app/shared/_services/request-interceptor.service.ts
+++ editor/src/app/shared/_services/request-interceptor.service.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router, RouterStateSnapshot } from '@angular/router';
 import 'rxjs/add/operator/do';
 import { Observable } from 'rxjs/Observable';
 import { TangyErrorHandler } from './tangy-error-handler.service';
@@ -8,12 +9,13 @@ import { _TRANSLATE } from './translation-marker';
 @Injectable()
 export class RequestInterceptor implements HttpInterceptor {
 
-  constructor(private errorHandler: TangyErrorHandler) { }
+  constructor(private errorHandler: TangyErrorHandler, private router: Router) { }
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).do((event: HttpEvent<any>) => { }, (err: any) => {
       if (err instanceof HttpErrorResponse) {
         if (err.status === 401) {
           this.errorHandler.handleError(_TRANSLATE('Permission Denied. Login to Continue'));
+          this.router.navigate(['login']);
         }
         if (err.status === 404) {
           this.errorHandler.handleError(_TRANSLATE('Item Not Found'));
