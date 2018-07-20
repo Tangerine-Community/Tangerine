acb290f1d9564d2d1810acad251d90f058548e51
R.J. Steinert <rj@rjsteinert.com>
2018-07-20 13:58:56 -0400

    Remove ckeditor from source control in editor, include it as a library from the tangy-form-editor module using angular-cli config

--- editor/src/ckeditor/plugins/about/lang/da.js
+++
@@ -1,9 +0,0 @@
-﻿/*
-Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
-*/
-CKEDITOR.plugins.setLang( 'about', 'da', {
-	copy: 'Copyright &copy; $1. Alle rettigheder forbeholdes.',
-	dlgTitle: 'Om CKEditor',
-	moreInfo: 'For informationer omkring licens, se venligst vores hjemmeside (på engelsk):'
-} );
