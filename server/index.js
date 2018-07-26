a3869c028487871e5a7f3480a083a9c38d5c6233
R.J. Steinert <rj@rjsteinert.com>
2018-07-26 10:12:51 -0400

    #994 Better logging when generating CSV

--- server/index.js
+++ server/index.js
@@ -825,8 +825,12 @@ app.get('/csv/:groupName/:formId', async function (req, res) {
   const batchSize = (process.env.T_CSV_BATCH_SIZE) ? process.env.T_CSV_BATCH_SIZE : 5
   const outputPath = `/csv/${fileName}`
   const cmd = `cd /tangerine/scripts/generate-csv/ && ./bin.js '${JSON.stringify(pouchDbDefaults)}' ${groupName}-reporting ${formId} ${outputPath} ${batchSize}`
-  clog(cmd)
-  exec(cmd)
+  log.info(`generating csv start: ${cmd}`)
+  exec(cmd).then(status => {
+    log.info(`generate csv done: ${JSON.stringify(status)}`)
+  }).catch(error => {
+    log.error(error)
+  })
   res.send({
     stateUrl: `${process.env.T_PROTOCOL}://${process.env.T_HOST_NAME}/csv/${fileName.replace('.csv', '.state.json')}`,
     downloadUrl: `${process.env.T_PROTOCOL}://${process.env.T_HOST_NAME}/csv/${fileName}`
