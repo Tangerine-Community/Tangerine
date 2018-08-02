cdedaee5a0ef58d173f62bca5320290bf9dcfd67
Chris E. Kelley <chris@vetula.com>
2018-08-02 11:24:01 +0200

    Added separator to UUID() enables easier use of uuid in var name
    (there is an option for no sep.)

--- editor/src/app/groups/group-details/group-details.component.ts
+++ editor/src/app/groups/group-details/group-details.component.ts
@@ -44,7 +44,10 @@ export class GroupDetailsComponent implements OnInit {
 
   }
 
-  UUID() {
+  UUID(separator) {
+    if (!separator) {
+        separator = '';
+    }
       var self = {};
       var lut = [];
       for (var i = 0; i < 256; i++) {
@@ -54,17 +57,17 @@ export class GroupDetailsComponent implements OnInit {
        * Generates a UUID
        * @returns {string}
        */
-      let generate = function() {
+      let generate = function(separator) {
           var d0 = Math.random() * 0xffffffff | 0;
           var d1 = Math.random() * 0xffffffff | 0;
           var d2 = Math.random() * 0xffffffff | 0;
           var d3 = Math.random() * 0xffffffff | 0;
-          return lut[d0 & 0xff] + lut[d0 >> 8 & 0xff] + lut[d0 >> 16 & 0xff] + lut[d0 >> 24 & 0xff] + '-' +
-              lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + '-' + lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + '-' +
-              lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
+          return lut[d0 & 0xff] + lut[d0 >> 8 & 0xff] + lut[d0 >> 16 & 0xff] + lut[d0 >> 24 & 0xff] + separator +
+              lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + separator + lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + separator +
+              lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + separator + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
               lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
       };
-      return generate();
+      return generate(separator);
   }
 
   async addForm() {
