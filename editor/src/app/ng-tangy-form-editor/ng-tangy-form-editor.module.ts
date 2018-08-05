64e4278327c2a1889bfcc5b533017d0e13d8064d
Chris E. Kelley <chris@vetula.com>
2018-08-05 16:28:26 +0200

    Added tabs and translate support to NgTangyFormEditorComponent
    link to groups

--- editor/src/app/ng-tangy-form-editor/ng-tangy-form-editor.module.ts
+++ editor/src/app/ng-tangy-form-editor/ng-tangy-form-editor.module.ts
@@ -1,14 +1,18 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { NgTangyFormEditorRoutingModule } from './ng-tangy-form-editor-routing.module';
 import { NgTangyFormEditorComponent } from './ng-tangy-form-editor/ng-tangy-form-editor.component';
+import {SharedModule} from "../shared/shared.module";
+import {MatTabsModule} from "@angular/material";
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     CommonModule,
-    NgTangyFormEditorRoutingModule
+    NgTangyFormEditorRoutingModule,
+    MatTabsModule,
+    SharedModule
   ],
   declarations: [NgTangyFormEditorComponent]
 })
