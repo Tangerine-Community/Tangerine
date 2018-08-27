bbed0987b97b223a5400e8fb4813dfda5c286027
R.J. Steinert <rj@rjsteinert.com>
2018-08-27 16:26:43 -0400

    Add a filter by form select to responses list in group's editor

--- editor/src/app/groups/responses/responses.component.ts
+++ editor/src/app/groups/responses/responses.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { GroupsService } from '../services/groups.service';
+import { HttpClient } from '@angular/common/http';
 import * as moment from 'moment'
 
 @Component({
@@ -8,15 +10,32 @@ import * as moment from 'moment'
 })
 export class ResponsesComponent implements OnInit {
 
-  @Input() responses = [];
   @Input() groupName = '';
   moment;
+  responses;
+  forms = [];
 
-  constructor() {
+  constructor(
+    private groupsService: GroupsService,
+    private http: HttpClient
+  ) {
     this.moment = moment
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    let responses = await this.http.get(`/api/${this.groupName}/responses`).toPromise()
+    this.responses = responses
+    this.forms = await this.groupsService.getFormsList(this.groupName);
+  }
+
+  async filterByForm(event) {
+    if (event.target.value === '*') {
+      let responses = await this.http.get(`/api/${this.groupName}/responses`).toPromise()
+      this.responses = responses
+    } else {
+      let responses = await this.http.get(`/api/${this.groupName}/responsesByFormId/${event.target.value}`).toPromise()
+      this.responses = responses
+    }
   }
 
 }
