4fda9db635d464b2323b3a4b1a7132a88b243252
R.J. Steinert <rj@rjsteinert.com>
2018-08-29 13:46:26 -0400

    When playing user profiles, prevent locking, also show a nav to go back to group

--- editor/src/app/tangy-forms/tangy-forms.module.ts
+++ editor/src/app/tangy-forms/tangy-forms.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { TangyFormsRoutingModule } from './tangy-forms-routing.module';
 import { TangyFormsPlayerComponent } from './tangy-forms-player/tangy-forms-player.component';
+import {MatTabsModule} from "@angular/material";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
+    MatTabsModule,
+    SharedModule,
     TangyFormsRoutingModule
   ],
   declarations: [TangyFormsPlayerComponent]
