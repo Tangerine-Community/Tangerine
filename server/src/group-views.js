59535b1743e328f57c28af72e896956d886a2d50
R.J. Steinert <rj@rjsteinert.com>
2018-08-29 17:19:39 -0400

    Add responsesByUserProfileId view and corresponding API route

--- server/src/group-views.js
+++ server/src/group-views.js
@@ -13,3 +13,18 @@ module.exports.responsesByStartUnixTime = function(doc) {
     return emit(doc.startUnixtime, true)
   }
 }
+
+module.exports.responsesByUserProfileId = function(doc) {
+  if (doc.collection = "TangyFormResponse") {
+    var inputs = doc.items.reduce(function(acc, item) { return acc.concat(item.inputs)}, [])
+    var userProfileInput = null
+    inputs.forEach(function(input) {
+      if (input.name === 'userProfileId') {
+        userProfileInput = input
+      }
+    })
+    if (userProfileInput) {
+      emit(userProfileInput.value, true)
+    }
+  }
+}
