2deaa731965b333f80f8a7903415e498ba731b93
R.J. Steinert <rj@rjsteinert.com>
2018-09-12 11:08:53 -0400

    Clean up and fix creating new groups reporting infrastructure

--- server/src/insert-group-reporting-views.js
+++ server/src/insert-group-reporting-views.js
@@ -3,9 +3,9 @@ const clog = require('tangy-log').clog
 const groupReportingViews = require(`./group-reporting-views.js`)
 const dbConnection = require('./db')
 
-module.exports = async function insertGroupViews(databaseName) {
+module.exports = async function insertGroupReportingViews(groupName) {
   let designDoc = Object.assign({}, groupReportingViews)
-  let groupDb = new dbConnection(`${databaseName}-reporting`)
+  let groupDb = new dbConnection(`${groupName}-reporting`)
   try {
     const existingDesignDoc = await groupDb.get(`_design/tangy-reporting`)
     designDoc._rev = existingDesignDoc._rev
@@ -14,9 +14,8 @@ module.exports = async function insertGroupViews(databaseName) {
   }
   try {
     let status = await groupDb.post(designDoc)
-    log.info(`group reporting views inserted into ${databaseName}-reporting`)
+    log.info(`group reporting views inserted into ${groupName}-reporting`)
   } catch (error) {
     log.error(error)
   }
-  let groupReportingDb = new dbConnection(`${databaseName}-reporting`)
 }
