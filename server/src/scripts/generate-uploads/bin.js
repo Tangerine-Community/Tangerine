c581726fa1c15cf6ec2d7b809e1bbd3c01d4b2b2
R.J. Steinert <rj@rjsteinert.com>
2018-09-27 16:18:36 -0400

    Update generate-uploads script to work with new Authorization token and URL path

--- server/src/scripts/generate-uploads/bin.js
+++ server/src/scripts/generate-uploads/bin.js
@@ -11,7 +11,7 @@ const axios = require('axios')
 const uuidv1 = require('uuid/v1');
 const templateDoc = require('./template-doc.js').doc
 const numberOfUploads = parseInt(process.argv[2])
-const url = `http://localhost/upload/${process.argv[3]}`
+const url = `http://127.0.0.1/api/${process.argv[3]}/upload`
 const delay = process.argv[4] ? parseInt(process.argv[4]) : 2000
 const batchSize = process.argv[5] ? parseInt(process.argv[5]) : 100
 
@@ -31,7 +31,7 @@ async function go() {
       let body = pako.deflate(JSON.stringify({ doc }), {to: 'string'})
       bodies.push(body)
     }
-    let batch = bodies.map((body) => axios({method: 'post', url, data: `${body}`, headers: { 'content-type': 'text/plain' },}))
+    let batch = bodies.map((body) => axios({method: 'post', url, data: `${body}`, headers: { 'content-type': 'text/plain', 'Authorization': `${process.env.T_UPLOAD_TOKEN}` },}))
     // Don't await, the world doesn't wait. We're trying to cause stress at the rate defined.
     // await Promise.all(batch)
     numberOfUploadsCompleted = numberOfUploadsCompleted + batchSize
@@ -39,4 +39,9 @@ async function go() {
     await sleep(delay)
   }
 }
-go()
+
+try {
+  go()
+} catch(e) {
+  console.log(e)
+}
