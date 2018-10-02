1c1468e73c6f51fb43ed430442f25365dd6a67c2
R.J. Steinert <rj@rjsteinert.com>
2018-10-02 17:38:14 -0400

    #1067 Add better support for container naming to build and run, fix in config.defaults.sh

--- build-and-start.sh
+++ build-and-start.sh
@@ -1,3 +1,12 @@
+
+# Load config.
+source ./config.defaults.sh
+if [ -f "./config.sh" ]; then
+  source ./config.sh
+else
+  echo "You have no config.sh. Copy config.defaults.sh to config.sh, change the passwords and try again." && exit 1;
+fi
+
 docker build -t tangerine/tangerine:local .
 ./start.sh local 
-docker logs -f tangerine
+docker logs -f $T_CONTAINER_NAME 
