f70862a0b3281b94195d80fb96595ca6de9fdc68
R.J. Steinert <rj@rjsteinert.com>
2018-10-03 09:25:56 -0400

    #954 Refactor links to register, login, and reset password to use paper tabs and buttons

--- client/app/src/app/core/auth/auth.module.ts
+++ client/app/src/app/core/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule, MatInputModule, MatSelectModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +13,7 @@ import { RegistrationComponent } from './registration/registration.component';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     CommonModule,
     MatInputModule,
