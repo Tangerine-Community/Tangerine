e952ca628c252ae1a7090899317d4c945d4b16f3
R.J. Steinert <rj@rjsteinert.com>
2018-10-03 14:07:48 -0400

    #1098 Refactor import of user profile to use short code instead of full UUID

--- server/src/group-views.js
+++ server/src/group-views.js
@@ -16,11 +16,11 @@ module.exports.responsesByStartUnixTime = function(doc) {
 
 module.exports.responsesByUserProfileId = function(doc) {
   if (doc.collection = "TangyFormResponse") {
-    var inputs = doc.items.reduce(function(acc, item) { return acc.concat(item.inputs)}, [])
-    var userProfileInput = null
     if (doc.form && doc.form.id === 'user-profile') {
       return emit(doc._id, true)
     }
+    var inputs = doc.items.reduce(function(acc, item) { return acc.concat(item.inputs)}, [])
+    var userProfileInput = null
     inputs.forEach(function(input) {
       if (input.name === 'userProfileId') {
         userProfileInput = input
@@ -28,6 +28,24 @@ module.exports.responsesByUserProfileId = function(doc) {
     })
     if (userProfileInput) {
       emit(userProfileInput.value, true)
+    }
+  }
+}
+
+module.exports.responsesByUserProfileShortCode = function(doc) {
+  if (doc.collection = "TangyFormResponse") {
+    if (doc.form && doc.form.id === 'user-profile') {
+      return emit(doc._id.substr(doc._id.length-6, doc._id.length), true)
+    }
+    var inputs = doc.items.reduce(function(acc, item) { return acc.concat(item.inputs)}, [])
+    var userProfileInput = null
+    inputs.forEach(function(input) {
+      if (input.name === 'userProfileId') {
+        userProfileInput = input
+      }
+    })
+    if (userProfileInput) {
+      emit(userProfileInput.value.substr(userProfileInput.value.length-6, userProfileInput.value.length), true)
     }
   }
 }
