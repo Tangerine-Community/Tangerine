85b275959b6cc1aed33ad6a223234802ebf397cd
R.J. Steinert <rj@rjsteinert.com>
2018-12-17 16:10:15 -0500

    Remove deprecated reporting server

--- reporting/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo 'node version is...'
-node -v
-export NVM_DIR="$HOME/.nvm" && \
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
-nvm use 8 && \
-node -v && \
-#nodemon --inspect=[::]:9229 index.js
-nodemon index.js
