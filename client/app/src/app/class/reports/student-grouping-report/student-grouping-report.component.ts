14d40d290c484a9c477559f355e64d046cacd436
Chris E. Kelley <chris@vetula.com>
2018-12-19 16:44:41 +0100

    Class: implementing percentile in grouping report

--- client/app/src/app/class/reports/student-grouping-report/student-grouping-report.component.ts
+++ client/app/src/app/class/reports/student-grouping-report/student-grouping-report.component.ts
@@ -105,13 +105,12 @@ export class StudentGroupingReportComponent implements OnInit {
     const itemId = this.route.snapshot.paramMap.get('type');
     const classId = this.route.snapshot.paramMap.get('classId');
     let curriculumId = this.route.snapshot.paramMap.get('curriculumId');
-    let classDoc = await this.classFormService.getResponse(classId);
-    let classRegistration = this.classUtils.getInputValues(classDoc);
+    // let classDoc = await this.classFormService.getResponse(classId);
+    // let classRegistration = this.classUtils.getInputValues(classDoc);
 
     // Get data about this particular subtest
     let curriculumFormHtml = await this.dashboardService.getCurriculaForms(curriculumId);
-    const container = this.container.nativeElement
-    let curriculumFormsList = await this.classUtils.createCurriculumFormsList(curriculumFormHtml, container);
+    let curriculumFormsList = await this.classUtils.createCurriculumFormsList(curriculumFormHtml);
 
     this.formList = [];
     for (const form of curriculumFormsList) {
@@ -159,65 +158,103 @@ export class StudentGroupingReportComponent implements OnInit {
       studentResults["name"] = student.doc.items[0].inputs[0].value
       studentResults["classId"] = student.doc.items[0].inputs[3].value
       studentResults["forms"] = [];
-      // for (const form of curriculumFormsList) {
-        if (this.studentsResponses[student.id]) {
-          // if (form.id === itemId) {
-            let studentResponse = this.studentsResponses[student.id][itemId]
-            if (studentResponse) {
-              studentResults["response"] = studentResponse
-              let score = studentResponse["score"]
-              if (score) {
-                studentResults["score"] = score
-                // console.log("student: " + studentResults["name"]  + " form item: " + studentResults["response"]["formTitle"]  + " score: " + score)
-              }
-              let totalGridCorrect = studentResponse["totalGridCorrect"]
-              let totalGridPercentageCorrect = studentResponse["totalGridPercentageCorrect"]
-              let totalGridAnswers = studentResponse["totalGridAnswers"]
-              duration = studentResponse["duration"]
-              if (totalGridPercentageCorrect) {
-                studentResults["score"] = totalGridPercentageCorrect
-                let index;
-                if (totalGridPercentageCorrect >= 80)
-                  index = 3
-                else if (totalGridPercentageCorrect >= 60 && totalGridPercentageCorrect <= 79)
-                  index = 2
-                else if (totalGridPercentageCorrect >= 30 && totalGridPercentageCorrect <= 59)
-                  index = 1
-                else
-                  index = 0
-                studentResults["index"] = index
-                studentResults["status"] = status[index]
-                studentResults["colorClass"] = colorClass[index]
-                if (index === 0) {
-                  studentsToWatch.push(studentResults["name"])
-                }
-              }
-              aveCorrect += totalGridCorrect
-              aveCorrectPerc += totalGridPercentageCorrect
-              attempted += totalGridAnswers
-              studentsAssessed ++
-              // TODO: calculate percentile and stdDev.
-              // TODO: factor correct answers per minute.
-              // TODO: factor attempted vs not attempted.
+      if (this.studentsResponses[student.id]) {
+        let studentResponse = this.studentsResponses[student.id][itemId]
+        if (studentResponse) {
+          studentResults["response"] = studentResponse
+          let score = studentResponse["score"]
+          if (score) {
+            studentResults["score"] = score
+            // console.log("student: " + studentResults["name"]  + " form item: " + studentResults["response"]["formTitle"]  + " score: " + score)
+          }
+          let totalGridCorrect = studentResponse["totalGridCorrect"]
+          let totalGridPercentageCorrect = studentResponse["totalGridPercentageCorrect"]
+          studentResults["totalGridPercentageCorrect"] = totalGridPercentageCorrect
+          let totalGridAnswers = studentResponse["totalGridAnswers"]
+          duration = studentResponse["duration"]
+          if (totalGridPercentageCorrect) {
+            studentResults["score"] = totalGridPercentageCorrect
+            let index;
+            if (totalGridPercentageCorrect >= 80)
+              index = 3
+            else if (totalGridPercentageCorrect >= 60 && totalGridPercentageCorrect <= 79)
+              index = 2
+            else if (totalGridPercentageCorrect >= 30 && totalGridPercentageCorrect <= 59)
+              index = 1
+            else
+              index = 0
+            studentResults["index"] = index
+            studentResults["status"] = status[index]
+            studentResults["colorClass"] = colorClass[index]
+            if (index === 0) {
+              studentsToWatch.push(studentResults["name"])
             }
-          // }
+          }
+          aveCorrect += totalGridCorrect
+          aveCorrectPerc += totalGridPercentageCorrect
+          attempted += totalGridAnswers
+          studentsAssessed ++
+          // TODO: calculate percentile and stdDev.
+          // TODO: factor correct answers per minute.
+          // TODO: factor attempted vs not attempted.
         }
-        // else {
-        //   if (form.id === itemId) {
-        //     studentResults["status"] = this.status[0]
-        //     studentsToWatch.push(studentResults["name"])
-        //   }
-        // }
-      // }
+      }
       allStudentResults.push(studentResults)
     }
 
     // aveCorrectPerc = this.classUtils.decimals( aveCorrectPerc / this.students.length, 0 )
     // aveCorrect = this.classUtils.decimals( aveCorrect / this.students.length, 2 )
-    aveCorrectPerc = this.classUtils.round( aveCorrectPerc / studentsAssessed, 0 )
-    aveCorrect = this.classUtils.round( aveCorrect / studentsAssessed, 2 )
-    attempted = this.classUtils.round( attempted / studentsAssessed, 2 )
+    aveCorrectPerc = this.classUtils.decimals( aveCorrectPerc / studentsAssessed, 0 )
+    aveCorrect = this.classUtils.decimals( aveCorrect / studentsAssessed, 2 )
+    attempted = this.classUtils.decimals( attempted / studentsAssessed, 2 )
 
+    let stdDev = 0
+
+    // for (const studentResult of allStudentResults) {
+    //   let totalGridPercentageCorrect = studentResult['totalGridPercentageCorrect']
+    //   // @summary.stdDev += Math.pow(person.pCorrect - @summary.aCorrect, 2)
+    //   stdDev += Math.pow(totalGridPercentageCorrect - aveCorrectPerc, 2)
+    // }
+
+    var squareDiffs = allStudentResults.map(function(studentResult){
+      let totalGridPercentageCorrect = studentResult['totalGridPercentageCorrect']
+      var diff = totalGridPercentageCorrect - aveCorrectPerc;
+      var sqr = diff * diff;
+      return sqr;
+    });
+
+    var avgSquareDiff = average(squareDiffs);
+
+    stdDev = Math.sqrt(avgSquareDiff);
+
+    function average(data){
+      var sum = data.reduce(function(sum, value){
+        return sum + value;
+      }, 0);
+
+      var avg = sum / data.length;
+      return avg;
+    }
+
+  // @summary.stdDev = Math.decimals(Math.pow( @summary.stdDev / @table.length, 0.5), 2)
+  //   stdDev = this.classUtils.decimals(Math.pow( stdDev / studentsAssessed, 0.5), 2)
+
+    for (const studentResult of allStudentResults) {
+      let totalGridPercentageCorrect = studentResult['totalGridPercentageCorrect']
+      // dev = (person.pCorrect - @summary.aCorrect) / @summary.stdDev
+      let dev = (totalGridPercentageCorrect - aveCorrectPerc) / stdDev
+      let devIndex = Math.round(dev * 100)
+      let percentile
+      if (devIndex > 409 || devIndex < -409)
+        percentile = 0
+      else if (devIndex > 0)
+        percentile = 100 * Math.round(50 + 100 * normalCurve[devIndex] ) / 100
+      else if (devIndex < 0)
+        percentile = 100 * Math.round(50 - 100 * normalCurve[devIndex * -1] ) / 100
+      else
+        percentile = 50
+      studentResult['percentile'] = percentile
+    }
 
     function compare(a,b) {
       if (a.score < b.score)
