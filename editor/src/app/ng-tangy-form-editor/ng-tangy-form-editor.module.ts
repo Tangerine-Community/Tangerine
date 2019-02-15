8e2f337ea9502ca0924e1c8f91a7f55e18fb1626
Chris E. Kelley <chris@vetula.com>
2019-02-15 16:44:14 +0100

    Refactored feedback editor from dialog to app component.

--- editor/src/app/ng-tangy-form-editor/ng-tangy-form-editor.module.ts
+++ editor/src/app/ng-tangy-form-editor/ng-tangy-form-editor.module.ts
@@ -2,20 +2,18 @@ import { CommonModule } from '@angular/common';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { NgTangyFormEditorRoutingModule } from './ng-tangy-form-editor-routing.module';
-import {
-  NgTangyFormEditorComponent
-} from './ng-tangy-form-editor/ng-tangy-form-editor.component';
-import {SharedModule} from "../shared/shared.module";
-import {MatTabsModule, MatIconModule, MatDialog} from "@angular/material";
-import { FormJsonEditorComponent } from './form-json-editor/form-json-editor.component';
-import {MatDialogModule} from '@angular/material/dialog';
+import { NgTangyFormEditorComponent } from './ng-tangy-form-editor/ng-tangy-form-editor.component';
+import { SharedModule } from "../shared/shared.module";
+import { MatTabsModule, MatIconModule } from "@angular/material";
+import { FeedbackEditorComponent } from './feedback-editor/feedback-editor.component';
 import { FormsModule } from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
-import {FeedbackService} from "./form-json-editor/feedback.service";
+import {FeedbackService} from "./feedback-editor/feedback.service";
 import {MatTableModule} from '@angular/material/table';
+import {FormMetadata} from "./feedback-editor/form-metadata";
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -24,7 +22,6 @@ import {MatTableModule} from '@angular/material/table';
     NgTangyFormEditorRoutingModule,
     MatTabsModule,
     MatIconModule,
-    MatDialogModule,
     FormsModule,
     MatSelectModule,
     MatInputModule,
@@ -33,9 +30,8 @@ import {MatTableModule} from '@angular/material/table';
     MatTableModule,
     SharedModule
   ],
-  declarations: [NgTangyFormEditorComponent, FormJsonEditorComponent],
-  entryComponents:[FormJsonEditorComponent],
-  providers:[FeedbackService]
+  declarations: [NgTangyFormEditorComponent, FeedbackEditorComponent],
+  providers:[FeedbackService, FormMetadata]
 })
 export class NgTangyFormEditorModule { }
 
