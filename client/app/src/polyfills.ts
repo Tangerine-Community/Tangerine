ada4fb504961f6226af660afbffaa7b8e474dac9
R.J. Steinert <rj@rjsteinert.com>
2019-03-06 12:46:04 -0500

    Add tangy-qr editing and usage to editor and client

--- client/app/src/polyfills.ts
+++ client/app/src/polyfills.ts
@@ -27,6 +27,7 @@ import 'tangy-form/input/tangy-gps.js'
 import 'tangy-form/input/tangy-acasi.js';
 import 'tangy-form/input/tangy-eftouch.js';
 import 'tangy-form/input/tangy-photo-capture.js';
+import 'tangy-form/input/tangy-qr.js';
 
 
 // An attempt to fix something...
