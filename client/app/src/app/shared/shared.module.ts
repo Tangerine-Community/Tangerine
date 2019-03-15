b04cb9e6bd4fd3b2fff04125b9cd9c74b979737a
R.J. Steinert <rj@rjsteinert.com>
2019-03-15 11:28:42 -0400

    Remove directives no longer being used

--- client/app/src/app/shared/shared.module.ts
+++ client/app/src/app/shared/shared.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { AppConfigService } from '../shared/_services/app-config.service';
 
-import { SafeUrlPipe } from '../tangy-forms/safe-url.pipe';
 import {
   RedirectToDefaultRouteComponent,
 } from './_components/redirect-to-default-route/redirect-to-default-route.component';
 import { TangySvgLogoComponent } from './_components/tangy-svg-logo/tangy-svg-logo.component';
-import { SeamlessWithWindowDirective } from './_directives/seamless-with-window.directive';
 import { TruncateValuePipe } from './_pipes/truncate-value.pipe';
 import { UnsanitizeHtmlPipe } from './_pipes/unsanitize-html.pipe';
 import { TranslateModule } from '@ngx-translate/core';
@@ -19,15 +17,15 @@ import { TranslateModule } from '@ngx-translate/core';
     MatTooltipModule
   ],
   providers: [AppConfigService],
-  declarations: [SafeUrlPipe,
+  declarations: [
     UnsanitizeHtmlPipe,
-    SeamlessWithWindowDirective, TangySvgLogoComponent,
+    TangySvgLogoComponent,
     TruncateValuePipe,
     RedirectToDefaultRouteComponent],
 
-  exports: [RedirectToDefaultRouteComponent, SafeUrlPipe,
+  exports: [RedirectToDefaultRouteComponent,
     UnsanitizeHtmlPipe,
-    SeamlessWithWindowDirective, TangySvgLogoComponent,
+    TangySvgLogoComponent,
     TruncateValuePipe, TranslateModule]
 })
 export class SharedModule { }
