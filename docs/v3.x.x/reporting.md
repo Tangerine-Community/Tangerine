f0a47ac2e34d6838d1a7156daf3e035c76de7a1f
Chris E. Kelley <chris@vetula.com>
2019-03-19 20:51:03 +0100

    Moved generateFlatResponse to csv and logstash modules
    Updated report debugging documentation.

--- docs/v3.x.x/reporting.md
+++ docs/v3.x.x/reporting.md
@@ -1,6 +1,11 @@
 
 ## Debugging the Reporting Cache process
 
+Configure your project to use the CSV and Logstash modules:
+```
+T_MODULES="['csv','class', 'logstash']"
+```
+
 Start the development environment...
 ```
 ./develop.sh
@@ -8,11 +13,11 @@ Start the development environment...
 
 Create a group called `foo` in the GUI. Then open `./server/index.js` and comment out the call to `keepAliveReportingWorker(initialGroups)`. 
 
-"exec" into the container and note how `foo` has been added to the `/worker-state.json` file.
+"exec" into the container and note how `foo` has been added to the `/reporting-worker-state.json` file.
 
 ```
 docker exec -it tangerine bash
-cat /worker-state.json
+cat /reporting-worker-state.json
 ```
 
 Seed the `foo` group with 100 form responses.
@@ -20,27 +25,32 @@ Seed the `foo` group with 100 form responses.
 docker exec tangerine generate-uploads 100 foo
 ```
 
-//@TODO OUTDATED
+In Chrome, go to `chrome://inspect`, click `Configure...`, and add `127.0.0.1:9228` as an entry in "Target discovery settings".
+
+//@TODO OUTDATED but still relevant
 Now manually trigger a batch. After the command finishes, verify the batch by checking `http://localhost:5984/_utils/#database/foo-reporting/_all_docs`.
 ```
 node --inspect-brk=0.0.0.0:9228 $(which reporting-worker-batch)
 ```
+There will be only 15 docs in your reporting db because that is the batch size.
 
-If no errors occured, copy the temporary state to the current state.
+Although Tangerine in develop.sh mode runs node in a debugger process, you must launch a separate node process to debug the batch reporting worker.
+
+If no errors occurred, copy the temporary state to the current state.
 ```
-cp /worker-state.json_tmp /worker-state.json
+cp /reporting-worker-state.json_tmp /reporting-worker-state.json
 ```
 
 Keep repeating to continue processing...
 ```
-cat /worker-state.json | node /tangerine/server/reporting/run-worker.js | tee /worker-state.json_tmp
-cp /worker-state.json_tmp /worker-state.json
+cat /reporting-worker-state.json | node /tangerine/server/reporting/run-worker.js | tee /reporting-worker-state.json_tmp
+cp /reporting-worker-state.json_tmp /reporting-worker-state.json
 ```
 
 If you would like to debug, add the `--inspect-brk=0.0.0.0:9227` option to the `run-worker.js` command.
 
 ```
-cat /worker-state.json | node --inspect-brk=0.0.0.0:9227 /tangerine/server/reporting/run-worker.js | tee /worker-state.json_tmp
+cat /reporting-worker-state.json | node --inspect-brk=0.0.0.0:9227 /tangerine/server/reporting/run-worker.js | tee /reporting-worker-state.json_tmp
 ```
 
 When you run that command, it will wait on the first line of the script for a debugger to connect to it. In Chrome, go to `chrome://inspect`, click `Configure...`, and add `127.0.0.1:9227` as an entry in "Target discovery settings". Now back to the `chrome://inspect` page and you will find under the `Remote Target #127.0.0.1` group, a new target has been discovered called `/tangerine/server/reporting/run-worker.js`. Click `inspect` and now you should be able to set breakpoints and walk through the code. You may not be able to set breakpoints in all files so use "step into" and the `debugger` keyword to get the debugger to the focus you want.
@@ -48,7 +58,7 @@ When you run that command, it will wait on the first line of the script for a de
 
 If you want to keep the cache worker running, use watch.
 ```
-watch -n 1 "cat /worker-state.json | node /tangerine/server/reporting/run-worker.js | tee /.worker-state.json | json_pp && cp /.worker-state.json /worker-state.json"
+watch -n 1 "cat /reporting-worker-state.json | node /tangerine/server/reporting/run-worker.js | tee /.reporting-worker-state.json | json_pp && cp /.reporting-worker-state.json /reporting-worker-state.json"
 ```
 
 If you need to clear a reporting cache, don't simply delete the reporting db. Use
@@ -56,3 +66,35 @@ If you need to clear a reporting cache, don't simply delete the reporting db. Us
 cd /tangerine/server/src/scripts
 ./clear-reporting-cache.js
 ```
+
+## A typical report debugging workflow:
+
+Remember to setup config.sh properly!
+Comment out keepAliveReportingWorker in server/index.js.
+Remember to add `127.0.0.1:9228` as an entry in "Target discovery settings".
+
+You may need to add `debugger` before the line of code you wish to debug. 
+
+docker exec into your container
+
+```
+docker exec -it tangerine bash
+```
+Then you'll typically need to rm the reporting-worker-running - it keeps reporting-cache-clear from running if a previous debug session crashed.
+
+```
+rm /reporting-worker-running
+reporting-cache-clear
+node --inspect-brk=0.0.0.0:9228 $(which reporting-worker-batch)
+```
+Switch back to Chrome, open `chrome://inspect`. The debugger will be the session that looks like this:
+
+```
+Target
+/usr/local/bin/reporting-worker-batch file:///tangerine/server/src/scripts/reporting-worker-batch.js
+Inspect
+```
+
+When it launches, it will wait on the first line of the script for a debugger to connect to it. Click F8 to run. 
+If all is right and good in this world, it will stop at your debugger statement.
+When the batch has completed, your debugger window will close.
--- server/src/modules/csv/index.js
+++ server/src/modules/csv/index.js
@@ -2,6 +2,11 @@ const DB = require('../../db.js')
 const log = require('tangy-log').log
 const clog = require('tangy-log').clog
 const insertGroupReportingViews = require(`../../insert-group-reporting-views.js`)
+const {promisify} = require('util');
+const fs = require('fs');
+const readFile = promisify(fs.readFile);
+const tangyModules = require('../index.js')()
+const CODE_SKIP = '999'
 
 module.exports = {
   hooks: {
@@ -16,19 +21,126 @@ module.exports = {
     },
     reportingOutputs: function(data) {
       return new Promise(async (resolve, reject) => {
-          const {flatResponse, doc, sourceDb} = data
-          // @TODO Rename `-reporting` to `-csv`.
-          const REPORTING_DB = new DB(`${sourceDb.name}-reporting`);
-          // @TODO Ensure design docs are in the database.
-          await saveFormInfo(flatResponse, REPORTING_DB);
-          await saveFlatFormResponse(flatResponse, REPORTING_DB);
-          // Index the view now.
-          await REPORTING_DB.query('tangy-reporting/resultsByGroupFormId', {limit: 0})
-          resolve(data)
+        const {doc, sourceDb} = data
+        const locationList = JSON.parse(await readFile(`/tangerine/client/content/groups/${sourceDb.name}/location-list.json`))
+        let flatResponse = await generateFlatResponse(doc, locationList);
+
+        // Process the flatResponse
+        // @TODO Rename `-reporting` to `-csv`.
+        const REPORTING_DB = new DB(`${sourceDb.name}-reporting`);
+        // @TODO Ensure design docs are in the database.
+        await saveFormInfo(flatResponse, REPORTING_DB);
+        await saveFlatFormResponse(flatResponse, REPORTING_DB);
+        // Index the view now.
+        await REPORTING_DB.query('tangy-reporting/resultsByGroupFormId', {limit: 0})
+        resolve(data)
       })
     }
   }
 }
+
+/** This function processes form response for csv.
+ *
+ * @param {object} formData - form response from database
+ * @param {object} locationList - location list doing label lookups on TANGY-LOCATION inputs
+ *
+ * @returns {object} processed results for csv
+ */
+
+const generateFlatResponse = async function (formResponse, locationList) {
+  if (formResponse.form.id === '') {
+    formResponse.form.id = 'blank'
+  }
+  let flatFormResponse = {
+    _id: formResponse._id,
+    formId: formResponse.form.id,
+    startUnixtime: formResponse.startUnixtime,
+    complete: formResponse.complete
+  };
+  let formID = formResponse.form.id;
+  for (let item of formResponse.items) {
+    for (let input of item.inputs) {
+      if (input.hidden || item.disabled) {
+        // If the input is disabled or hidden, or the the item is disabled, set SKIP code.
+        flatFormResponse[`${formID}.${item.id}.${input.name}`] = CODE_SKIP
+      } else if (input.tagName === 'TANGY-LOCATION') {
+        // Populate the ID and Label columns for TANGY-LOCATION levels.
+        locationKeys = []
+        for (let group of input.value) {
+          flatFormResponse[`${formID}.${item.id}.${input.name}.${group.level}`] = group.value;
+          locationKeys.push(group.value)
+          try {
+            const location = getLocationByKeys(locationKeys, locationList)
+            for (let keyName in location) {
+              if (keyName !== 'children') {
+                flatFormResponse[`${formID}.${item.id}.${input.name}.${group.level}_${keyName}`] = location[keyName]
+              }
+            }
+          } catch(e) {
+            flatFormResponse[`${formID}.${item.id}.${input.name}.${group.level}_label`] = 'orphaned';
+          }
+        }
+      } else if (input.tagName === 'TANGY-RADIO-BUTTONS') {
+        flatFormResponse[`${formID}.${item.id}.${input.name}`] = input.value.find(input => input.value == 'on')
+          ? input.value.find(input => input.value == 'on').name
+          : ''
+      } else if (input.tagName === 'TANGY-CHECKBOXES') {
+        for (let checkboxInput of input.value) {
+          flatFormResponse[`${formID}.${item.id}.${input.name}_${checkboxInput.name}`] = checkboxInput.value
+            ? "1"
+            : "0"
+        };
+      } else if (input.tagName === 'TANGY-TIMED') {
+        let hitLastAttempted = false
+        for (let toggleInput of input.value) {
+          let derivedValue = ''
+          if (hitLastAttempted === true) {
+            // Not attempted.
+            derivedValue = '.'
+          } else if (toggleInput.value === 'on') {
+            // Incorrect.
+            derivedValue = '0'
+          } else {
+            // Correct.
+            derivedValue = '1'
+          }
+          flatFormResponse[`${formID}.${item.id}.${input.name}_${toggleInput.name}`] = derivedValue
+          if (toggleInput.highlighted === true) {
+            hitLastAttempted = true
+          }
+        };
+        flatFormResponse[`${formID}.${item.id}.${input.name}.duration`] = input.duration
+        flatFormResponse[`${formID}.${item.id}.${input.name}.time_remaining`] = input.timeRemaining
+        // Calculate Items Per Minute.
+        let numberOfItemsAttempted = input.value.findIndex(el => el.highlighted ? true : false) + 1
+        let numberOfItemsIncorrect = input.value.filter(el => el.value ? true : false).length
+        let numberOfItemsCorrect = numberOfItemsAttempted - numberOfItemsIncorrect
+        flatFormResponse[`${formID}.${item.id}.${input.name}.number_of_items_correct`] = numberOfItemsCorrect
+        flatFormResponse[`${formID}.${item.id}.${input.name}.number_of_items_attempted`] = numberOfItemsAttempted
+        let timeSpent = input.duration - input.timeRemaining
+        flatFormResponse[`${formID}.${item.id}.${input.name}.items_per_minute`] = Math.round(numberOfItemsCorrect / (timeSpent / 60))
+      } else if (input.tagName === 'TANGY-BOX' || input.name === '') {
+        // Do nothing :).
+      } else if (input && typeof input.value === 'string') {
+        flatFormResponse[`${formID}.${item.id}.${input.name}`] = input.value;
+      } else if (input && typeof input.value === 'number') {
+        flatFormResponse[`${formID}.${item.id}.${input.name}`] = input.value;
+      } else if (input && Array.isArray(input.value)) {
+        for (let group of input.value) {
+          flatFormResponse[`${formID}.${item.id}.${input.name}.${group.name}`] = group.value;
+        }
+      } else if ((input && typeof input.value === 'object') && (input && !Array.isArray(input.value)) && (input && input.value !== null)) {
+        let elementKeys = Object.keys(input.value);
+        for (let key of elementKeys) {
+          flatFormResponse[`${formID}.${item.id}.${input.name}.${key}`] = input.value[key];
+        };
+      }
+
+    }
+  }
+  let data = await tangyModules.hook("csv_flatFormReponse", {flatFormResponse, formResponse});
+  return data.flatFormResponse;
+};
 
 function saveFormInfo(flatResponse, db) {
   return new Promise(async (resolve, reject) => {
