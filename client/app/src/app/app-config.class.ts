c9a30b7b91a4ff78812deb58b178ec9ae0e69f45
R.J. Steinert <rj@rjsteinert.com>
2019-03-23 05:10:56 -0400

    Have app-config service return an AppConfig data object

---
+++ client/app/src/app/app-config.class.ts
@@ -0,0 +1,24 @@
+
+export class AppConfig {
+  securityPolicy = ["password"]
+  columnsOnVisitsTab = []
+  securityQuestionText = "What is your year of birth?"
+  hashSecurityQuestionResponse = false
+  hideProfile = false
+  registrationRequiresServerUser = false
+  listUsernamesOnLoginScreen = true
+  serverUrl = "http://localhost/"
+  categories = []
+  uploadUnlockedFormReponses = false
+  groupName = ""
+  uploadToken = "change_this_token"
+  homeUrl = "case-management"
+  centrallyManagedUserProfile = false
+  languageDirection  = "ltr"
+  languageCode = "en"
+  // @TODO Deprecated...
+  uploadUrl = ''
+  syncProtocol = ''
+
+
+}
--- client/app/src/app/shared/_services/app-config.service.ts
+++ client/app/src/app/shared/_services/app-config.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AppConfig } from 'src/app/app-config.class';
 
 @Injectable()
 export class AppConfigService {
   constructor(
     private http: HttpClient
   ) { }
-  async getAppConfig() {
-    const res = await this.http.get('./assets/app-config.json').toPromise();
-    const appConfig:any = res;
-    return appConfig;
+  async getAppConfig():Promise<AppConfig> {
+    return <AppConfig>await this.http.get('./assets/app-config.json').toPromise();
   }
   public async getDefaultURL() {
     const result:any = await this.getAppConfig();
