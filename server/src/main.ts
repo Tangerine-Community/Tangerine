b359e3d22e042c04f546b70e54f9916b87f21ec3
R.J. Steinert <rj@rjsteinert.com>
2019-04-01 10:50:38 -0400

    Fix paths in express app for new location in src, new test.sh command, change default nest app route to not claim the root

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+const expressAppBootstrap = require('./express-app');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const httpAdapter = app.getHttpAdapter();
+  const expressInstance = httpAdapter.getInstance()
+  await expressAppBootstrap(expressInstance)
+  await app.listen(80);
 }
 bootstrap();
