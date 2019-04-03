05530c1951302901901fa06254970c8340192395
R.J. Steinert <rj@rjsteinert.com>
2019-04-03 15:18:41 -0400

    Implement new nest API and _id/label awareness in editor. Also improve language on PWA release

--- editor/src/app/groups/new-group/new-group.component.ts
+++ editor/src/app/groups/new-group/new-group.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GroupsService } from '../services/groups.service';
 import { _TRANSLATE } from '../../shared/_services/translation-marker';
 import { TangyErrorHandler } from '../../shared/_services/tangy-error-handler.service';
+import { WindowRef } from '../../../app/core/window-ref.service';
 
 @Component({
   selector: 'app-new-group',
@@ -11,7 +12,8 @@ import { TangyErrorHandler } from '../../shared/_services/tangy-error-handler.se
 export class NewGroupComponent implements OnInit {
 
   groupName = '';
-  constructor(private groupsService: GroupsService, private errorHandler: TangyErrorHandler) { }
+  constructor(private groupsService: GroupsService, private errorHandler: TangyErrorHandler,
+    private window:WindowRef) { }
 
   ngOnInit() {
 
@@ -19,7 +21,9 @@ export class NewGroupComponent implements OnInit {
 
   async createGroup() {
     try {
-      const result: any = await this.groupsService.createGroup(this.groupName.toLowerCase().replace(' ', '-').replace(/[^a-z]+|\s+/gmi, ''));
+      const result: any = await this.groupsService.createGroup(this.groupName);
+      this.window.nativeWindow.location = `${this.window.nativeWindow.location.origin}/app/${result._id}/index.html#/groups/${result._id}`
+
       if (result && result.statusCode && result.statusCode === 200) {
         this.groupName = '';
         this.errorHandler.handleError(_TRANSLATE('Group Created Succesfully'));
--- editor/src/app/groups/release-pwa/release-pwa.component.ts
+++ editor/src/app/groups/release-pwa/release-pwa.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { GroupsService } from '../services/groups.service';
 import { TangyErrorHandler } from '../../shared/_services/tangy-error-handler.service';
 import { _TRANSLATE } from '../../shared/_services/translation-marker';
+import { WindowRef } from 'src/app/core/window-ref.service';
 
 @Component({
   selector: 'app-release-pwa',
@@ -15,10 +16,13 @@ export class ReleasePwaComponent implements OnInit {
   groupName = '';
   releaseType = '';
   errorGeneratingPWA;
+  pwaUrl
   constructor(
     private route: ActivatedRoute,
     private groupsService: GroupsService,
-    private errorHandler: TangyErrorHandler) { }
+    private errorHandler: TangyErrorHandler,
+    private windowRef: WindowRef 
+  ) { }
 
   async ngOnInit() {
     this.route.params.subscribe(params => {
@@ -31,6 +35,8 @@ export class ReleasePwaComponent implements OnInit {
   async releasePWA() {
     try {
       const result: any = await this.groupsService.releasePWA(this.groupName, this.releaseType);
+      this.pwaUrl = `${this.windowRef.nativeWindow.location.origin}/releases/${this.releaseType}/pwas/${this.groupName}`
+
       this.buildPwaIsComplete = result.statusCode === 200;
     } catch (error) {
       this.errorGeneratingPWA = true;
