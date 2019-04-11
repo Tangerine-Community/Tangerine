133acb1d2bc39dd0b02b687f06dba21e207df42d
R.J. Steinert <rj@rjsteinert.com>
2019-04-11 00:29:26 -0400

    Add share module to server user controller spec

--- server/src/core/user/user.controller.spec.ts
+++ server/src/core/user/user.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { SharedModule } from '../../shared/shared.module';
 
 describe('User Controller', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [SharedModule],
       controllers: [UserController],
     }).compile();
 
