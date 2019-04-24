21c46d1749729c0f6121c09f360e054b537d894a
R.J. Steinert <rj@rjsteinert.com>
2019-04-24 16:38:53 -0400

    Refactor SyncService on server to look for user's location and return a sync selector that uses it

--- server/src/modules/sync/services/sync-session/sync-session.service.spec.ts
+++ server/src/modules/sync/services/sync-session/sync-session.service.spec.ts
@@ -6,6 +6,7 @@ import { ClientUserService } from '../../../../shared/services/client-user/clien
 import { HttpModule, HttpService } from '@nestjs/common';
 import { TangerineConfigService } from '../../../../shared/services/tangerine-config/tangerine-config.service';
 import { of } from 'rxjs';
+import { DbService } from '../../../../shared/services/db/db.service';
 
 class MockGroupService {
   read(groupId:string) {
@@ -41,11 +42,17 @@ class MockConfigService {
   }
 }
 
-/* TODO: When it's time to work on location filtering we'll need this.
 class MockPouchDB {
+  constructor(userProfileId:string) {
+    
+  }
   get(_id:string) {
     return {
-      _id
+      _id,
+      location: {
+        "county": "a",
+        "facility": "1"
+      }
     }
   }
 }
@@ -54,7 +61,7 @@ class MockDbService {
     return new MockPouchDB(userProfileId)
   }
 }
-*/
+
 describe('SyncSessionService', () => {
   let service: SyncSessionService;
 
@@ -79,6 +86,10 @@ describe('SyncSessionService', () => {
           {
             provide: ClientUserService,
             useClass: MockClientUserService
+          },
+          {
+            provide: DbService,
+            useClass: MockDbService
           }
         ]
       }).compile();
@@ -95,13 +106,16 @@ describe('SyncSessionService', () => {
 
   it('should start a sync session', async () => {
     const response = await service.start('group1', 'profile1')
+    debugger
     const shouldRespondWith = {
       "pouchDbSyncUrl": "https://syncUser-ac15bf36-f9ea-4cbc-8761-48c3ebe968e0:6e99e4a5-15ac-488f-8c15-dd3633a77d72@localhost/db/group1",
       "pouchDbSyncOptions": {
         "selector": {
           "$or": [
             {
-              "form.id": "example"
+              "form.id": "example",
+              "location.county": "a",
+              "location.facility": "1"
             },
             {
               "_id": "profile1"
@@ -116,6 +130,8 @@ describe('SyncSessionService', () => {
     // Can't expect this to be the same, random UUID used in formation of url.
     // expect(response.pouchDbSyncUrl).toBe(shouldRespondWith.pouchDbSyncUrl)
     expect(response.pouchDbSyncOptions.selector['$or'][0]['form.id']).toBe(shouldRespondWith.pouchDbSyncOptions.selector['$or'][0]['form.id'])
+    expect(response.pouchDbSyncOptions.selector['$or'][0]['location.county']).toBe(shouldRespondWith.pouchDbSyncOptions.selector['$or'][0]['location.county'])
+    expect(response.pouchDbSyncOptions.selector['$or'][0]['location.facility']).toBe(shouldRespondWith.pouchDbSyncOptions.selector['$or'][0]['location.facility'])
     expect(response.pouchDbSyncOptions.selector['$or'][1]['_id']).toBe(shouldRespondWith.pouchDbSyncOptions.selector['$or'][1]['_id'])
     expect(response.formIdsToNotPush[0]).toBe(shouldRespondWith.formIdsToNotPush[0])
   })
--- server/src/modules/sync/services/sync-session/sync-session.service.ts
+++ server/src/modules/sync/services/sync-session/sync-session.service.ts
@@ -25,9 +25,7 @@ export class SyncSessionService {
   
   async start(groupId:string, profileId:string):Promise<SyncSession> {
     try {
-      const groupDb = this.dbService.instantiate(groupId)
-      // @TODO Use this clientUser to limit the selector by location.
-      // const clientUser = await groupDb.get(profileId) 
+
       // Create sync user
       const syncUsername = `syncUser-${UUID()}`
       const syncPassword = UUID()
@@ -40,15 +38,27 @@ export class SyncSessionService {
         "password": syncPassword 
       }
       await this.http.post(`${config.couchdbEndpoint}/_users`, syncUserDoc).toPromise()
+      // Get the formIds that we'll use to build the sync selector.
       const formIds = (await this.groupConfig.read(groupId)).config.sync.formIds
       log.info(`Created sync session for user ${profileId} in group ${groupId}`)
+      // Build the location based part of the sync selector.
+      const groupDb = this.dbService.instantiate(groupId)
+      const userProfile = await groupDb.get(profileId) 
+      const userProfileDefinedSyncLocation = {}
+      for (const locationLevel in userProfile.location) {
+        userProfileDefinedSyncLocation[`location.${locationLevel}`] = userProfile.location[locationLevel]
+      }
+      // Template and return the full sync session.
       return <SyncSession>{
         pouchDbSyncUrl: `${config.protocol}://${syncUsername}:${syncPassword}@${config.hostName}/db/${groupId}`,
         pouchDbSyncOptions: {
           selector: {
             "$or": [
               ...formIds.map(formId => {
-                return { "form.id": formId }
+                return { 
+                  "form.id": formId,
+                  ...userProfileDefinedSyncLocation
+                }
               }),
               {
                 "_id": profileId
