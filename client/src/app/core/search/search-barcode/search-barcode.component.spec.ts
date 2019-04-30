7f927c1905a4dda4677831785f8007c7972f3c93
R.J. Steinert <rj@rjsteinert.com>
2019-04-30 11:27:27 -0400

    Add search-barcode component

---
+++ client/src/app/core/search/search-barcode/search-barcode.component.spec.ts
@@ -0,0 +1,87 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchBarcodeComponent } from './search-barcode.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AppConfigService } from 'src/app/shared/_services/app-config.service';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+class MockAppConfigService {
+  async getAppConfig() {
+    return {
+      barcodeSearchMapFunction: 'if ((JSON.parse(data)).foo) { return (JSON.parse(data)).foo } else { throw "Incorrect format"} '
+    }
+  }
+}
+
+describe('SearchBarcodeComponent', () => {
+  let component: SearchBarcodeComponent;
+  let fixture: ComponentFixture<SearchBarcodeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: AppConfigService,
+          useClass: MockAppConfigService 
+        }
+      ],
+      declarations: [ SearchBarcodeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchBarcodeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  fit('should start scan', () => {
+    component.startScanning()
+    fixture.detectChanges();
+    expect(component.state).toBe('STATE_SCANNING')
+  })
+
+  fit('should cancel', (done) => {
+    component.startScanning()
+    fixture.detectChanges()
+    component.cancel.addListener('cancel', event => {
+      expect(component.state).toBe('STATE_READY')
+      done()
+    })
+    setTimeout(() => {
+      fixture.nativeElement.querySelector('tangy-qr').stopScanning()
+    }, 300)
+  })
+
+  fit('should scan and parse successfully', (done) => {
+    fixture.nativeElement.querySelector('tangy-qr').value = '{"foo": "bar"}'
+    component.change.addListener('change', event => {
+      expect(component.value).toBe('bar')
+      done()
+    })
+    setTimeout(() => {
+      fixture.nativeElement.querySelector('tangy-qr').dispatchEvent(new Event('change'))
+    }, 300)
+  });
+
+  fit('should scan and parse unsuccessfully', (done) => {
+    fixture.nativeElement.querySelector('tangy-qr').value = '{"differentFormat": "bar"}'
+    component.error.addListener('error', event => {
+      expect(component.state).toEqual('STATE_ERROR')
+      done()
+    })
+    setTimeout(() => {
+      fixture.nativeElement.querySelector('tangy-qr').dispatchEvent(new Event('change'))
+    }, 300)
+  });
+
+
+});
--- client/src/app/shared/_classes/app-config.class.ts
+++ client/src/app/shared/_classes/app-config.class.ts
@@ -19,4 +19,5 @@ export class AppConfig {
   syncProtocol = ''
   minimumFreeSpace
   usageCleanupBatchSize
+  barcodeSearchMapFunction:string
 } 
