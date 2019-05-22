d7ef066363aa03f85dedaa58f3d27da0a9fb8af0
R.J. Steinert <rj@rjsteinert.com>
2019-05-22 14:29:50 -0400

    Add missing dependencies for new tests

--- client/src/app/case/services/case-definitions.service.spec.ts
+++ client/src/app/case/services/case-definitions.service.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CaseDefinitionsService } from './case-definitions.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('CaseDefinitionsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({imports: [ HttpClientModule ]}));
 
   it('should be created', () => {
     const service: CaseDefinitionsService = TestBed.get(CaseDefinitionsService);
--- client/src/app/tangy-forms/tangy-form.service.spec.ts
+++ client/src/app/tangy-forms/tangy-form.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TangyFormService } from './tangy-form.service';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
 
 describe('TangyFormService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({imports: [HttpClientModule, SharedModule]}));
 
   it('should be created', () => {
     const service: TangyFormService = TestBed.get(TangyFormService);
