d2ad2cbd062dbdda8c7b3450e00ba4d9bcc08b88
R.J. Steinert <rj@rjsteinert.com>
2019-05-23 16:23:05 -0400

    Refactor schedule data model for CaseEvent for simplicity

--- client/src/app/case/classes/case-event.class.ts
+++ client/src/app/case/classes/case-event.class.ts
@@ -2,36 +2,15 @@ import { EventForm } from './event-form.class'
 
 class CaseEvent {
   id?:string;
-  complete:boolean = false
-  name:string;
   caseEventDefinitionId:string; 
-  eventForms?:Array<EventForm> = [];
-  scheduledDate:number
-  startDate:number
-  estimatedWindowStart:number
-  estimatedWindowEnd:number
-  constructor(
-    id,
-    complete = false,
-    name,
-    caseEventDefinitionId,
-    estimatedWindowStart,
-    estimatedWindowEnd,
-    eventForms?,
-    startDate?
-  ) {
-    this.id = id
-    this.name = name
-    this.complete = complete
-    this.caseEventDefinitionId = caseEventDefinitionId 
-    this.estimatedWindowEnd = estimatedWindowEnd
-    this.estimatedWindowStart = estimatedWindowStart
-    this.eventForms = eventForms
-      ? eventForms
-      : this.eventForms
-    this.startDate = startDate
-      ? startDate
-      : Date.now()
+  complete:boolean = false
+  eventForms:Array<EventForm> = [];
+  estimate = true
+  dateStart:number
+  dateEnd:number
+
+  constructor() {
+
   }
 }
 
