026e3fe0ec70a026039890792fb870877085e16e
R.J. Steinert <rj@rjsteinert.com>
2019-06-05 15:10:30 -0400

    Make sure user service uses getUserAccount, not expecting the ID of an account doc to be the username

--- client/src/app/shared/_classes/user-account.class.ts
+++ client/src/app/shared/_classes/user-account.class.ts
@@ -1,10 +1,12 @@
 export class UserAccount {
   _id:string
+  username:string
   password:string
   securityQuestionResponse:string
   userUUID:string
   constructor(_id:string, password:string, securityQuestionResponse:string, userUUID:string) {
     this._id = _id 
+    this.username = _id 
     this.password = password
     this.securityQuestionResponse = securityQuestionResponse
     this.userUUID = userUUID
