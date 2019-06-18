ef1ede4c35e35c0ed7c452de88f018437f0dbc97
R.J. Steinert <rj@rjsteinert.com>
2019-06-18 14:13:04 -0400

    Refactor breadcrumb to use new design

--- client/src/app/case/components/case-breadcrumb/case-breadcrumb.component.ts
+++ client/src/app/case/components/case-breadcrumb/case-breadcrumb.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CaseService } from '../../services/case.service'
 import { t } from 'tangy-form/util/t.js'
-
-class Crumb {
-  url:string
-  label:string
-}
+import { WindowRef } from 'src/app/shared/_services/window-ref.service';
 
 @Component({
   selector: 'app-case-breadcrumb',
@@ -17,35 +13,36 @@ export class CaseBreadcrumbComponent implements OnInit {
   @Input() caseId:string
   @Input() caseEventId:string
   @Input() eventFormId:string
-  crumbs:Array<Crumb> = []
+  secondaryText = ''
+  secondaryLink = ''
 
-  constructor() { }
+  constructor(
+    private caseService: CaseService,
+    private windowRef:WindowRef
+  ) { }
 
   ngOnInit() {
-    if (this.caseId) {
-      
-      this.crumbs = [
-        ...this.crumbs,
-        <Crumb>{
-          url: `/case/${this.caseId}`,
-          label: `${t('Case')} ${this.caseId.substr(0,6)}`,
-          icon: 'folder'
-        }
-      ]
-    }
     if (this.caseEventId) {
-      this.crumbs.push(<Crumb>{
-        url: `/case/event/${this.caseId}/${this.caseEventId}`,
-        label: `${t('Event')} ${this.caseEventId.substr(0,6)}`,
-        icon: 'event'
-      })
-    }
-    if (this.eventFormId) {
-      this.crumbs.push(<Crumb>{
-        url: `/case/event/form/${this.caseId}/${this.caseEventId}/${this.eventFormId}`,
-        label: `${t('Form')} ${this.eventFormId.substr(0,6)}`,
-        icon: 'assignment'
-      })
+      const caseEvent = this
+        .caseService
+        .case
+        .events
+        .find(caseEvent => caseEvent.id === this.caseEventId)
+      const caseEventDefinition = this
+        .caseService
+        .caseDefinition
+        .eventDefinitions
+        .find(caseEventDefinition => caseEventDefinition.id === caseEvent.caseEventDefinitionId)
+      if (!this.eventFormId) {
+        this.secondaryText = caseEventDefinition.name
+        this.secondaryLink = `/case/${this.caseService.case._id}`
+      } else {
+        const eventForm = caseEvent.eventForms.find(eventForm => eventForm.id === this.eventFormId)
+        const eventFormDefinition = caseEventDefinition.eventFormDefinitions.find(eventFormDefinition => eventFormDefinition.id === eventForm.eventFormDefinitionId)
+        this.secondaryText = eventFormDefinition.name
+        this.secondaryLink = `/case/event/${this.caseService.case._id}/${caseEvent.id}`
+      }
+
     }
   }
 
