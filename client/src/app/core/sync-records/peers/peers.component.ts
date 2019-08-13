533da08dc8b62ebdfcd936bc5d6bbefd125205ef
Chris E. Kelley <ckelley@rti.org>
2019-08-13 12:08:34 +0200

    Since JSONObject is being passed by Cordova, don't need JSON.parse.
    added listenForTransfer

--- client/src/app/core/sync-records/peers/peers.component.ts
+++ client/src/app/core/sync-records/peers/peers.component.ts
@@ -7,7 +7,8 @@ import {WindowRef} from '../../../shared/_services/window-ref.service';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import PouchDB from 'pouchdb';
-import {UserService} from "../../../shared/_services/user.service";
+import {UserService} from '../../../shared/_services/user.service';
+import {Message} from './message';
 
 @Component({
   selector: 'app-peers',
@@ -84,30 +85,15 @@ export class PeersComponent implements OnInit {
     }
   }
 
-  async discoverPeers() {
-   await this.peersService.discoverPeers()
-     .subscribe(peers => {
-       this.peers = peers;
-       this.getPeers();
-     });
-  }
 
   async getTangyP2PPermissions() {
     if (this.window.isCordovaApp) {
       window['TangyP2PPlugin'].getPermission(null, function(message) {
-        console.log('Message from getTangyP2PPermissions: ' + message);
-      }, function(err) {
-        console.log('TangyP2P error:: ' + err);
-      });
-    }
-  }
-
-  async startAdvertising() {
-    if (this.window.isCordovaApp) {
-      window['TangyP2PPlugin'].startAdvertising(null, function(message) {
-        try {
-          const messageJson = JSON.parse(message)
-        } catch (e) {
+        const objectConstructor = ({}).constructor;
+        if (message.constructor === objectConstructor) {
+          const messageStr = message.message;
+          document.querySelector('#p2p-results').innerHTML += messageStr + '<br/>';
+        } else {
           console.log('Message: ' + message);
           document.querySelector('#p2p-results').innerHTML += message + '<br/>';
         }
@@ -117,25 +103,13 @@ export class PeersComponent implements OnInit {
     }
   }
 
-  async listenForTransfer() {
+  async startAdvertising() {
     if (this.window.isCordovaApp) {
-      window['TangyP2PPlugin'].listenForTransfer(null, (message) => {
-        if (message.startsWith('{"version":')) {
-          PouchDB.plugin(window['PouchReplicationStream'].plugin);
-          PouchDB.adapter('writableStream', window['PouchReplicationStream'].adapters.writableStream);
-          // const stream = new window['Memorystream'](message);
-          const writeStream =  new window['Memorystream']
-          writeStream.end(message);
-          const username = this.userService.getCurrentUser()
-          const dest = new PouchDB(username);
-          const pluginMessage = 'I loaded data from the peer device.'
-          dest.load(writeStream).then(function () {
-            document.querySelector('#p2p-results').innerHTML += pluginMessage + '<br/>';
-          }).catch(function (err) {
-            message = 'oh no an error: ' + err
-            console.log(message);
-            document.querySelector('#p2p-results').innerHTML += message + '<br/>';
-          });
+      window['TangyP2PPlugin'].startAdvertising(null, function(message) {
+        const objectConstructor = ({}).constructor;
+        if (message.constructor === objectConstructor) {
+          const messageStr = message.message;
+          document.querySelector('#p2p-results').innerHTML += messageStr + '<br/>';
         } else {
           console.log('Message: ' + message);
           document.querySelector('#p2p-results').innerHTML += message + '<br/>';
@@ -149,8 +123,14 @@ export class PeersComponent implements OnInit {
   async startDiscovery() {
     if (this.window.isCordovaApp) {
       window['TangyP2PPlugin'].startDiscovery(null, function(message) {
-        console.log('Message: ' + message);
-        document.querySelector('#p2p-results').innerHTML += message + '<br/>';
+        const objectConstructor = ({}).constructor;
+        if (message.constructor === objectConstructor) {
+          const messageStr = message.message;
+          document.querySelector('#p2p-results').innerHTML += messageStr + '<br/>';
+        } else {
+          console.log('Message: ' + message);
+          document.querySelector('#p2p-results').innerHTML += message + '<br/>';
+        }
       }, function(err) {
         console.log('TangyP2P error:: ' + err);
       });
@@ -166,22 +146,63 @@ export class PeersComponent implements OnInit {
       stream.on('data', function(chunk) {
         dumpedString += chunk.toString();
       });
-      const username = this.userService.getCurrentUser()
+      const username = this.userService.getCurrentUser();
       const source = new PouchDB(username);
       source.dump(stream).then(function () {
         // console.log('Yay, I have a dumpedString: ' + dumpedString);
         window['TangyP2PPlugin'].transferData(dumpedString, function(message) {
-          console.log('Message: ' + message);
-          document.querySelector('#p2p-results').innerHTML += message + '<br/>';
+          const objectConstructor = ({}).constructor;
+          if (message.constructor === objectConstructor) {
+            const messageStr = message.message;
+            document.querySelector('#p2p-results').innerHTML += messageStr + '<br/>';
+          } else {
+            console.log('Message: ' + message);
+            document.querySelector('#p2p-results').innerHTML += message + '<br/>';
+          }
         }, function(err) {
           console.log('TangyP2P error:: ' + err);
         });
       }).catch(function (err) {
         console.log('oh no an error', err);
       });
-
-
     }
   }
 
+  async listenForTransfer() {
+    if (this.window.isCordovaApp) {
+      window['TangyP2PPlugin'].listenForTransfer(null, (message) => {
+        const objectConstructor = ({}).constructor;
+        // test if it's an object:
+        if (message.constructor === objectConstructor) {
+          const messageStr = message.message;
+          // TODO: JSONObject is available if we need it.
+          // const object = message.object;
+          if (message.messageType === 'payload') {
+            PouchDB.plugin(window['PouchReplicationStream'].plugin);
+            PouchDB.adapter('writableStream', window['PouchReplicationStream'].adapters.writableStream);
+            const writeStream =  new window['Memorystream'];
+            writeStream.end(messageStr);
+            const username = this.userService.getCurrentUser();
+            const dest = new PouchDB(username);
+            const pluginMessage = 'I loaded data from the peer device.';
+            dest.load(writeStream).then(function () {
+              document.querySelector('#p2p-results').innerHTML += pluginMessage + '<br/>';
+            }).catch(function (err) {
+              message = 'oh no an error: ' + err;
+              console.log(message);
+              document.querySelector('#p2p-results').innerHTML += message + '<br/>';
+            });
+          } else {
+            console.log('Message: ' + messageStr);
+            document.querySelector('#p2p-results').innerHTML += messageStr + '<br/>';
+          }
+        } else {
+          console.log('Message: ' + message);
+          document.querySelector('#p2p-results').innerHTML += message + '<br/>';
+        }
+      }, function(err) {
+        console.log('TangyP2P error:: ' + err);
+      });
+    }
+  }
 }
