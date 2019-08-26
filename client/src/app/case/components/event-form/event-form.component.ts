82f4b784a58acae04342ccbc040db608f18f5e69
R.J. Steinert <rj@rjsteinert.com>
2019-08-26 18:18:25 -0400

    Ensure last response seen is saved in throttled save for EventFormComponent
    (cherry picked from commit 8c289df3865af115715115e9b7f933e0b4cde753)

--- client/src/app/case/components/event-form/event-form.component.ts
+++ client/src/app/case/components/event-form/event-form.component.ts
@@ -26,6 +26,7 @@ export class EventFormComponent implements AfterContentInit {
   throttledSaveFiring:boolean;
   formResponse:any
   loaded = false
+  lastResponseSeen:any
   @ViewChild('container') container: ElementRef;
   constructor(
     private route: ActivatedRoute,
@@ -84,17 +85,22 @@ export class EventFormComponent implements AfterContentInit {
   // Prevent parallel saves which leads to race conditions. Only save the first and then last state of the store.
   // Everything else in between we can ignore.
   async throttledSaveResponse(response) {
+    this.lastResponseSeen = response
     // If already loaded, return.
-    if (this.throttledSaveLoaded) return
+    if (this.throttledSaveLoaded) {
+      return
+    }
     // Throttle this fire by waiting until last fire is done.
     if (this.throttledSaveFiring) {
       this.throttledSaveLoaded = true
-      while (this.throttledSaveFiring) await sleep(200)
+      while (this.throttledSaveFiring) {
+        await sleep(200)
+      }
       this.throttledSaveLoaded = false
     }
     // Fire it.
     this.throttledSaveFiring = true
-    await this.saveResponse(response)
+    await this.saveResponse(this.lastResponseSeen)
     this.throttledSaveFiring = false
   }
 
