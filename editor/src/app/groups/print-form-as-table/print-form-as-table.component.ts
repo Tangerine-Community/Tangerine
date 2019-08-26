704dda03aa3de2e42c50f85409a0a36cd6bcae19
Evans D <evansdianga@gmail.com>
2019-08-26 11:22:03 +0300

    Add componnent for printing form details as rtable

---
+++ editor/src/app/groups/print-form-as-table/print-form-as-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-print-form-as-table',
+  templateUrl: './print-form-as-table.component.html',
+  styleUrls: ['./print-form-as-table.component.css']
+})
+export class PrintFormAsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
