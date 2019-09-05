a6dba89e9cc1b27842f2947ffc10f0743099eb45
Kevin Wilson <mail@kevinandjuliewilson.com>
2019-09-05 11:25:27 -0400

    Completion of initial query migration into case.

--- client/src/app/case/components/query/query.component.ts
+++ client/src/app/case/components/query/query.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { Query } from '../../classes/query.class';
+import { CaseService } from '../../services/case.service';
+
 
 @Component({
   selector: 'app-query',
   templateUrl: './query.component.html',
   styleUrls: ['./query.component.css']
 })
+
 export class QueryComponent implements OnInit {
+  queries: Array<Query>
+  constructor(
+    private caseService: CaseService
+  ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    this.queries = await this.caseService.getQueries();
   }
 
 }
