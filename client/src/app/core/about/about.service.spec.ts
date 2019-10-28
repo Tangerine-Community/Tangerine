efcc20750d88afae5125bb83854176a2616f90e2
R.J. Steinert <rj@rjsteinert.com>
2019-10-28 10:36:55 -0400

    Fix client tests

--- client/src/app/core/about/about.service.spec.ts
+++ client/src/app/core/about/about.service.spec.ts
@@ -1,9 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { AboutService } from './about.service';
 
 describe('AboutService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({ imports: [ HttpClientModule ]}));
 
   it('should be created', () => {
     const service: AboutService = TestBed.get(AboutService);
--- client/src/app/core/about/about/about.component.spec.ts
+++ client/src/app/core/about/about/about.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
@@ -8,6 +9,7 @@ describe('AboutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ AboutComponent ]
     })
     .compileComponents();
