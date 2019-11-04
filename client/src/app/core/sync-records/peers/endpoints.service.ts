78a32db51e634939f200b4cb23254ce8b414e674
Chris E. Kelley <ckelley@rti.org>
2019-11-04 09:01:32 -0600

    updated docker-tangerine-base-image to v3.1.1
    removed unused Device

--- client/src/app/core/sync-records/peers/endpoints.service.ts
+++ client/src/app/core/sync-records/peers/endpoints.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {WindowRef} from '../../../shared/_services/window-ref.service';
-import {Device} from './device';
 import {Endpoint} from './endpoint';
 
 @Injectable({
@@ -11,7 +10,6 @@ export class EndpointsService {
 
   window: any;
   endpoints: Endpoint[];
-  device: Device;
 
   constructor(
     private windowRef: WindowRef
