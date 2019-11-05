5f38d22e5034c934fbff4a94bcef9db41ccfe092
Evans D <evansdianga@gmail.com>
2019-11-05 16:30:51 +0300

    Fixed tests

--- client/src/app/case/components/case-event-schedule-list/case-event-schedule-list.component.spec.ts
+++ client/src/app/case/components/case-event-schedule-list/case-event-schedule-list.component.spec.ts
@@ -47,10 +47,95 @@ class MockUserService {
 const REFERENCE_TIME = 1558715176000
 
 class MockCasesService {
-
-  async getEventsByDate (username:string, dateStart, dateEnd, excludeEstimates = false):Promise<Array<CaseEvent>> {
+  
+  async getEventsByDate(username: string, dateStart, dateEnd, excludeEstimates = false): Promise<Array<CaseEventInfo>> {
+    const caseDefinition= {
+      "id": "c1",
+      "formId": "mother-manifest-3a453b",
+      "name": "Pregnant Woman",
+      "description": "Select this case type to screen, consent and enroll a pregnant womans.",
+      "templateCaseTitle": "${getVariable('participant_id') ? `${getVariable('firstname')} ${getVariable('surname')}` : '...'}",
+      "templateCaseDescription": "${getVariable('participant_id') ? `<b>Participant ID</b>: ${getVariable('participant_id')} <b>Village</b>: ${getVariable('village')} <b>Head of household</b>: ${getVariable('headofhouse')}` : '...'}",
+      "templateScheduleListItemIcon": "${caseEventInfo.complete ? 'event_available' : 'event_note'}",
+      "templateScheduleListItemPrimary": "<span>${caseEventDefinition.name}</span>",
+      "templateScheduleListItemSecondary": "<span>${caseInstance.label}</span>",
+    
+      "templateCaseEventListItemIcon": "${caseEventInfo.complete ? 'event_available' : 'event_note'}",
+      "templateCaseEventListItemPrimary": "<span>${caseEventDefinition.name}</span>",
+      "templateCaseEventListItemSecondary": "${TRANSLATE('Scheduled')}: ${formatDate(caseEventInfo.dateStart,'dddd, MMMM Do YYYY, h:mm:ss a')}",
+      "templateEventFormListItemIcon": "",
+      "templateEventFormListItemPrimary": "<span>${eventFormDefinition.name}</span>",
+      "templateEventFormListItemSecondary": "Workflow: ${getValue('workflow_state')} Status: ${!eventForm.complete ? 'Incomplete' : 'Complete'}",
+      "startFormOnOpen": {
+        "eventId": "c1",
+        "eventFormId": "event-form-definition-ece26e"
+      },
+      "eventDefinitions": [
+        {
+          "id": "c1",
+          "name": "ANC-Enrollment",
+          "description": "",
+          "repeatable": false,
+          "estimatedTimeFromCaseOpening": 0,
+          "estimatedTimeWindow": 0,
+          "required": true,
+          "eventFormDefinitions": [
+            {
+              "id": "event-form-definition-ece26e",
+              "formId": "s0-participant-information-f254b9",
+              "name": "S01 - Screening and Enrollment",
+              "required": true,
+              "repeatable": false
+            },
+            {
+              "id": "event-form-definition-574497",
+              "formId": "s02-drug-administration-99380a",
+              "name": "S02 - AZ dosing",
+              "required": true,
+              "repeatable": false
+            },
+            {
+              "id": "event-form-definition-c94289",
+              "formId": "s03-demographic-characteristics-c09a84",
+              "name": "S03 - Demographic information",
+              "required": true,
+              "repeatable": false
+            },
+            {
+              "id": "event-form-definition-a5301b",
+              "formId": "s04-current-pregnancy-record-and-pregnancy-history-9858d6",
+              "name": "S04 - Maternal Clinical history",
+              "required": true,
+              "repeatable": false
+            },
+            {
+              "id": "event-form-definition-c58ef3",
+              "formId": "s05-maternal-physical-exam-1c983b",
+              "name": "S05 - Physical Exam",
+              "required": true,
+              "repeatable": false
+            },
+            {
+              "id": "event-form-definition-z830kj",
+              "formId": "s06-current-anc-visit-68fc78",
+              "name": "S06 - Current ANC visit",
+              "required": true,
+              "repeatable": false
+            },
+            {
+              "id": "event-form-definition-z452kj",
+              "formId": "s07-visit-information-82328c",
+              "name": "S07 - Visit Close Out",
+              "required": true,
+              "repeatable": false
+            }
+          ]
+        },
+        
+      ]
+    }
     return [
-      <CaseEvent>{
+      <CaseEventInfo>{
         id: 'e1',
         caseId: 'response1',
         caseEventDefinitionId: 'c1',
@@ -135,7 +220,7 @@ describe('CaseEventScheduleListComponent', () => {
     expect(component).toBeTruthy();
     component.didSearch$.subscribe((value) => {
       fixture.detectChanges();
-      expect(fixture.nativeElement.querySelectorAll('.search-result').length).toEqual(3)
+      expect(fixture.nativeElement.querySelectorAll('.event-info').length).toEqual(1)
       done()
     })
     component.date = REFERENCE_TIME
@@ -148,8 +233,8 @@ describe('CaseEventScheduleListComponent', () => {
       i++
       if (i === 2) {
         fixture.detectChanges()
-        expect(fixture.nativeElement.querySelectorAll('.search-result').length).toEqual(3)
-        expect(component.eventsInfo.length).toEqual(3)
+        expect(fixture.nativeElement.querySelectorAll('.event-info').length).toEqual(1)
+        expect(component.eventsInfo.length).toEqual(1)
         done()
       }
     })
