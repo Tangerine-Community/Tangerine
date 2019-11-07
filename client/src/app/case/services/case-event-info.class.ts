e7e553a46d6c571cd0e98742c153f98de9f90fcd
Evans D <evansdianga@gmail.com>
2019-11-07 11:15:08 +0300

    Chnage class export

--- client/src/app/case/services/case-event-info.class.ts
+++ client/src/app/case/services/case-event-info.class.ts
@@ -2,7 +2,11 @@ import { Case } from '../classes/case.class';
 import { CaseEvent } from '../classes/case-event.class';
 import { CaseDefinition } from '../classes/case-definition.class';
 
-export class CaseEventInfo extends CaseEvent {
+ class CaseEventInfo extends CaseEvent {
 caseInstance?:Case
 caseDefinition?:CaseDefinition
+constructor() {
+super()
 }
+}
+export {CaseEventInfo}
