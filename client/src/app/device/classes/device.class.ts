f31481d13313b6ad5dee4a3b3da7dc4d514ae3e6
R.J. Steinert <rj@rjsteinert.com>
2019-11-22 00:20:47 -0500

    Test and fix DeviceService

--- client/src/app/device/classes/device.class.ts
+++ client/src/app/device/classes/device.class.ts
@@ -1,7 +1,10 @@
 export class Device {
   _id:string
+  collection = 'Device'
   token:string
+  claimed:boolean
   syncLocations:Array<SyncLocation> = []
+  location:any
 }
 
 export interface SyncLocation {
