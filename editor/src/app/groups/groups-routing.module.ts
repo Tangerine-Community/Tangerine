4844d125d668e040405e8029cc52ac21049988bf
R.J. Steinert <rj@rjsteinert.com>
2019-12-19 06:50:01 -0500

    Fix location list editor by making groupName to groupId

--- editor/src/app/groups/groups-routing.module.ts
+++ editor/src/app/groups/groups-routing.module.ts
@@ -29,11 +29,11 @@ const groupsRoutes: Routes = [
   { path: 'groups/:groupName/addUser', component: AddUserComponent, canActivate: [LoginGuard] },
   { path: 'groups/:groupName/manage-location-list-levels', component: ManageLocationListLevelsComponent, canActivate: [LoginGuard] },
   {
-    path: 'groups/:groupName/import-location-list',
+    path: 'groups/:groupId/import-location-list',
     component: ImportLocationListComponent, canActivate: [LoginGuard, AdminUserGuard]
   },
   {
-    path: 'groups/:groupName/manage-location-list-metadata/:locationLevel',
+    path: 'groups/:groupId/manage-location-list-metadata/:locationLevel',
     component: ManageLocationListMetadataComponent, canActivate: [LoginGuard]
   },
   { path: 'groups/:groupName/download-csv/:formId', component: DownloadCsvComponent, canActivate: [LoginGuard] },
