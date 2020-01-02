9a5ed8c03be27226c2c0c41424b7b9d5cd5a38e1
Chris E. Kelley <ckelley@rti.org>
2020-01-01 21:50:46 -0600

    Added templates to support cases in report module
    increment docker-tangerine-base-image to v3.2.1

--- server/src/scripts/generate-uploads/bin.js
+++ server/src/scripts/generate-uploads/bin.js
@@ -12,7 +12,7 @@ const axios = require('axios')
 const uuidv1 = require('uuid/v1');
 const numberOfUploads = parseInt(process.argv[2])
 const groupName = process.argv[3];
-const url = `http://127.0.0.1/api/${process.argv[3]}/upload`
+const url = `http://localhost/api/${process.argv[3]}/upload`
 const delay = process.argv[4] ? parseInt(process.argv[4]) : 2000
 const defaultBatchSize =  numberOfUploads < 100 ? numberOfUploads : 100
 const batchSize = process.argv[5] ? parseInt(process.argv[5]) : defaultBatchSize
@@ -21,6 +21,7 @@ const alternativeTemplate = process.argv[6] ? process.argv[6] : ''
 const templateDocfilename = './template-' + alternativeTemplate + '-doc.js'
 const templateDoc = require(templateDocfilename).doc
 const userProfileTemplateDoc = require('./template-user-profile-doc.js').doc
+const form01ATemplateDoc = require('./template-case-form01A-doc.js').doc
 
 const sleep = (milliseconds) => {
   return new Promise((res, rej) => {
@@ -37,10 +38,10 @@ async function go() {
   while (numberOfUploadsCompleted < numberOfUploads) {
     let batchCount = 0
     let bodies = []
-    let profiles = []
+    let linkedRecords1 = []
     for(var i=0; i < batchSize; i++){
       let doc;
-      let userProfileDoc;
+      let linkedDoc1;
       if (alternativeTemplate === 'class') {
         let studentRegistration = {
           _id: uuidv1(),
@@ -55,6 +56,26 @@ async function go() {
           }
         }
         doc = Object.assign({}, templateDoc, studentRegistration)
+      } else if (alternativeTemplate === 'case-mother') {
+        let participantId = uuidv1()
+        let caseId = uuidv1();
+        let participant_id = Math.round(Math.random() * 1000000)
+        let caseMother = {
+          _id: caseId,
+          "participants": [{
+            "id": participantId,
+            "caseRoleId": "mother-role",
+            "data": {"firstname": "A", "surname": "Test", "participant_id": participant_id}
+          }],
+        }
+        console.log("motherId: " + caseId + " participantId: " + participantId + " participant_id: " + participant_id);
+        doc = Object.assign({}, templateDoc, caseMother);
+        form01ATemplateDoc.items[10].inputs[2].value = participant_id;
+        linkedDoc1 =  Object.assign({}, form01ATemplateDoc, {
+          _id: participantId,
+          caseId: caseId
+        });
+
       } else {
         let date = randomDate(new Date(2019, 0, 1), new Date())
         let d = new Date(date),
@@ -68,20 +89,25 @@ async function go() {
         doc = Object.assign({}, templateDoc, {
           _id: uuidv1()
         })
-        userProfileDoc = Object.assign({}, userProfileTemplateDoc, {
+        linkedDoc1 = Object.assign({}, userProfileTemplateDoc, {
           _id: userProfileId
         })
         // console.log("userProfileDoc: " + JSON.stringify(userProfileDoc))
       }
       let body = pako.deflate(JSON.stringify({ doc }), {to: 'string'})
       bodies.push(body)
-      let profile = pako.deflate(JSON.stringify({ doc: userProfileDoc }), {to: 'string'})
-      profiles.push(profile)
+      let linkedRecord = pako.deflate(JSON.stringify({ doc: linkedDoc1 }), {to: 'string'})
+      linkedRecords1.push(linkedRecord)
     }
     // Upload the profiles first
-    let batchProfiles = profiles.map((profile) => axios({method: 'post', url, data: `${profile}`, headers: { 'content-type': 'text/plain', 'Authorization': `${process.env.T_UPLOAD_TOKEN}` },}))
+    let batchlinkedRecords1 = linkedRecords1.map((linkedRecord) => axios({method: 'post', url, data: `${linkedRecord}`, headers: { 'content-type': 'text/plain', 'Authorization': `${process.env.T_UPLOAD_TOKEN}` },}))
     // now upload the others
-    let batch = bodies.map((body) => axios({method: 'post', url, data: `${body}`, headers: { 'content-type': 'text/plain', 'Authorization': `${process.env.T_UPLOAD_TOKEN}` },}))
+    let batch = bodies.map((body) => {
+      axios({method: 'post', url, data: `${body}`, headers: { 'content-type': 'text/plain', 'Authorization': `${process.env.T_UPLOAD_TOKEN}` },})
+          .then(function (response) {
+            // console.log(response);
+          })
+    })
     // Don't await, the world doesn't wait. We're trying to cause stress at the rate defined.
     // await Promise.all(batch)
     numberOfUploadsCompleted = numberOfUploadsCompleted + batchSize
