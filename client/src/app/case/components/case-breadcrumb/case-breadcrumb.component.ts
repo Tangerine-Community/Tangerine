a1afe9b130a681d6eaf63afcefb90da7aed1dfb5
R.J. Steinert <rj@rjsteinert.com>
2020-01-10 09:19:53 -0500

    Initial concept of adding participant context to breadcrumb

--- client/src/app/case/components/case-breadcrumb/case-breadcrumb.component.ts
+++ client/src/app/case/components/case-breadcrumb/case-breadcrumb.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CaseService } from '../../services/case.service'
 import { t } from 'tangy-form/util/t.js'
+import { CaseDefinition } from '../../classes/case-definition.class';
+import { translate } from '@polymer/polymer/lib/utils/path';
 
 @Component({
   selector: 'app-case-breadcrumb',
@@ -12,6 +14,7 @@ export class CaseBreadcrumbComponent implements OnInit {
   @Input() caseId:string
   @Input() caseEventId:string
   @Input() eventFormId:string
+  primaryText = ''
   secondaryText = ''
   secondaryLink = ''
 
@@ -32,11 +35,18 @@ export class CaseBreadcrumbComponent implements OnInit {
         .eventDefinitions
         .find(caseEventDefinition => caseEventDefinition.id === caseEvent.caseEventDefinitionId)
       if (!this.eventFormId) {
+        this.primaryText = this.caseService.caseDefinition.templateBreadcrumbText
+          ? eval(this.caseService.caseDefinition.templateBreadcrumbText)
+          : `${t('Case')}: ${this.caseService.case._id.substr(0,6)}`
         this.secondaryText = caseEventDefinition.name
         this.secondaryLink = `/case/${this.caseService.case._id}`
       } else {
+        const participant = this.caseService.case.participants.find(participant => participant.id === eventForm.participantId)
         const eventForm = caseEvent.eventForms.find(eventForm => eventForm.id === this.eventFormId)
         const eventFormDefinition = caseEventDefinition.eventFormDefinitions.find(eventFormDefinition => eventFormDefinition.id === eventForm.eventFormDefinitionId)
+        this.primaryText = this.caseService.caseDefinition.templateBreadcrumbText
+          ? eval(this.caseService.caseDefinition.templateBreadcrumbText)
+          : `${t('Case')}: ${this.caseService.case._id.substr(0,6)}`
         this.secondaryText = eventFormDefinition.name
         this.secondaryLink = `/case/event/${this.caseService.case._id}/${caseEvent.id}`
       }
