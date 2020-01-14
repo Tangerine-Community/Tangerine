f645c1d6ce1cb3670ab099bd7d985cd18cf3bbdb
R.J. Steinert <rj@rjsteinert.com>
2020-01-14 12:38:17 -0500

    Refactor about service into device service, show more device info on about page, and add release channel to APK and PWA builds

--- client/src/app/device/services/device.service.ts
+++ client/src/app/device/services/device.service.ts
@@ -1,4 +1,4 @@
-import { AboutService } from './../../core/about/about.service';
+import { Loc } from 'tangy-form/util/loc.js';
 import { Device } from './../classes/device.class';
 import PouchDB from 'pouchdb';
 import { AppConfigService } from './../../shared/_services/app-config.service';
@@ -13,6 +13,15 @@ class TangerineDeviceDoc {
   device:Device
 }
 
+export interface AppInfo {
+  serverUrl:string
+  groupName:string
+  groupId:string
+  buildChannel:string
+  buildId:string
+  assignedLocation:string
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -21,8 +30,7 @@ export class DeviceService {
   db:PouchDB 
 
   constructor(
-    private http:HttpClient,
-    private aboutService:AboutService,
+    private httpClient:HttpClient,
     private appConfigService:AppConfigService
   ) { 
     this.db = new PouchDB(TANGERINE_DEVICE_STORE)
@@ -42,7 +50,7 @@ export class DeviceService {
     const appConfig = await this.appConfigService.getAppConfig()
     const tangerineDeviceDoc = <TangerineDeviceDoc>await this.db.get(TANGERINE_DEVICE_DOC)
     const device = <Device>await this
-      .http
+      .httpClient
       .get(`${appConfig.serverUrl}group-device/register/${appConfig.groupId}/${id}/${token}`).toPromise() 
     await this.db.put({
       ...tangerineDeviceDoc,
@@ -70,7 +78,7 @@ export class DeviceService {
     const appConfig = await this.appConfigService.getAppConfig()
     const tangerineDeviceDoc = <TangerineDeviceDoc>await this.db.get(TANGERINE_DEVICE_DOC)
     const device = <Device>await this
-      .http
+      .httpClient
       .get(`${appConfig.serverUrl}group-device/info/${appConfig.groupId}/${tangerineDeviceDoc.device._id}/${tangerineDeviceDoc.device.token}`).toPromise() 
     await this.db.put({
       ...tangerineDeviceDoc,
@@ -82,18 +90,60 @@ export class DeviceService {
   async didUpdate():Promise<any> {
     const appConfig = await this.appConfigService.getAppConfig()
     const tangerineDeviceDoc = <TangerineDeviceDoc>await this.db.get(TANGERINE_DEVICE_DOC)
-    const version = await this.aboutService.getBuildNumber()
+    const version = await this.getBuildId()
     const device = <Device>await this
-      .http
+      .httpClient
       .get(`${appConfig.serverUrl}group-device/did-update/${appConfig.groupId}/${tangerineDeviceDoc.device._id}/${tangerineDeviceDoc.device.token}/${version}`).toPromise() 
   }
 
   async didSync():Promise<any> {
     const appConfig = await this.appConfigService.getAppConfig()
     const tangerineDeviceDoc = <TangerineDeviceDoc>await this.db.get(TANGERINE_DEVICE_DOC)
-    const version = await this.aboutService.getBuildNumber()
+    const version = await this.getBuildId()
     const device = <Device>await this
-      .http
+      .httpClient
       .get(`${appConfig.serverUrl}group-device/did-sync/${appConfig.groupId}/${tangerineDeviceDoc.device._id}/${tangerineDeviceDoc.device.token}/${version}`).toPromise() 
+  }
+
+  async getAppInfo() {
+    const appConfig = await this.appConfigService.getAppConfig()
+    const buildId = await this.getBuildId()
+    const buildChannel = await this.getBuildChannel()
+    const device = await this.getDevice()
+    const locationList = await this.httpClient.get('./assets/location-list.json').toPromise()
+    const flatLocationList = Loc.flatten(locationList)
+    const assignedLocation = device && device.assignedLocation && device.assignedLocation.value && Array.isArray(device.assignedLocation.value)
+      ? device.assignedLocation.value.map(value => ` ${value.level}: ${flatLocationList.locations.find(node => node.id === value.value).label}`).join(', ')
+      : 'N/A'
+    return <AppInfo>{
+      serverUrl: appConfig.serverUrl,
+      groupName: appConfig.groupName,
+      groupId: appConfig.groupId,
+      buildChannel,
+      buildId,
+      deviceId: device._id,
+      assignedLocation
+    }
+  }
+
+  async getBuildId() {
+    try {
+      return await this.httpClient.get('./assets/tangerine-build-id', {responseType: 'text'}).toPromise()
+    } catch (e) {
+      return 'N/A'
+    }
+  }
+
+  async getBuildChannel() {
+    try {
+      const raw = await this.httpClient.get('./assets/tangerine-build-channel', {responseType: 'text'}).toPromise()
+      return raw.includes('prod')
+        ? 'live'
+        : raw.includes('qa')
+          ? 'test'
+          : 'unknown'
+    } catch (e) {
+      return 'N/A'
+    }
   }
 }
