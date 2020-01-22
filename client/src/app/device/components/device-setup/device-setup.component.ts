7958aefa63737d295c5977f7c543042e4f88999a
R.J. Steinert <rj@rjsteinert.com>
2020-01-22 14:24:54 -0500

    Add setting device password to second step of device setup process

--- client/src/app/device/components/device-setup/device-setup.component.ts
+++ client/src/app/device/components/device-setup/device-setup.component.ts
@@ -1,3 +1,4 @@
+import { DevicePasswordComponent } from './../device-password/device-password.component';
 import { Router } from '@angular/router';
 import { DeviceLanguageComponent } from './../device-language/device-language.component';
 import { DeviceSyncComponent } from './../device-sync/device-sync.component';
@@ -7,6 +8,7 @@ import { LanguagesService } from './../../../shared/_services/languages.service'
 import { Component, OnInit, ViewChild, ElementRef, AfterContentInit } from '@angular/core';
 
 const STEP_LANGUAGE_SELECT = 'STEP_LANGUAGE_SELECT'
+const STEP_DEVICE_PASSWORD = 'STEP_DEVICE_PASSWORD'
 const STEP_DEVICE_REGISTRATION = 'STEP_DEVICE_REGISTRATION'
 const STEP_SYNC = 'STEP_SYNC'
 
@@ -22,6 +24,7 @@ export class DeviceSetupComponent implements OnInit {
   // Prevent reloads required by changing languagr during testing.
   testing = false
   @ViewChild('stepLanguageSelect') stepLanguageSelect:DeviceLanguageComponent
+  @ViewChild('stepDevicePassword') stepDevicePassword:DevicePasswordComponent
   @ViewChild('stepDeviceRegistration') stepDeviceRegistration:DeviceRegistrationComponent
   @ViewChild('stepDeviceSync') stepDeviceSync:DeviceSyncComponent
 
@@ -35,15 +38,18 @@ export class DeviceSetupComponent implements OnInit {
     if (!this.languagesService.userHasSetLanguage()) {
       this.step = STEP_LANGUAGE_SELECT
     } else {
-      this.step = STEP_DEVICE_REGISTRATION
+      this.step = STEP_DEVICE_PASSWORD
     }
     // Listen to when steps are done.
     this.stepLanguageSelect.done$.subscribe(value => {
       if (!this.testing) {
         window.location.href = window.location.href.replace(window.location.hash, 'index.html')
       } else {
-        this.step = STEP_DEVICE_REGISTRATION
+        this.step = STEP_DEVICE_PASSWORD
       }
+    })
+    this.stepDevicePassword.done$.subscribe(value => {
+      this.step = STEP_DEVICE_REGISTRATION
     })
     this.stepDeviceRegistration.done$.subscribe((value) => {
       if (value === true) {
