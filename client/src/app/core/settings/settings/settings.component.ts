3875247f04f60cc617e3a225f1ca28848b2b7f3a
Chris E. Kelley <ckelley@rti.org>
2020-02-18 08:14:37 -0600

    Disabled tab animation in Case home
    Removed the animation on tab transitions to reduce jerky behavior
    Removed some redundant text headers that were already in the tab headers.
    Fix issue in Settings caused by change to mat-select

--- client/src/app/core/settings/settings/settings.component.ts
+++ client/src/app/core/settings/settings/settings.component.ts
@@ -12,16 +12,18 @@ export class SettingsComponent implements OnInit {
   translations:any
   window:any
   languageCode = 'en'
+  selected = ''
   constructor(
     private http: HttpClient
-  ) { 
+  ) {
     this.languageCode = localStorage.getItem('languageCode')
   }
 
   async ngOnInit() {
+    this.selected = this.languageCode;
     this.translations = await this.http.get('./assets/translations.json').toPromise();
     this.form.nativeElement.querySelector('[type="submit"]').addEventListener('click', (event) => {
-      const selectedLanguage = this.translations.find(languageInfo => languageInfo.languageCode === this.form.nativeElement.querySelector('[name=translation]').value)
+      const selectedLanguage = this.translations.find(languageInfo => languageInfo.languageCode === this.selected)
       localStorage.setItem('languageCode', selectedLanguage.languageCode)
       localStorage.setItem('languageDirection', selectedLanguage.languageDirection)
       window.location.href = window.location.href.replace(window.location.hash, 'index.html')
