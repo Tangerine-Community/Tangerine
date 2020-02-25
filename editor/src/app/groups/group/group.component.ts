8e2c10506a8c44514165a7a5747a79f1f5c311d0
Evans D <evansdianga@gmail.com>
2020-02-25 13:51:14 +0300

    Added route guards to paths that require administrative rights, conditionally show parts that require admin access.

--- editor/src/app/groups/group/group.component.ts
+++ editor/src/app/groups/group/group.component.ts
@@ -2,6 +2,7 @@ import { Breadcrumb } from './../../shared/_components/breadcrumb/breadcrumb.com
 import { GroupsService } from './../services/groups.service';
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/core/auth/_services/user.service';
 
 
 @Component({
@@ -13,12 +14,15 @@ export class GroupComponent implements OnInit {
 
   title:string
   breadcrumbs:Array<Breadcrumb>
+  isAdminUser = false
 
   constructor(
-    private groupsService: GroupsService
+    private groupsService: GroupsService,
+    private userService:UserService
   ) {}
 
   async ngOnInit() {
+    this.isAdminUser = await this.userService.isCurrentUserAdmin()
     const group = await this.groupsService.getGroupInfo(window.location.hash.split('/')[2])
     this.title = group.label
     this.breadcrumbs = []
