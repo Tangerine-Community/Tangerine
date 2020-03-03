454a158883937a89b99427bd3c9f6fd3683fb30a
Chris E. Kelley <ckelley@rti.org>
2020-03-03 09:41:09 -0600

    Fixed lifecycle issue with adding user to role

--- editor/src/app/groups/add-users/add-user.component.ts
+++ editor/src/app/groups/add-users/add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {Component, OnInit, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GroupsService } from '../services/groups.service';
 import { fromEvent, of } from 'rxjs';
@@ -12,13 +12,15 @@ import {Breadcrumb} from '../../shared/_components/breadcrumb/breadcrumb.compone
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.css']
 })
-export class AddUserComponent implements OnInit, AfterContentInit {
+export class AddUserComponent implements OnInit, AfterContentInit, AfterViewInit
+{
   users;
   selectedUser;
   role;
   group;
   title = _TRANSLATE('Assign User Role')
   breadcrumbs: Array<Breadcrumb> = []
+  newUser = true;
   @ViewChild('search') search: ElementRef;
   constructor(
     private route: ActivatedRoute,
@@ -31,6 +33,37 @@ export class AddUserComponent implements OnInit, AfterContentInit {
     this.route.params.subscribe(params => {
       this.group = params.groupId;
     });
+  }
+
+  async ngAfterContentInit() {
+    this.route.params.subscribe(async params => {
+      this.selectedUser = params.username;
+      if (typeof this.selectedUser !== 'undefined') {
+        this.newUser = false;
+        await this.getUsers(this.selectedUser)
+        const groups = <Array<any>>await this.groupsService.getUserGroupRoles(this.selectedUser)
+        const currentGroup = groups.find(group => group.attributes.name === this.group);
+        const currentRole = currentGroup.attributes.role;
+        this.role = currentRole;
+      }
+      if (this.newUser) {
+        this.breadcrumbs = [];
+      } else {
+        this.breadcrumbs = [
+          <Breadcrumb>{
+            label: _TRANSLATE('Security'),
+            url: `security`
+          },
+          <Breadcrumb>{
+            label: _TRANSLATE('Assign Role'),
+            url: `security/role/${this.selectedUser}`
+          }
+        ]
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
     if (this.search) {
       fromEvent(this.search.nativeElement, 'keyup')
         .pipe(debounceTime(500))
@@ -38,28 +71,6 @@ export class AddUserComponent implements OnInit, AfterContentInit {
         .pipe(map(val => val['target'].value.trim()))
         .subscribe(async (value) => value ? await this.getUsers(value.trim()) : of([])); // Dont send request for empty username
     }
-  }
-
-  async ngAfterContentInit() {
-    this.route.params.subscribe(async params => {
-      this.selectedUser = params.username;
-
-      this.breadcrumbs = [
-        <Breadcrumb>{
-          label: _TRANSLATE('Security'),
-          url: `security`
-        },
-        <Breadcrumb>{
-          label: _TRANSLATE('Assign Role'),
-          url: `security/role/${this.selectedUser}`
-        }
-      ]
-      await this.getUsers(this.selectedUser)
-      const groups = <Array<any>>await this.groupsService.getUserGroupRoles(this.selectedUser)
-      const currentGroup = groups.find(group => group.attributes.name === this.group);
-      const currentRole = currentGroup.attributes.role;
-      this.role = currentRole;
-    })
   }
 
   async getUsers(username: string) {
