1446600291b789bfff3718d43dbc38be74338196
R.J. Steinert <rj@rjsteinert.com>
2020-03-06 17:03:42 -0500

    Refactor main.ts to be compatible for prod mode

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,24 +1,25 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import 'hammerjs';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-declare const window: any;
+//declare const window: any;
 
 if (environment.production) {
   enableProdMode();
 }
 
-if (window['isCordovaApp']) {
-  console.log('isCordovaApp')
-  document.addEventListener('deviceready', () => {
-    console.log('deviceready')
-    platformBrowserDynamic().bootstrapModule(AppModule)
-      .catch(err => console.log(err));
-  }, false);
+if (environment['isCordova']) {
+  let onDeviceReady = () => {
+      window.open = window['cordova'].InAppBrowser.open;
+      bootstrapFn();
+  };
+  document.addEventListener("deviceready", onDeviceReady, false);
 } else {
-  platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.log(err));
+  bootstrapFn();
+}
+
+function bootstrapFn() {
+platformBrowserDynamic().bootstrapModule(AppModule);
 }
