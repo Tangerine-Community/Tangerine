2aec0f1e0b3e901a3e2fbc5cf05be9157d90ade9
Chris E. Kelley <ckelley@rti.org>
2020-03-16 10:04:17 -0500

    reverted to earlier version of sqlitePlugin openDatabase

--- client/src/app/shared/_factories/db.factory.ts
+++ client/src/app/shared/_factories/db.factory.ts
@@ -14,22 +14,17 @@ PouchDB.defaults({auto_compaction: true, revs_limit: 1});
 export function DB(name, key = ''):PouchDB {
   let options = <any>{};
   if (window['isCordovaApp'] && window['sqlitePlugin']) {
-    // options = {
-    //   adapter: 'cordova-sqlite',
-    //   location: 'default',
-    //   androidDatabaseImplementation: 2
-    // };
     options = {
       adapter: 'cordova-sqlite',
-      location: 'default'
+      location: 'default',
+      androidDatabaseImplementation: 2
     };
-    // if (key) {
-    //   window['sqlitePlugin'].openDatabase({name, key, location: 'default', androidDatabaseImplementation: 2});
-    //   options.key = key
-    // } else {
-    //   window['sqlitePlugin'].openDatabase({name, location: 'default', androidDatabaseImplementation: 2});
-    // }
-    // window['sqlitePlugin'].openDatabase({name, location: 'default'});
+    if (key) {
+      window['sqlitePlugin'].openDatabase({name, key, location: 'default', androidDatabaseImplementation: 2});
+      options.key = key
+    } else {
+      window['sqlitePlugin'].openDatabase({name, location: 'default', androidDatabaseImplementation: 2});
+    }
   }
   return new PouchDB(name, options);
 }
