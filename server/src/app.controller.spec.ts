73aee53c33c7bb9ad88834e780db2d372e63bf8e
Chris E. Kelley <ckelley@rti.org>
2020-04-02 16:49:20 -0500

    Moved pouchdb-related imports to load earlier
    Disabled generation of es5 code.

--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -2,13 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
+import {HttpModule} from '@nestjs/common';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      imports: [SharedModule],
+      imports: [SharedModule, HttpModule],
       controllers: [AppController],
       providers: [AppService]
     }).compile();
