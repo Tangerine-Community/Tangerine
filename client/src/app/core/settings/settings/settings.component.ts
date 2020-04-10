e9b98e8fb612b08e9c829ceb25f05e7c5001bc55
R.J. Steinert <rj@rjsteinert.com>
2020-04-10 11:20:42 -0400

    #2044 Add text on settings page that indicates user will be required to login after submitting, also alert after they click submit that they will now be redirected to login

--- client/src/app/core/settings/settings/settings.component.ts
+++ client/src/app/core/settings/settings/settings.component.ts
@@ -1,3 +1,4 @@
+import { _TRANSLATE } from 'src/app/shared/translation-marker';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -27,6 +28,7 @@ export class SettingsComponent implements OnInit {
       const selectedLanguage = this.translations.find(languageInfo => languageInfo.languageCode === this.selected)
       localStorage.setItem('languageCode', selectedLanguage.languageCode)
       localStorage.setItem('languageDirection', selectedLanguage.languageDirection)
+      alert(_TRANSLATE('Settings have been updated. You will now be redirected to log in.'))
       window.location.href = window.location.href.replace(window.location.hash, 'index.html')
     })
   }
