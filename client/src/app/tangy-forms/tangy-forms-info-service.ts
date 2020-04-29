0ee85da0921b555a753b4dccfb2084df866028a3
R.J. Steinert <rj@rjsteinert.com>
2020-04-29 08:50:56 -0400

    Remove unused code form TangyForms module

--- client/src/app/tangy-forms/tangy-forms-info-service.ts
+++ client/src/app/tangy-forms/tangy-forms-info-service.ts
@@ -2,11 +2,6 @@ import { FormInfo } from './classes/form-info.class';
 import { Inject, Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
-// A dummy function so TS does not complain about our use of emit in our pouchdb queries.
-const emit = (key, value) => {
-  return true;
-}
-
 @Injectable({
   providedIn: 'root'
 })
