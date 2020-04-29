021866af30dfd54773a3bd0d47f28a7e7a111161
Evans D <evansdianga@gmail.com>
2020-04-29 14:27:20 +0300

    Refactor isauthenticated middleware to use JTWs to verify authentication ststus

--- server/src/middleware/is-authenticated.js
+++ server/src/middleware/is-authenticated.js
@@ -1,13 +1,21 @@
-const log = require('tangy-log').log
-
+const log = require('tangy-log').log;
+const { verifyJWT, decodeJWT } = require('../auth-utils');
 module.exports = function (req, res, next) {
-  // Uncomment next two lines when you want to turn off authentication during development.
-  // req.user = {}; req.user.name = 'user1';
-  // return next();
-  if (req.isAuthenticated()) {
-    return next();
+  const token = req.headers.authorization;
+  const errorMessage = `Permission denied at ${req.url}`;
+  if (token && verifyJWT(token)) {
+    const { username } = decodeJWT(token);
+    if (!username) {
+      log.warn(errorMessage);
+      res.status(401).send(errorMessage);
+    } else {
+      req.user= {}
+      req.user.name = username;
+      res.locals.username = username;
+      next();
+    }
+  } else {
+    log.warn(errorMessage);
+    res.status(401).send(errorMessage);
   }
-  let errorMessage = `Permission denied at ${req.url}`;
-  log.warn(errorMessage)
-  res.status(401).send(errorMessage)
-}
+};
