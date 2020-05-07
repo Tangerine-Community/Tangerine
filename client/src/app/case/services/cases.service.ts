ae74146ca07cb93168eeafa28ce94e437b097d4c
R.J. Steinert <rj@rjsteinert.com>
2020-05-07 13:58:09 -0400

    Refactor CasesService.getEventsByDate() to use case-events-by-all-days query

--- client/src/app/case/services/cases.service.ts
+++ client/src/app/case/services/cases.service.ts
@@ -1,8 +1,7 @@
-import { TangyFormService } from 'src/app/tangy-forms/tangy-form.service';
+import { UserService } from './../../shared/_services/user.service';
 import { Injectable } from '@angular/core';
-import { CaseEventInfo } from './case-event-info.class';
 import { CaseService } from './case.service';
-import * as moment from 'moment';
+import { CaseEvent } from '../classes/case-event.class';
 
 @Injectable({
   providedIn: 'root'
@@ -11,36 +10,19 @@ export class CasesService {
 
   constructor(
     private caseService: CaseService,
-    private tangyFormService:TangyFormService
+    private userService:UserService
   ) { }
 
-  async getEventsByDate(dateStart, dateEnd, excludeEstimates = false): Promise<Array<CaseEventInfo>> {
-    const allResponses = await this.tangyFormService.getAllResponses()
-    const docs = <Array<CaseEventInfo>>(allResponses)
-      .filter(doc => doc.collection === 'TangyFormResponse' && doc.type === 'case')
-      .reduce((acc, caseDoc) => [...acc, ...caseDoc.events
-        .map(event => {
-          return this.caseService.getCaseDefinitionByID(caseDoc.caseDefinitionId).then(caseDefinition => {
-            return { ...event, caseInstance: caseDoc, caseDefinition }
-          })
-        })], <Array<CaseEventInfo>>[]);
-    return Promise.all(docs).
-    then(data => data.filter(eventInfo => this.doesOverlap(dateStart, dateEnd, eventInfo) && !(excludeEstimates && eventInfo.estimate))
-      .sort(function (a, b) {
-        const dateA = new Date(a.occurredOnDay || a.scheduledDay || a.estimatedDay || a.windowStartDay).getTime()
-        const dateB = new Date(b.occurredOnDay || b.scheduledDay || b.estimatedDay || a.windowEndDay).getTime()
-        return dateA - dateB;
-      }))
+  async getEventsByDate(dateStart:string, dateEnd:string, excludeEstimates = false): Promise<Array<CaseEvent>> {
+    const db = await this.userService.getUserDatabase()
+    const queryResults = await db.query('case-events-by-all-days', { start_key: dateStart, end_key: dateEnd })
+    const caseEvents = []
+    for (let row of queryResults.rows) {
+      await this.caseService.load(row.value.caseId)
+      let caseEvent = this.caseService.case.events.find(event => event.id === row.value.eventId)
+      caseEvents.push(caseEvent)
+    }
+    return caseEvents
   }
 
-  private doesOverlap(dateStart, dateEnd, eventInfo: CaseEventInfo): boolean {
-    // Only show items on schedule view if the following dates are set on the event
-    if (!(eventInfo.scheduledDay || eventInfo.estimatedDay || eventInfo.windowStartDay)){
-      return false;
-    }
-    dateStart = moment(new Date(dateStart)).format('YYYY-MM-DD')
-    dateEnd = moment(new Date(dateEnd)).format('YYYY-MM-DD')
-    return moment(eventInfo.scheduledDay || eventInfo.estimatedDay || eventInfo.windowStartDay).isBetween(dateStart, dateEnd, 'days', '[]')
-    || moment(eventInfo.scheduledDay || eventInfo.estimatedDay ||eventInfo.windowEndDay).isBetween(dateStart, dateEnd, 'days', '[]')
-  }
 }
