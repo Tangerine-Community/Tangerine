f15f9918c9b6db7029bdd401199489605107e165
Evans D <evansdianga@gmail.com>
2020-05-19 11:28:42 +0300

    Rename parameter

--- server/src/middleware/is-authenticated.js
+++ server/src/middleware/is-authenticated.js
@@ -4,14 +4,14 @@ module.exports = function(req, res, next) {
   const token = req.headers.authorization || req.cookies.Authorization
   const errorMessage = `Permission denied at ${req.url}`;
   if (token && verifyJWT(token)) {
-    const { username, roles } = decodeJWT(token);
+    const { username, permissions } = decodeJWT(token);
     if (!username) {
       log.warn(errorMessage);
       res.status(401).send(errorMessage);
     } else {
       req.user= {}
       req.user.name = username;
-      req.user.roles = roles;
+      req.user.permissions = permissions;
       next();
     }
   } else {
--- server/src/middleware/permitted.js
+++ server/src/middleware/permitted.js
@@ -1,7 +1,7 @@
-const permit = (allowedRoles) => {
-    const isAllowed = roles => (allowedRoles.filter(Set.prototype.has, new Set(roles))).length > 0;
+const permit = (allowedPermissions) => {
+    const isAllowed = permissions => (allowedPermissions.filter(Set.prototype.has, new Set(permissions))).length > 0;
     return (req, res, next) => {
-      if (isAllowed(req.user.roles)) {
+      if (isAllowed(req.user.permissions)) {
           next();
       } else {
         res.status(403).send(`Access Denied at ${req.url}`);
