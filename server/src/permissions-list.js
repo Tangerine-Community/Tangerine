38fce50adc63d9a7bd0fe0246c64f337bc72a2ad
Evans D <evansdianga@gmail.com>
2020-05-20 14:25:43 +0300

    Wire up assign permissions component

--- server/src/permissions-list.js
+++ server/src/permissions-list.js
@@ -1,5 +1,5 @@
 const permissionsList = {
-  groupPermissions: ['can_manage_group_deployment'],
+  groupPermissions: ['can_manage_group_deployment','can_assign_permissions_to_group_user'],
   sitewidePermissions: [
     'can_manage_site_wide_users',
     'can_create_group',
