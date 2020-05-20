88752f2a97e60d8de746e5b0d22679d8fc8bd77f
Evans D <evansdianga@gmail.com>
2020-05-20 16:04:11 +0300

    Add assign permissions content

--- editor/src/app/groups/assign-permissions/assign-permissions.component.ts
+++ editor/src/app/groups/assign-permissions/assign-permissions.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { _TRANSLATE } from 'src/app/shared/_services/translation-marker';
+import { Breadcrumb } from 'src/app/shared/_components/breadcrumb/breadcrumb.component';
+import { AuthenticationService } from 'src/app/core/auth/_services/authentication.service';
+import { NgxPermissionsService } from 'ngx-permissions';
 
 @Component({
   selector: 'app-assign-permissions',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assign-permissions.component.css']
 })
 export class AssignPermissionsComponent implements OnInit {
+  title = _TRANSLATE('Assign User Permissions');
+  breadcrumbs: Array<Breadcrumb> = [];
+  username;
+  permissionsList;
+  usersPermissions;
+  constructor(private route: ActivatedRoute, 
+    private authenticationService:AuthenticationService, 
+    private permissionsService:NgxPermissionsService) { }
 
-  constructor() { }
+  async ngOnInit(){
+    this.username = this.route.snapshot.paramMap.get('username')
+    this.breadcrumbs = [
+      <Breadcrumb>{
+        label: _TRANSLATE('Security'),
+        url: `security`
+      },
+      <Breadcrumb>{
+        label: `${_TRANSLATE('Assign Permissions to')} ${this.username}`,
+        url: `security/permissions/${this.username}`
+      }
+    ]
 
-  ngOnInit(): void {
+    this.permissionsList = await this.authenticationService.getPermissionsList()
+    this.usersPermissions = this.permissionsService.getPermissions();
+  }
+
+  async addPermissionsToUser(){
+    
   }
 
 }
