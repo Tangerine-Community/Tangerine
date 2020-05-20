909660ecf39c61ba13d27d14b1f61bde66d0fc78
Evans D <evansdianga@gmail.com>
2020-05-20 13:25:41 +0300

    Add permision list file

---
+++ server/src/permissions-list.js
@@ -0,0 +1,8 @@
+const permissionsList = {
+  groupPermissions: ['can_manage_group_deployment'],
+  sitewidePermissions: [
+    'can_manage_site_wide_users',
+    'can_create_group',
+    'can_create_user'],
+};
+module.exports = { permissionsList };
