be49a9d662cec6158b5d409565432f77ecb6a192
Evans D <evansdianga@gmail.com>
2020-05-20 13:26:35 +0300

    Add file assign permissions

---
+++ editor/src/app/groups/assign-permissions/assign-permissions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assign-permissions',
+  templateUrl: './assign-permissions.component.html',
+  styleUrls: ['./assign-permissions.component.css']
+})
+export class AssignPermissionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
