eb999bae9844d14e81741e75509829f7c539c9b8
Evans D <evansdianga@gmail.com>
2020-05-27 20:21:30 +0300

    Add delete user nd update status code

--- server/src/express-app.js
+++ server/src/express-app.js
@@ -39,7 +39,7 @@ const sep = path.sep;
 const tangyModules = require('./modules/index.js')()
 const {doesUserExist, extendSession, findUserByUsername, isSuperAdmin,
    USERS_DB, login, getUserPermissions, updateUserSiteWidePermissions} = require('./auth');
-const {registerUser,  getUserByUsername, isUserSuperAdmin, isUserAnAdminUser, getGroupsByUser} = require('./users');
+const {registerUser,  getUserByUsername, isUserSuperAdmin, isUserAnAdminUser, getGroupsByUser, deleteUser} = require('./users');
 log.info('heartbeat')
 setInterval(() => log.info('heartbeat'), 5*60*1000)
 var cookieParser = require('cookie-parser');
@@ -252,6 +252,7 @@ app.get('/users/byUsername/:username', isAuthenticated, getUserByUsername);
 app.get('/users/isSuperAdminUser/:username', isAuthenticated, isUserSuperAdmin);
 
 app.get('/users/isAdminUser/:username', isAuthenticated, isUserAnAdminUser);
+app.delete('/users/delete/:username', isAuthenticated, deleteUser);
 
 app.post('/editor/file/save', isAuthenticated, async function (req, res) {
   const filePath = req.body.filePath
