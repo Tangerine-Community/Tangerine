1603f33509aed92e6be6c90f84c49ccf6355685a
Evans D <evansdianga@gmail.com>
2020-05-28 18:07:26 +0300

    User one should not have non_user1_user permission. non_user1_user is for users who arent user1s

--- editor/src/app/core/auth/auth-routing.module.ts
+++ editor/src/app/core/auth/auth-routing.module.ts
@@ -7,6 +7,7 @@ import { LoginGuard } from './_guards/login-guard.service';
 import { NgxPermissionsGuard } from 'ngx-permissions';
 import { ManageSitewidePermissionsComponent } from './_components/manage-sitewide-permissions/manage-sitewide-permissions.component';
 import { EditUserComponent } from './_components/edit-user/edit-user.component';
+import { UpdatePersonalProfileComponent } from './_components/update-personal-profile/update-personal-profile.component';
 const routes: Routes = [{
   path: 'register-user',
   component: UserResgistrationComponent
@@ -14,7 +15,14 @@ const routes: Routes = [{
 {
   path: 'login',
   component: LoginComponent
-}, {
+},
+{
+  path: 'update-personal-profile',
+  component: UpdatePersonalProfileComponent,
+  canActivate: [LoginGuard, NgxPermissionsGuard],
+  data: { permissions: { only: ['non_user1_user'], redirectTo: '/projects' } }
+},
+{
   path: 'manage-users',
   component: ManageUsersComponent,
   canActivate: [LoginGuard, NgxPermissionsGuard],
