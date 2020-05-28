eba022495abb7bdfc0f582fbf242eb07d7265904
Evans D <evansdianga@gmail.com>
2020-05-28 17:55:20 +0300

    Add component for user to update their personal profile

---
+++ editor/src/app/core/auth/_components/update-personal-profile/update-personal-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-personal-profile',
+  templateUrl: './update-personal-profile.component.html',
+  styleUrls: ['./update-personal-profile.component.css']
+})
+export class UpdatePersonalProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
