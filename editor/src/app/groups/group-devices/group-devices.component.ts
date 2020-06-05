13d236fbd24b171a2c2e9bb1a1e0ba27eac88418
R.J. Steinert <rj@rjsteinert.com>
2020-06-05 13:42:16 -0400

    On devices list, show both assigned location and sync location

--- editor/src/app/groups/group-devices/group-devices.component.ts
+++ editor/src/app/groups/group-devices/group-devices.component.ts
@@ -57,7 +57,7 @@ export class GroupDevicesComponent implements OnInit {
   flatLocationList
   locationFilter:Array<LocationNode> = []
   tab = 'TAB_USERS'
-  devicesDisplayedColumns = ['id', 'location', 'claimed', 'registeredOn', 'syncedOn', 'updatedOn', 'version', 'star']
+  devicesDisplayedColumns = ['id', 'assigned-location', 'sync-location', 'claimed', 'registeredOn', 'syncedOn', 'updatedOn', 'version', 'star']
 
   @Input('groupId') groupId:string
   @ViewChild('dialog', {static: true}) dialog: ElementRef;
