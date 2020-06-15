e8339da478b38cfb4bb10eba22f387a7217be888
Chris E. Kelley <ckelley@rti.org>
2020-06-14 23:37:04 -0500

    Added support for custom queries

--- client/src/app/case-home/components/case-reports/case-reports.component.ts
+++ client/src/app/case-home/components/case-reports/case-reports.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {UserService} from "../../../shared/_services/user.service";
 
 @Component({
   selector: 'app-case-reports',
@@ -11,10 +12,12 @@ export class CaseReportsComponent implements OnInit {
   @ViewChild('container', {static: true}) container: ElementRef
 
   constructor(
-    private http:HttpClient
+    private http:HttpClient,
+    private userService: UserService,
   ) { }
 
   async ngOnInit() {
+    window['TangyDb'] = await this.userService.getUserDatabase(this.userService.getCurrentUser())
     const reportsHtml = await this.http.get('./assets/reports.html', {responseType: 'text'}).toPromise()
     this.container.nativeElement.innerHTML = reportsHtml
   }
