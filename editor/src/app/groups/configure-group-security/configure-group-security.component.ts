3773ef8616bed706458c98cda4b48e88776f3c0f
Evans D <evansdianga@gmail.com>
2020-06-15 14:02:00 +0300

    Move component `ListComponent` to `ConfigureGroupSecurityComponent`

--- editor/src/app/groups/configure-group-security/configure-group-security.component.ts
+++ editor/src/app/groups/configure-group-security/configure-group-security.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Breadcrumb } from 'src/app/shared/_components/breadcrumb/breadcrumb.component';
+import { ActivatedRoute } from '@angular/router';
+import { _TRANSLATE } from '../../shared/_services/translation-marker';
 
 @Component({
   selector: 'app-configure-group-security',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configure-group-security.component.css']
 })
 export class ConfigureGroupSecurityComponent implements OnInit {
+  title = _TRANSLATE('Security');
+  breadcrumbs;
+  groupId;
+  constructor(private route: ActivatedRoute) { }
 
-  constructor() { }
+  async ngOnInit() {
+    this.breadcrumbs = [
+      <Breadcrumb>{
+        label: _TRANSLATE('Security'),
+        url: `security`
+      }
+    ];
 
-  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.groupId = params.groupId;
+    });
   }
 
 }
--- editor/src/app/groups/groups-routing.module.ts
+++ editor/src/app/groups/groups-routing.module.ts
@@ -47,6 +47,7 @@ import { PrintFormAsTableComponent } from './print-form-as-table/print-form-as-t
 import { GroupDeviceUserComponent } from './group-device-user/group-device-user.component';
 import { CaseSettingsComponent } from './case-settings/case-settings.component';
 import { NgxPermissionsGuard } from 'ngx-permissions';
+import { ConfigureGroupSecurityComponent } from './configure-group-security/configure-group-security.component';
 
 const groupsRoutes: Routes = [
   // { path: 'projects', component: GroupsComponent },
@@ -73,7 +74,8 @@ const groupsRoutes: Routes = [
   { path: 'groups/:groupId/configure/location-list/manage-location-list-metadata/:locationLevel', component: ManageLocationListMetadataComponent, canActivate: [LoginGuard] },
   { path: 'groups/:groupId/configure/sync', component: GroupFormsSyncComponent, canActivate: [LoginGuard] },
   { path: 'groups/:groupId/configure/case', component: CaseSettingsComponent, canActivate: [LoginGuard, AdminUserGuard] },
-  { path: 'groups/:groupId/configure/security', component: ListUsersComponent, canActivate: [LoginGuard] },
+  { path: 'groups/:groupId/configure/security', component: ConfigureGroupSecurityComponent, canActivate: [LoginGuard] },
+  { path: 'groups/:groupId/configure/security/assign-role', component: ListUsersComponent, canActivate: [LoginGuard] },
   { path: 'groups/:groupId/configure/security/role/:username', component: AddUserComponent, canActivate: [LoginGuard] },
   { path: 'groups/:groupId/deploy', component: GroupDeployComponent, canActivate: [LoginGuard, AdminUserGuard ]},
   { path: 'groups/:groupId/deploy/device-users', component: GroupDeviceUsersComponent, canActivate: [LoginGuard] },
