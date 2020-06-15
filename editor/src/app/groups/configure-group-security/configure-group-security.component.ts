cc4c30c90121ce0f1de3c8030dbe6ea4d12cc596
Evans D <evansdianga@gmail.com>
2020-06-15 13:33:03 +0300

    Add component

---
+++ editor/src/app/groups/configure-group-security/configure-group-security.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-configure-group-security',
+  templateUrl: './configure-group-security.component.html',
+  styleUrls: ['./configure-group-security.component.css']
+})
+export class ConfigureGroupSecurityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
