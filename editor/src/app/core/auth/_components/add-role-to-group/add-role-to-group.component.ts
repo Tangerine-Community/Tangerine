71ae8d1a2defb9a9fe98061271d6bd494292eef7
Evans D <evansdianga@gmail.com>
2020-06-15 14:32:04 +0300

    Add breadcrubs to component

--- editor/src/app/core/auth/_components/add-role-to-group/add-role-to-group.component.ts
+++ editor/src/app/core/auth/_components/add-role-to-group/add-role-to-group.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { _TRANSLATE } from 'src/app/shared/_services/translation-marker';
+import { ActivatedRoute } from '@angular/router';
+import { Breadcrumb } from 'src/app/shared/_components/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-add-role-to-group',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-role-to-group.component.css']
 })
 export class AddRoleToGroupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  title = _TRANSLATE('Security');
+  breadcrumbs;
+  groupId;
+  constructor(private route: ActivatedRoute) { }
+  async ngOnInit() {
+    this.breadcrumbs = [
+      <Breadcrumb>{
+        label: _TRANSLATE('Security'),
+        url: `security`
+      },
+      <Breadcrumb>{
+        label: _TRANSLATE('Configure Roles'),
+        url: `security/configure-roles`
+      },
+      <Breadcrumb>{
+        label: _TRANSLATE('Add Roles to Group'),
+        url: `security/configure-roles/add-role`
+      }
+    ];
+    this.groupId = this.route.snapshot.paramMap.get('groupId');
   }
-
 }
