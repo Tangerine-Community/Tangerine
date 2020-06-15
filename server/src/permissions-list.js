a62546647628f92985a1a26b6a8de0d84b8e8d47
Evans D <evansdianga@gmail.com>
2020-06-15 14:02:00 +0300

    Move component `ListComponent` to `ConfigureGroupSecurityComponent`

--- server/src/permissions-list.js
+++ server/src/permissions-list.js
@@ -1,15 +1,18 @@
 const permissionsList = {
-  groupPermissions: ['can_manage_group_deployment','can_assign_permissions_to_group_user'],
+  groupPermissions: [
+    'can_manage_group_deployment',
+    'can_assign_permissions_to_group_user',
+  ],
   sitewidePermissions: [
     'can_create_group',
     'can_view_users_list',
     'can_create_users',
     'can_edit_users',
-    'can_manage_users_site_wide_permissions'
-  ]
+    'can_manage_users_site_wide_permissions',
+  ],
 };
 
 const getPermissionsList = async (req, res) => {
   return res.status(200).send(permissionsList);
 };
-module.exports = { permissionsList , getPermissionsList};
+module.exports = { permissionsList, getPermissionsList };
