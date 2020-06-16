c6a4bc5eec2076abeabaa686c26868b3b762c8f9
Evans D <evansdianga@gmail.com>
2020-06-16 22:14:29 +0300

    Add component to update user role

---
+++ editor/src/app/core/auth/_components/update-user-role/update-user-role.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-user-role',
+  templateUrl: './update-user-role.component.html',
+  styleUrls: ['./update-user-role.component.css']
+})
+export class UpdateUserRoleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
