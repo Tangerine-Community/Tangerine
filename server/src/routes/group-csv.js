62ad25975c36ab04bea7b5f7888a1317c04b16a3
Chris E. Kelley <ckelley@rti.org>
2020-06-22 15:50:38 -0500

    Fetch group and form name to use in CSV output name

--- server/src/routes/group-csv.js
+++ server/src/routes/group-csv.js
@@ -4,6 +4,23 @@ const log = require('tangy-log').log
 const sanitize = require('sanitize-filename');
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
+const fs = require('fs-extra')
+const axios = require('axios')
+
+async function getUser1HttpInterface() {
+  const body = await axios.post('http://localhost/login', {
+    username: process.env.T_USER1,
+    password: process.env.T_USER1_PASSWORD
+  })
+  const token = body['data']['data']['token']
+  let http = axios.create({
+    headers: {
+      authorization: token
+    },
+    baseUrl: 'http://localhost'
+  })
+  return http
+}
 
 module.exports = async function (req, res) {
   const groupId = sanitize(req.params.groupId)
@@ -13,13 +30,25 @@ module.exports = async function (req, res) {
     sanitizedExtension = '-sanitized'
   }
   let dbName = `${groupId}-reporting${sanitizedExtension}`;
-  const fileName = `${groupId}-${formId}${sanitizedExtension}-${Date.now()}.csv`
+  const forms = await fs.readJson(`/tangerine/client/content/groups/${groupId}/forms.json`)
+  const formInfo = forms.find(formInfo => formInfo.id === formId)
+  const title = formInfo.title.replace(/ /g, '_')
+  // this.group = await this.groupsService.getGroupInfo(groupId);
+  const http = await getUser1HttpInterface()
+  const group = (await http.get(`/nest/group/read/${groupId}`)).data
+  const groupLabel = group.label.replace(/ /g, '_')
+  console.log("groupLabel: " + groupLabel)
+  const options = {
+    replacement: '_'
+  }
+  const groupFormname = sanitize(groupLabel + '-' + title, options)
+  const fileName = `${groupFormname}${sanitizedExtension}-${Date.now()}.csv`
   let outputPath = `/csv/${fileName}`
   const batchSize = (process.env.T_CSV_BATCH_SIZE) ? process.env.T_CSV_BATCH_SIZE : 5
-  console.log("req.originalUrl " + req.originalUrl + " outputPath: " + outputPath + " dbName: " + dbName);
+  // console.log("req.originalUrl " + req.originalUrl + " outputPath: " + outputPath + " dbName: " + dbName);
   
   const sleepTimeBetweenBatches = 0
-  let cmd = `cd /tangerine/server/src/scripts/generate-csv/ && ./bin.js ${dbName} ${formId} ${outputPath} ${batchSize} ${sleepTimeBetweenBatches}`
+  let cmd = `cd /tangerine/server/src/scripts/generate-csv/ && ./bin.js ${dbName} ${formId} "${outputPath}" ${batchSize} ${sleepTimeBetweenBatches}`
   if (req.params.year && req.params.month) {
     cmd += ` ${sanitize(req.params.year)} ${sanitize(req.params.month)}`
   }
