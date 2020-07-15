feb49f518f220a531b95fb0176d0a3b4ff8c5ca4
Evans D <evansdianga@gmail.com>
2020-07-15 09:30:56 +0300

    Add export location list component

---
+++ editor/src/app/groups/export-location-list/export-location-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-export-location-list',
+  templateUrl: './export-location-list.component.html',
+  styleUrls: ['./export-location-list.component.css']
+})
+export class ExportLocationListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
