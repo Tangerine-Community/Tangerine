bd37faee785667f584c09e53fc9a9d8ff1880f64
R.J. Steinert <rj@rjsteinert.com>
2020-07-23 15:31:53 -0400

    Get working and test diffType_EventForm_FormResponseIDCreated.resolve

--- client/src/app/sync/conflict/diff-type--event-form--form-response-id-created.ts
+++ client/src/app/sync/conflict/diff-type--event-form--form-response-id-created.ts
@@ -66,9 +66,37 @@ export function detect({a, b, diffs, caseDefinition}:DiffInfo):DiffInfo {
 }
 
 export function resolve({diffInfo, merged}:MergeInfo):MergeInfo {
+  const recognizedDiffs = diffInfo.diffs.filter(diff => diff.type === DIFF_TYPE__EVENT_FORM__FORM_RESPONSE_ID_CREATED)
+  const affectedEventFormIds = recognizedDiffs.map(diff => diff.info.eventFormId)
   return {
-    diffInfo,
-    merged,
+    diffInfo: {
+      ...diffInfo,
+      diffs: diffInfo.diffs.map(diff => {
+        return diff.type === DIFF_TYPE__EVENT_FORM__FORM_RESPONSE_ID_CREATED
+          ? {
+            ...diff,
+            resolved: true
+          }
+          : diff
+      })
+    },
+    merged: {
+      ...merged,
+      events: merged.events.map(event => {
+        return {
+          ...event,
+          eventForms: event.eventForms.map(eventForm => {
+            return recognizedDiffs.some(diff => diff.info.eventFormId === eventForm.id)
+              ? {
+                ...eventForm,
+                formResponseId: recognizedDiffs.find(diff => diff.info.eventFormId === eventForm.id).info.formResponseId
+              } 
+              : eventForm
+          })
+        }
+
+      })
+    }
   }
 }
 
