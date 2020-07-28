428981e08d716838af91ce846cfab69d713c143b
R.J. Steinert <rj@rjsteinert.com>
2020-07-28 12:15:08 -0400

    Install.sh docker setup and executable

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
-# Copy over config.sh script, prompting for settings and passwords, ensuring password strength.
 # Install Docker
+apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+# Copy over config.sh script, prompting for settings and passwords, ensuring password strength.
 # Set up Docker Log settings
 # Create outage alarm in AWS Cloudwatch 
 # Create DNS records in AWS Route 53
