87e6c683090574d2589ec46c4846beeb3ac02028
R.J. Steinert <rj@rjsteinert.com>
2020-07-28 12:00:38 -0400

    Add start of install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Copy over config.sh script, prompting for settings and passwords, ensuring password strength.
+# Install Docker
+# Set up Docker Log settings
+# Create outage alarm in AWS Cloudwatch 
+# Create DNS records in AWS Route 53
+# Set up Nginx Reverse Proxy with SSL 
+# Configure cron
+
