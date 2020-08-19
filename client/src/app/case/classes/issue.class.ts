30a7889f8198c377d47ca7f3db63a21172e1a0bd
R.J. Steinert <rj@rjsteinert.com>
2020-08-19 14:12:17 -0400

    Refactor Issue.resolveOnAppContext to support multiple contexts

--- client/src/app/case/classes/issue.class.ts
+++ client/src/app/case/classes/issue.class.ts
@@ -41,7 +41,7 @@ class Issue extends TangyFormResponseModel {
   type:string = 'issue'
   createdOn:number
   createdAppContext:AppContext
-  resolveOnAppContext:AppContext
+  resolveOnAppContexts:Array<AppContext>
 
   constructor(data?:any) {
     super()
