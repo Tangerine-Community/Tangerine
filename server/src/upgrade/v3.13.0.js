56469be892d2f3222e138969a0a37236925dc4a1
Chris E. Kelley <ckelley@rti.org>
2020-08-31 10:59:19 -0500

    Fix to upgrade script

--- server/src/upgrade/v3.13.0.js
+++ server/src/upgrade/v3.13.0.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-
+const util = require('util');
+const exec = util.promisify(require('child_process').exec)
 const PouchDB = require('pouchdb')
 async function go() {
   console.log('Upgrading groups Admin roles with new permissions...')
@@ -30,12 +31,17 @@ async function go() {
   for (const group of groups) {
     await groupsDb.put(group)
     const groupId = group._id
-    await exec(`mkdir /tangerine/groups/${groupId}/`)
-    await exec(`mv /tangerine/client/content/groups/${groupId} /tangerine/groups/${groupId}/client`)
-    await exec(`mkdir /tangerine/groups/${groupId}/editor`)
-    // @TODO Create a symlink to the old group client directory until all the other APIs are updated and we have 
-    // a proper upgrade script to migrate group directories.
-    await exec(`ln -s /tangerine/groups/${groupId}/client /tangerine/client/content/groups/${groupId}`
+    try {
+      await exec(`mkdir /tangerine/groups/${groupId}/`)
+      await exec(`mv /tangerine/client/content/groups/${groupId} /tangerine/groups/${groupId}/client`)
+      await exec(`mkdir /tangerine/groups/${groupId}/editor`)
+      // @TODO Create a symlink to the old group client directory until all the other APIs are updated and we have 
+      // a proper upgrade script to migrate group directories.
+      await exec(`ln -s /tangerine/groups/${groupId}/client /tangerine/client/content/groups/${groupId}`)
+    } catch (e) {
+      console.log(e)
+    }
+
   }
 }
 
