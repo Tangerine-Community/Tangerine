988e77c110ec2de7ca60310669b282bfe6732f90
Evans D <evansdianga@gmail.com>
2020-09-08 17:58:18 +0300

    Create shell chrome for app

--- online-survey-app/src/app/app.component.ts
+++ online-survey-app/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'online-survey-app';
+  isLoggedIn = true;
+  languageDirection = 'ltr';
+  appName = 'Evano';
 }
