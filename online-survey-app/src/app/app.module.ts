0da7b01992e1dd3c875ddbf1771a1f9f8d7ab35b
Evans D <evansdianga@gmail.com>
2020-09-08 17:58:18 +0300

    Create shell chrome for app

--- online-survey-app/src/app/app.module.ts
+++ online-survey-app/src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { TangySvgLogoComponent } from './shared/tangy-svg-logo/tangy-svg-logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TangySvgLogoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
