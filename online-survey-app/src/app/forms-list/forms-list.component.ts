d34c2508d5ae45c95ccd0814453101fe6d55d429
Evans D <evansdianga@gmail.com>
2020-09-10 18:36:32 +0300

    Create forms list component

---
+++ online-survey-app/src/app/forms-list/forms-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forms-list',
+  templateUrl: './forms-list.component.html',
+  styleUrls: ['./forms-list.component.css']
+})
+export class FormsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
