73610bdb6328d5f59792cf32312bb95b9eb11cab
Evans D <evansdianga@gmail.com>
2020-09-10 19:06:39 +0300

    List forms

--- online-survey-app/src/app/forms-list/forms-list.component.ts
+++ online-survey-app/src/app/forms-list/forms-list.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsListComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private httpClient: HttpClient) { }
+  formsList;
+  async ngOnInit(): Promise<any> {
+    this.formsList = await this.httpClient.get('./assets/forms.json').toPromise();
   }
 
 }
