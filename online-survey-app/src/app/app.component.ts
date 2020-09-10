7777f98f8c63ee867d262fad5f9c7c38be1b044b
Evans D <evansdianga@gmail.com>
2020-09-10 18:35:38 +0300

    Usee appconfig service to set some global variables

--- online-survey-app/src/app/app.component.ts
+++ online-survey-app/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppConfigService } from './shared/_services/app-config.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'online-survey-app';
-  isLoggedIn = true;
-  languageDirection = 'ltr';
-  appName = 'Evano';
+export class AppComponent implements OnInit{
+  languageDirection: string;
+  appName: string;
+  constructor(private appConfigService: AppConfigService){
+  }
+
+  async ngOnInit(): Promise<any>{
+    try {
+      const appConfig = await this.appConfigService.getAppConfig();
+      this.appName = appConfig.appName;
+      this.languageDirection = appConfig.languageDirection;
+    } catch (error) {
+      this.appName = '';
+      this.languageDirection = 'ltr';
+    }
+  }
 }
