a3dcede87d54f5daadf7ab933e64898010f1b229
Evans D <evansdianga@gmail.com>
2020-09-10 19:55:48 +0300

    Add routes

--- online-survey-app/src/app/app-routing.module.ts
+++ online-survey-app/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsListComponent } from './forms-list/forms-list.component';
+import { TangyFormsPlayerComponent } from './tangy-forms-player/tangy-forms-player.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FormsListComponent},
+  { path: 'forms', component: FormsListComponent},
+  { path: 'tangy-forms/new/:formId', component: TangyFormsPlayerComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
