3b6b873852cac0737b96922deab1b26e7fa4a467
Evans D <evansdianga@gmail.com>
2020-09-17 00:20:45 +0300

    Create new route and links for releasing survey

--- editor/src/app/groups/groups-routing.module.ts
+++ editor/src/app/groups/groups-routing.module.ts
@@ -53,6 +53,7 @@ import { GroupDeviceSheetComponent } from './group-device-sheet/group-device-she
 import {MatTableModule} from "@angular/material/table";
 import {CommonModule} from "@angular/common";
 import {TranslateModule} from "@ngx-translate/core";
+import { ReleaseOnlineSurveyComponent } from './release-online-survey/release-online-survey.component';
 
 const groupsRoutes: Routes = [
   // { path: 'projects', component: GroupsComponent },
@@ -87,6 +88,7 @@ const groupsRoutes: Routes = [
   { path: 'groups/:groupId/configure/security/role/:username', component: UpdateUserRoleComponent, canActivate: [LoginGuard] },
   { path: 'groups/:groupId/deploy', component: GroupDeployComponent, canActivate: [LoginGuard ]},
   { path: 'groups/:groupId/deploy/device-users', component: GroupDeviceUsersComponent, canActivate: [LoginGuard] },
+  { path: 'groups/:groupId/deploy/onlineSurvey', component: ReleaseOnlineSurveyComponent, canActivate: [LoginGuard ]},
   { path: 'groups/:groupId/deploy/device-users/:responseId', component: GroupDeviceUserComponent, canActivate: [LoginGuard] },
   { path: 'groups/:groupId/deploy/devices', component: GroupDevicesComponent, canActivate: [LoginGuard] },
   { path: 'groups/:groupId/deploy/device-sheet', component: GroupDeviceSheetComponent, canActivate: [LoginGuard] },
