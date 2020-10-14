e9f55475bd066799f13babbf266d5a012ae52d43
Chris E. Kelley <ckelley@rti.org>
2020-10-14 13:08:36 -0500

    changed android_identifier and name in hcp-template
    so it can be modified to supplied AppName and packageName when building APK
    Moved platform rm comands higher in release-apk to fix compile issue

--- server/src/scripts/release-apk.sh
+++ server/src/scripts/release-apk.sh
@@ -53,17 +53,27 @@ rm -rf $RELEASE_DIRECTORY/www/shell/assets
 cp -r $CONTENT_PATH $RELEASE_DIRECTORY/www/shell/assets
 cp /tangerine/logo.svg $RELEASE_DIRECTORY/www/
 
+cd $RELEASE_DIRECTORY
+
+echo "RELEASE APK: removing cordova-android package symlink"
+rm -rf $RELEASE_DIRECTORY/node_modules/cordova-android
+rm -rf $RELEASE_DIRECTORY/package-lock.json
+
+echo "RELEASE APK: removing Android platform"
+cordova platform rm android --no-telemetry
+
 CUSTOM_SCRIPTS_PATH="$RELEASE_DIRECTORY/www/shell/assets/custom-scripts.js"
 if [ -f "$CUSTOM_SCRIPTS_PATH" ]; then
+  echo "RELEASE APK webpacking CUSTOM_SCRIPTS"
   webpack $CUSTOM_SCRIPTS_PATH -o "$CUSTOM_SCRIPTS_PATH.tmp"
+  mv "$CUSTOM_SCRIPTS_PATH" $CUSTOM_SCRIPTS_PATH.original 
   mv "$CUSTOM_SCRIPTS_PATH.tmp" $CUSTOM_SCRIPTS_PATH 
 fi
 
 # Stash the Build ID in the release.
 echo $BUILD_ID > $RELEASE_DIRECTORY/www/shell/assets/tangerine-build-id 
 echo $RELEASE_TYPE > $RELEASE_DIRECTORY/www/shell/assets/tangerine-build-channel
-
-cd $RELEASE_DIRECTORY
+echo $PACKAGE > $RELEASE_DIRECTORY/www/shell/assets/tangerine-package-name
 
 # replace the URL property in config.xml
 sed -i -e "s#CHCP_URL#"$CHCP_URL"#g" $RELEASE_DIRECTORY/config.xml
@@ -77,31 +87,18 @@ sed -i -e s#"<name>Tangerine"#"${APPNAME_REPLACE}"#g $RELEASE_DIRECTORY/config.x
 echo "Copying cordova-hcp.json $RELEASE_DIRECTORY"
 cp /tangerine/client/android-tools/cordova-hot-code-push/cordova-hcp-template.json $RELEASE_DIRECTORY/cordova-hcp.json
 sed -i -e "s#URL#"$URL"#g" $RELEASE_DIRECTORY/cordova-hcp.json
+sed -i -e "s#PACKAGE#"$PACKAGE"#g" $RELEASE_DIRECTORY/cordova-hcp.json
+sed -i -e "s#APPNAME#$APPNAME#g" $RELEASE_DIRECTORY/cordova-hcp.json
 
 # Create the chcp manifest.
 /tangerine/server/node_modules/cordova-hot-code-push-cli/bin/cordova-hcp build
 echo '{"processing":true,"step":"Compiling APK"}' > $STATUS_FILE
 
-echo "RELEASE APK: removing cordova-android package symlink"
-rm -rf $RELEASE_DIRECTORY/node_modules/cordova-android
-echo "RELEASE APK: removing Android platform"
-cordova platform rm android
+#echo "RELEASE APK: npm install cordova-android"
+#npm install $CORDOVA_ANDROID_DIRECTORY
+
 echo "RELEASE APK: adding Android platform"
-#cordova platform add android
-#cordova platform add github:apache/cordova-android
-#cordova platform add $CORDOVA_DIRECTORY/platforms/android --link=$CORDOVA_DIRECTORY/platforms/android
-cordova platform add $CORDOVA_ANDROID_DIRECTORY
-
-#echo "RELEASE APK: Deleting Android platform directory"
-#rm -rf $RELEASE_DIRECTORY/platforms
-#echo "RELEASE APK: Adding default Android platform directory"
-#cp -r $CORDOVA_DIRECTORY/platforms .
-
-#echo "RELEASE APK: Cordova clean"
-#cordova clean
-#
-#echo "RELEASE APK: gradlew clean"
-#gradlew clean
+cordova platform add $CORDOVA_ANDROID_DIRECTORY --no-telemetry
 
 echo "RELEASE APK: running Cordova build."
 cordova build --no-telemetry android
