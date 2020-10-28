fcc13fb31871f51db6105b469fb88711bf898040
R.J. Steinert <rj@rjsteinert.com>
2020-10-28 11:07:52 -0400

    Add support for starting ngrok on custom subdomain when using develop.sh

--- entrypoint-development.sh
+++ entrypoint-development.sh
@@ -3,6 +3,12 @@
 cd /tangerine/server 
 npm run start:debug &
 
+if [ "$T_NGROK_AUTH_TOKEN" ]; then
+  ./node_modules/.bin/ngrok authtoken "$T_NGROK_AUTH_TOKEN" 
+  ./node_modules/.bin/ngrok http -subdomain="$T_NGROK_SUBDOMAIN" 80 &
+fi
+
+
 cd /tangerine/client
 ./node_modules/.bin/ng build --watch --poll 100 --base-href ./ --output-path ./dev &
 
