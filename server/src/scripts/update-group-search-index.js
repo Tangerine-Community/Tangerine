0115c04466a76f55957a53a55f99e68e82b48c9f
R.J. Steinert <rj@rjsteinert.com>
2020-10-28 12:23:02 -0400

    Add update-group-search-index command for adding a view based on forms.json that indexes variables defined in forms.json

---
+++ server/src/scripts/update-group-search-index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+const fs = require('fs-extra')
+const { emit } = require('process')
+const DB = require(`../db.js`)
+
+if (process.argv[2] === '--help') {
+  console.log('Usage:')
+  console.log('       ./bin.js')
+  process.exit()
+}
+
+const GROUP_ID = process.argv[2]
+
+async function go() {
+  console.log('')
+  console.log(`Updating search index for group ${GROUP_ID}`)
+  console.log('')
+  const formsInfo = await fs.readJSON(`/tangerine/groups/${GROUP_ID}/client/forms.json`) 
+  const db = DB(GROUP_ID)
+  const variablesToIndexByFormId = formsInfo.reduce((variablesToIndexByFormId, formInfo) => {
+    return formInfo.searchSettings.shouldIndex
+      ? {
+        ...variablesToIndexByFormId,
+        [formInfo.id]: formInfo.searchSettings.variablesToIndex
+      }
+      : variablesToIndexByFormId
+  }, {})
+  let map = `function(doc) {
+      var variablesToIndexByFormId = ${JSON.stringify(variablesToIndexByFormId)}
+      if (
+        doc.collection === 'TangyFormResponse' &&
+        doc.items &&
+        Array.isArray(doc.items) &&
+        doc.form &&
+        doc.form.id &&
+        variablesToIndexByFormId.hasOwnProperty(doc.form.id)
+      ) {
+        
+      }
+      emit(doc._id,true)
+    }`
+  //eval(`map = ${map}`)
+  const doc = {
+    _id: '_design/search',
+    views: {
+      'search': {
+        map
+      }
+    }
+  }
+  try {
+    const existingSearchDoc = await db.get('_design/search')
+    doc['_rev'] = existingSearchDoc._rev
+  } catch (e) { }
+  try {
+    await db.put(doc)
+
+  } catch (e) {
+    console.log(e)
+  }
+}
+go()
