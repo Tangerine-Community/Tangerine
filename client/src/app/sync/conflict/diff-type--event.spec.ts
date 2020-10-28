fdf7c5881c4f867af0d8f1ad52fc28003ed49cbb
Chris E. Kelley <ckelley@rti.org>
2020-10-27 19:18:32 -0500

    Added check for canonicalTimestamp
    Overrides conflict resolution

--- client/src/app/sync/conflict/diff-type--event.spec.ts
+++ client/src/app/sync/conflict/diff-type--event.spec.ts
@@ -4,6 +4,7 @@ import { diff } from './diff';
 import { merge } from './merge';
 import {DIFF_TYPE__EVENT, diffType_Event} from "./diff-type--event";
 import {DIFF_TYPE__EVENT_FORM} from "./diff-type--event-form";
+import * as moment from 'moment'
 
 const caseDefinition:CaseDefinition = {
   id: "test",
@@ -98,6 +99,49 @@ describe('diffType_Event', () => {
       diffs: [],
       caseDefinition
     })
+    expect(diffInfo.diffs.length).toEqual(1)
+    expect(diffInfo.diffs[0].type).toEqual(DIFF_TYPE__EVENT)
+    expect(diffInfo.diffs[0].resolved).toEqual(false)
+    expect(diffInfo.diffs[0].info.where).toEqual('b')
+    expect(diffInfo.diffs[0].info.differences[0]).toEqual('new')
+  })
+  
+  it('should override difference and mark as canonical', () => {
+    const aCopy = JSON.parse(JSON.stringify(a))
+    const bCopy = JSON.parse(JSON.stringify(b))
+    const tangerineModifiedOn = moment().subtract(10, 'days').valueOf();
+    let canonicalTimestamp = Date.now();
+    const diffInfo = diffType_Event.detect({
+      a:{
+        ...aCopy,
+        canonicalTimestamp: canonicalTimestamp
+      },
+      b:{
+        ...bCopy,
+        tangerineModifiedOn: tangerineModifiedOn,
+        events: [...bCopy.events,
+          {
+            id: 'event2',
+            caseEventDefinitionId: 'event-definition-1',
+            eventForms: [
+              {
+                id: 'event-form-2',
+                "caseEventId": "event2",
+                eventFormDefinitionId: 'event-form-definition-1',
+                formResponseId: 'form-response-2'
+              }
+            ]
+          }
+        ]
+      },
+      diffs: [],
+      caseDefinition
+    })
+    if (diffInfo.a['canonicalTimestamp'] && (diffInfo.a['canonicalTimestamp'] > diffInfo.b['tangerineModifiedOn'])) {
+      debugger;
+      diffInfo.canonical = true
+    }
+    expect(diffInfo.canonical).toEqual(true)
     expect(diffInfo.diffs.length).toEqual(1)
     expect(diffInfo.diffs[0].type).toEqual(DIFF_TYPE__EVENT)
     expect(diffInfo.diffs[0].resolved).toEqual(false)
