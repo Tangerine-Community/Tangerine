4815e8d2913b56f498de1eb37a085c49d32bed13
R.J. Steinert <rj@rjsteinert.com>
2020-10-30 13:58:37 -0400

    Remove find-participant.component.ts and related component, it's in the case-module content set as an optional component/query

--- client/src/app/case/case.docs.ts
+++ client/src/app/case/case.docs.ts
@@ -1,27 +1,5 @@
 const emit = function(key, value) { }
 
 export const CaseDocs = [
-  {
-    _id: '_design/participantSearch',
-    views: {
-      participantSearch: {
-        map: function (doc) {
-          if (doc.type === 'case' && doc.participants && Array.isArray(doc.participants)) {
-            for (let participant of doc.participants) {
-              if (participant.data && typeof participant.data === 'object') {
-                for (let property in participant.data) {
-                  emit(`${participant.data[property]}`.toLocaleLowerCase(), {
-                    caseId: doc._id,
-                    participantId: participant.id,
-                    matchesOn: property
-                  })
-                }
-              }
-            }
-          }
-        }.toString()
-      }
-    }
-  }
-]
 
+]
