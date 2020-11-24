f871988f9c5242035f3c53a05f7410a16ac56c74
R.J. Steinert <rj@rjsteinert.com>
2020-11-24 11:42:38 -0500

    Add T.case.isIssueContext() for detecting if in the Issue context in a form

--- editor/src/app/case/services/case.service.ts
+++ editor/src/app/case/services/case.service.ts
@@ -810,6 +810,12 @@ class CaseService {
     return diff
   }
 
+  isIssueContext() {
+    return window.location.hash.includes('/issues/')
+      ? true
+      : false
+  }
+
   /*
    * Data Inquiries API
    */
