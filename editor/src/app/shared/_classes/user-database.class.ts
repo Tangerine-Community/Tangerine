ed12bd625f15c053101d3fa0b4ec8df3ace111ef
R.J. Steinert <rj@rjsteinert.com>
2020-12-02 14:18:36 -0500

    Add support for attachHistoryToDocs to editor context

--- editor/src/app/shared/_classes/user-database.class.ts
+++ editor/src/app/shared/_classes/user-database.class.ts
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import * as jsonpatch from "fast-json-patch"
+
 
 export class UserDatabase {
 
@@ -9,6 +11,7 @@ export class UserDatabase {
   buildId:string;
   buildChannel:string;
   groupId:string;
+  attachHistoryToDocs:boolean = undefined
 
   constructor(userId: string, groupId = '') {
     this.userId = userId
@@ -26,29 +29,18 @@ export class UserDatabase {
   }
 
   async put(doc) {
-    const token = localStorage.getItem('token');
-    return await (<any>axios.post(`/group-responses/update/${this.groupId}`, {response: {
-      ...doc,
-      tangerineModifiedByUserId: this.userId,
-      tangerineModifiedByDeviceId: this.deviceId,
-      tangerineModifiedOn: Date.now(),
-      buildId: this.buildId,
-      deviceId: this.deviceId,
-      groupId: this.groupId,
-      buildChannel: this.buildChannel,
-      // Backwards compatibility for sync protocol 1. 
-      lastModified: Date.now()
-    }},
-    {
-      headers: {
-        authorization: token
-      }
-    })).data;
+    return await this.post(doc)
   }
 
   async post(doc) {
     const token = localStorage.getItem('token');
-    return (<any>await axios.post(`/group-responses/update/${this.groupId}`, {response: {
+    if (this.attachHistoryToDocs === undefined) {
+      const appConfig = (<any>await axios.get('./assets/app-config.json', { headers: { authorization: token }})).data
+      this.attachHistoryToDocs = appConfig['attachHistoryToDocs']
+        ? true
+        : false
+    }
+    const newDoc = {
       ...doc,
       tangerineModifiedByUserId: this.userId,
       tangerineModifiedByDeviceId: this.deviceId,
@@ -59,7 +51,15 @@ export class UserDatabase {
       buildChannel: this.buildChannel,
       // Backwards compatibility for sync protocol 1. 
       lastModified: Date.now()
-    }},
+    }
+    return (<any>await axios.post(`/group-responses/update/${this.groupId}`, {
+      response: {
+        ...newDoc,
+        ...this.attachHistoryToDocs
+          ? { history: await this._calculateHistory(newDoc) }
+          : { }
+      }
+    },
     {
       headers: {
         authorization: token
@@ -72,6 +72,27 @@ export class UserDatabase {
     // This is not implemented...
     const token = localStorage.getItem('token');
     return await axios.delete(`/api/${this.groupId}`, doc)
+  }
+
+  async _calculateHistory(newDoc) {
+    let history = []
+    try {
+      const currentDoc = await this.get(newDoc._id)
+      const entry = {
+        lastRev: currentDoc._rev,
+        patch: jsonpatch.compare(currentDoc, newDoc).filter(mod => mod.path.substr(0,8) !== '/history')
+      }
+      history = currentDoc.history
+        ? [ entry, ...currentDoc.history ]
+        : [ entry ]
+    } catch (e) {
+      const entry = {
+        lastRev: 0,
+        patch: jsonpatch.compare({}, newDoc).filter(mod => mod.path.substr(0,8) !== '/history')
+      }
+      history = [ entry ]
+    }
+    return history 
   }
 
 }
