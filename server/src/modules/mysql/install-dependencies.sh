f558b859b01c655613fc359504f7292a7a38d0d0
R.J. Steinert <rj@rjsteinert.com>
2020-12-07 16:35:51 -0500

    Add an install-dependencies.sh file for TangerineToMySQL.py

---
+++ server/src/modules/mysql/install-dependencies.sh
@@ -0,0 +1,10 @@
+pip3 install mysql-connector-python
+pip3 install configparser
+pip3 install cloudant
+pip3 install pandas
+pip3 install pymysql
+pip3 install SQLAlchemy
+sudo apt update
+sudo apt install snapd
+sudo snap install core
+sudo snap install mysql-shell
