d484833d55eb76a4f8538b0b1a260b7918a399a0
R.J. Steinert <rj@rjsteinert.com>
2020-12-08 17:59:35 -0500

    Install mysql in the container so it can interface with the mysql container

--- server/src/modules/mysql/install-dependencies.sh
+++ server/src/modules/mysql/install-dependencies.sh
@@ -4,7 +4,5 @@ pip3 install cloudant
 pip3 install pandas
 pip3 install pymysql
 pip3 install SQLAlchemy
-sudo apt update
-sudo apt install snapd
-sudo snap install core
-sudo snap install mysql-shell
+apt update
+apt-get install -y mysql-server
