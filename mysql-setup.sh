e10c3dae5aecbec261457052f3b7ab8d316e6ddf
R.J. Steinert <rj@rjsteinert.com>
2020-12-23 12:14:50 -0500

    Always run mysql setup script, just check to see if commands need to run

--- mysql-setup.sh
+++ mysql-setup.sh
@@ -7,18 +7,14 @@ else
   echo "You have no config.sh. Copy config.defaults.sh to config.sh, change the passwords and try again." && exit 1;
 fi
 
-CMD="docker exec mysql mysql -u root -p'$T_MYSQL_PASSWORD' -e \"CREATE USER '$T_MYSQL_USER'@'%' IDENTIFIED WITH mysql_native_password BY '$T_MYSQL_PASSWORD';\""
-echo ''
-echo ''
-echo ''
-echo $CMD
-echo ''
-eval ${CMD}
+USER_EXISTS=$(docker exec mysql mysql -u root -p"$T_MYSQL_PASSWORD" -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$T_MYSQL_USER')")
 
-CMD="docker exec mysql mysql -u root -p'$T_MYSQL_PASSWORD' -e \"GRANT ALL PRIVILEGES ON \"*.*\" TO '$T_MYSQL_USER'@'%' WITH GRANT OPTION;\""
-echo ''
-echo ''
-echo ''
-echo $CMD
-echo ''
-eval ${CMD}
+if [ "$USER_EXISTS" = 1 ]; then
+  echo "MySQL user is already set up."
+else
+  echo "Setting up mysql user..."
+  CMD="docker exec mysql mysql -u root -p'$T_MYSQL_PASSWORD' -e \"CREATE USER '$T_MYSQL_USER'@'%' IDENTIFIED WITH mysql_native_password BY '$T_MYSQL_PASSWORD';\""
+  eval ${CMD}
+  CMD="docker exec mysql mysql -u root -p'$T_MYSQL_PASSWORD' -e \"GRANT ALL PRIVILEGES ON \"*.*\" TO '$T_MYSQL_USER'@'%' WITH GRANT OPTION;\""
+  eval ${CMD}
+fi
