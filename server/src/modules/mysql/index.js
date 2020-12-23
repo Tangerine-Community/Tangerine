1dd66fab4d65205f5a9e85864abd65c32c3a5208
R.J. Steinert <rj@rjsteinert.com>
2020-12-23 12:19:43 -0500

    Add use of enable hook to mysql module

--- server/src/modules/mysql/index.js
+++ server/src/modules/mysql/index.js
@@ -24,21 +24,40 @@ module.exports = {
       return data
     },
     clearReportingCache: async function(data) {
+      // @TODO
       const { groupNames } = data
       for (let groupName of groupNames) {
       }
       return data
     },
-    groupNew: function(data) {
-      return new Promise(async (resolve, reject) => {
-        const {groupName, appConfig} = data
-        const groupId = groupName
-        const mysqlDbName = groupId.replace(/-/g,'')
-        console.log(`Creating mysql db ${mysqlDbName}`)
-        await exec(`mysql -u ${process.env.T_MYSQL_USER} -h mysql -p"${process.env.T_MYSQL_PASSWORD}" -e "CREATE DATABASE ${mysqlDbName};"`)
-        console.log(`Created mysql db ${mysqlDbName}`)
-        console.log('Creating tangerine to mysql state file...')
-        const state = `[TANGERINE]
+    enable: async function() {
+      const groups = await groupsList()
+      for (groupId of groups) {
+        await initializeGroupForMySQL(groupId)
+      }
+    },
+    disable: function(data) {
+
+    },
+    groupNew: async function(data) {
+      const {groupName} = data
+      const groupId = groupName
+      await initializeGroupForMySQL(groupId)
+      const pathToStateFile = `/mysql-module-state/${groupId}.ini`
+      startTangerineToMySQL(pathToStateFile)
+      return data
+    }
+  }
+
+}
+
+async function initializeGroupForMySQL(groupId) {
+  const mysqlDbName = groupId.replace(/-/g,'')
+  console.log(`Creating mysql db ${mysqlDbName}`)
+  await exec(`mysql -u ${process.env.T_MYSQL_USER} -h mysql -p"${process.env.T_MYSQL_PASSWORD}" -e "CREATE DATABASE ${mysqlDbName};"`)
+  console.log(`Created mysql db ${mysqlDbName}`)
+  console.log('Creating tangerine to mysql state file...')
+  const state = `[TANGERINE]
 DatabaseURL = http://couchdb:5984/
 DatabaseName = ${groupId}-synapse
 DatabaseUserName = ${process.env.T_COUCHDB_USER_ADMIN_NAME} 
@@ -51,16 +70,10 @@ HostName = mysql
 DatabaseName = ${mysqlDbName} 
 UserName = ${process.env.T_MYSQL_USER} 
 Password = ${process.env.T_MYSQL_PASSWORD} 
-        `
-        const pathToStateFile = `/mysql-module-state/${groupId}.ini`
-        await fs.writeFile(pathToStateFile, state)
-        console.log('Created tangerine to mysql state file.')
-        startTangerineToMySQL(pathToStateFile)
-        resolve(data)
-      })
-    }
-  }
-
+  `
+  const pathToStateFile = `/mysql-module-state/${groupId}.ini`
+  await fs.writeFile(pathToStateFile, state)
+  console.log('Created tangerine to mysql state file.')
 }
 
 async function startTangerineToMySQL(pathToStateFile) {
