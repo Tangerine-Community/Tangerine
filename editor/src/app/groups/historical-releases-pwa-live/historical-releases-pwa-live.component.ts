e7acc9011d5d344447dd5e8283eb6d23e19b99cb
Evans D <evansdianga@gmail.com>
2020-12-24 09:42:32 +0300

    Added components for various historical releases

---
+++ editor/src/app/groups/historical-releases-pwa-live/historical-releases-pwa-live.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GroupsService } from '../services/groups.service';
+
+@Component({
+  selector: 'app-historical-releases-pwa-live',
+  templateUrl: './historical-releases-pwa-live.component.html',
+  styleUrls: ['./historical-releases-pwa-live.component.css']
+})
+export class HistoricalReleasesPwaLiveComponent implements OnInit {
+
+  displayedColumns = ['buildId', 'build', 'releaseType', 'date'];
+  groupsData: [];
+
+  constructor(private groupsService: GroupsService, private route: ActivatedRoute) { }
+
+  async ngOnInit() {
+    const result = await this.groupsService.getGroupInfo(this.route.snapshot.paramMap.get('groupId'));
+    this.groupsData = result.releases.filter(e => e.releaseType === 'prod' && e.build === 'PWA');
+  }
+
+}
