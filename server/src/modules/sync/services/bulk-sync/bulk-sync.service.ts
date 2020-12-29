0d355066a597f452a1f7af4402f8545c5b19bbe5
Chris E. Kelley <ckelley@rti.org>
2020-12-28 19:47:42 -0600

    added filter for form.id and location for bulk-sync
    Completed round-trip bulk load on client and server

--- server/src/modules/sync/services/bulk-sync/bulk-sync.service.ts
+++ server/src/modules/sync/services/bulk-sync/bulk-sync.service.ts
@@ -1,8 +1,4 @@
 import {HttpService, Injectable} from '@nestjs/common';
-import axios, {AxiosResponse} from "axios";
-import {v4 as UUID} from 'uuid'
-import {Observable} from "rxjs";
-// import {Observable} from '@reactivex/rxjs/es6/Observable.js'
 const PouchDB = require('pouchdb')
 const dbDefaults = require('../../../../db-defaults')
 const replicationStream = require('pouchdb-replication-stream');
@@ -13,12 +9,12 @@ PouchDB.adapter('writableStream', replicationStream.adapters.writableStream);
 
 @Injectable()
 export class BulkSyncService {
-  
   constructor(private httpService: HttpService) {}
   
-  async dump(groupId:string, deviceId:string, syncUsername:string, syncPassword:string): Promise<Observable<AxiosResponse<any>>>  {
+  async dump(groupId:string, deviceId:string, syncUsername:string, syncPassword:string) {
     console.log("Dumping database at route /api/sync.")
-    return this.httpService.get(`/api/sync/${groupId}/${deviceId}/${syncUsername}/${syncPassword}`)
+    const response = await this.httpService.get(`/api/sync/${groupId}/${deviceId}/${syncUsername}/${syncPassword}`).toPromise();
+    return response.data;
   }
-
+  
 }
