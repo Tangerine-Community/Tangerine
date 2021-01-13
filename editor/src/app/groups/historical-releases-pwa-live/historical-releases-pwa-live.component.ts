37782bf0d5fc0bc038ca3d409ad2d42a007575c7
Evans D <evansdianga@gmail.com>
2021-01-13 20:13:29 +0300

    Add pagination, filtering to all hostorical releases components and APK downloads

--- editor/src/app/groups/historical-releases-pwa-live/historical-releases-pwa-live.component.ts
+++ editor/src/app/groups/historical-releases-pwa-live/historical-releases-pwa-live.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
+import { BuildInfo } from '../build-info';
 import { GroupsService } from '../services/groups.service';
 
 @Component({
@@ -10,13 +13,23 @@ import { GroupsService } from '../services/groups.service';
 export class HistoricalReleasesPwaLiveComponent implements OnInit {
 
   displayedColumns = ['buildId', 'build', 'releaseType', 'date', 'versionTag', 'releaseNotes'];
-  groupsData: [];
+  groupsData;
+  groupId;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private groupsService: GroupsService, private route: ActivatedRoute) { }
 
   async ngOnInit() {
-    const result = await this.groupsService.getGroupInfo(this.route.snapshot.paramMap.get('groupId'));
-    this.groupsData = result.releases.filter(e => e.releaseType === 'prod' && e.build === 'PWA');
+    this.groupId = this.route.snapshot.paramMap.get('groupId');
+    const result = await this.groupsService.getGroupInfo(this.groupId);
+    this.groupsData = new MatTableDataSource<BuildInfo>(result.releases.
+      filter(e => e.releaseType === 'prod' && e.build === 'PWA').map( e => ({...e, dateString: new Date(e.date)})));
+    this.groupsData.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.groupsData.filter = filterValue.trim().toLowerCase();
   }
 
 }
