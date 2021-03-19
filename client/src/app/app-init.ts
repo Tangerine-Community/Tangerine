1c703dee13e749e1d43eadbefbd0e3fbcc99236a
R.J. Steinert <rj@rjsteinert.com>
2021-03-19 15:53:59 -0400

    Reimplement usage of deviceready listener in App Init of client

--- client/src/app/app-init.ts
+++ client/src/app/app-init.ts
@@ -26,36 +26,40 @@ export class AppInit {
       debugger
       console.log("AppInitService.init() called");
       if (window['isCordovaApp']) {
-        while (!window['sqliteStorageFile']) {
-          await sleep(1000)
-        }
-        // Check if App is installed using SQLite.
-        let isInstalledOnSqlite
-        // This will connect to SQLite by default until we set window.turnOffAppLevelEncryption.
-        const tangerineVariablesDbInSqlite = DB('tangerine-variables')
-        try {
-          // See if we are installed using SQLite.
-          await tangerineVariablesDbInSqlite.get('installed')
-          isInstalledOnSqlite = true
-        } catch(e) {
-          isInstalledOnSqlite = false
-        }
-        // Check in app-config.json if we should turn off encryption.
-        const appConfig = await getAppConfig();
-        // If the app is not installed and app config says we should turn off encryption, then set localStorage flag of turnOffEncryption.
-        // Note that checking if installed prevents app upgrading to an app-config.json that says turnOffAppLevelEncryption of doing so
-        // because that would appear to result in data loss.
-        if (isInstalledOnSqlite) {
-          localStorage.setItem('turnOffAppLevelEncryption', 'no')
-        } else if (appConfig['turnOffAppLevelEncryption']) {
-          localStorage.setItem('turnOffAppLevelEncryption', 'yes')
-        }
-        // If localStorage flag of turnOffAppLevelEncryption is set, then set window level variable to let the db.factory.ts DB function know.
-        if (localStorage.getItem('turnOffAppLevelEncryption') === 'yes') {
-          window['turnOffAppLevelEncryption'] = true
-        }
+        document.addEventListener('deviceready', async () => {
+          while (!window['sqliteStorageFile']) {
+            await sleep(1000)
+          }
+          // Check if App is installed using SQLite.
+          let isInstalledOnSqlite
+          // This will connect to SQLite by default until we set window.turnOffAppLevelEncryption.
+          const tangerineVariablesDbInSqlite = DB('tangerine-variables')
+          try {
+            // See if we are installed using SQLite.
+            await tangerineVariablesDbInSqlite.get('installed')
+            isInstalledOnSqlite = true
+          } catch(e) {
+            isInstalledOnSqlite = false
+          }
+          // Check in app-config.json if we should turn off encryption.
+          const appConfig = await getAppConfig();
+          // If the app is not installed and app config says we should turn off encryption, then set localStorage flag of turnOffEncryption.
+          // Note that checking if installed prevents app upgrading to an app-config.json that says turnOffAppLevelEncryption of doing so
+          // because that would appear to result in data loss.
+          if (isInstalledOnSqlite) {
+            localStorage.setItem('turnOffAppLevelEncryption', 'no')
+          } else if (appConfig['turnOffAppLevelEncryption']) {
+            localStorage.setItem('turnOffAppLevelEncryption', 'yes')
+          }
+          // If localStorage flag of turnOffAppLevelEncryption is set, then set window level variable to let the db.factory.ts DB function know.
+          if (localStorage.getItem('turnOffAppLevelEncryption') === 'yes') {
+            window['turnOffAppLevelEncryption'] = true
+          }
+          resolve()
+        })
+      } else { 
+        resolve()
       }
-      resolve()
     })
   }
 
