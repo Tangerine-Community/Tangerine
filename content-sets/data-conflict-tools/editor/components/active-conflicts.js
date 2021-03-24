cf5bd9b2732d67aee497634c1b9b4b9bccc668e1
R.J. Steinert <rj@rjsteinert.com>
2021-03-24 09:01:47 -0400

    Add data-conflict-tools content set

---
+++ content-sets/data-conflict-tools/editor/components/active-conflicts.js
@@ -0,0 +1,215 @@
+import { sharedStyles } from './shared-styles.js'
+import axios from 'axios'
+import * as Jsondiffpatch from 'jsondiffpatch'
+import PouchDB from 'pouchdb'
+var jsondiffpatch = Jsondiffpatch.create({});
+
+async function archiveConflicts(dbPath, docId) {
+  try {
+    const dbSource = new PouchDB(dbPath)
+    const dbConflictRevs = new PouchDB(`${dbPath}-conflict-revs`)
+    const doc = await dbSource.get(docId, {conflicts: true})
+    for (let conflictRev of doc._conflicts) {
+      const conflictRevDoc = await dbSource.get(docId, {rev: conflictRev})
+      conflictRevDoc.conflictDocId = docId 
+      conflictRevDoc.conflictRev = conflictRev
+      delete conflictRevDoc._id
+      delete conflictRevDoc._rev
+      await dbConflictRevs.post(conflictRevDoc)
+      await dbSource.remove(docId, conflictRev)
+    }
+    return doc._conflicts
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+import { LitElement, html } from 'lit-element'
+import { unsafeHTML } from 'lit-html/directives/unsafe-html';
+class ActiveConflicts extends LitElement {
+
+  static get styles() {
+    return [
+      sharedStyles
+    ]
+  }
+
+  static get properties(){
+    return {
+      selection: { type: Object },
+      ready: { type: Boolean },
+      list: { type: Array }
+    };
+  }
+
+  constructor() {
+    super()
+    this.loadCount = 0
+    this.searchString = '' 
+    this.ready = false
+    this.conflictInfos = []
+    this.matches = []
+    this.selection = { conflicts:[] }
+  }
+
+  async connectedCallback() {
+    super.connectedCallback()
+    this.loadList()
+  }
+
+  async loadList() {
+    this.ready = false
+    const groupId = window.location.pathname.split('/')[2]
+    const result = await axios.get(`/db/${groupId}/_design/conflicts/_view/conflicts`)
+    this.list = result.data.rows.map(row => {
+      return { 
+        _id: row.key, 
+        numberOfConflicts: row.value
+      }
+    })
+    this.ready = true
+  }
+
+  render() {
+    return html`
+      <style>
+        #container {
+          margin: 15px;
+          padding: 15px;
+        }
+        .selection {
+          padding: 0px 15px;
+        }
+        .title {
+          margin: 15px 0px 5px;
+        }
+        .no-matches {
+          width: 300px
+        }
+        tr.header td {
+         font-weight: bold;
+        }
+        table.matches td {
+          text-align: right;
+          padding: 5px;
+          border: solid 1px #CCC;
+        }
+        juicy-ace-editor {
+          width: 750px;
+          height: 750px;
+        }
+      </style>
+      <div id="container">
+        <h2>Docs with Active Conflicts</h2>
+        ${!this.ready ? html`
+          Loading active conflicts... 
+        `: ``}
+        <table>
+          <tr>
+            <td valign="top">
+              ${this.list.length > 0 ? html`
+                <table class="matches">
+                  <tr class="header">
+                    <td> Doc ID </td>
+                    <td> Number of active conflicts </td>
+                  </tr>
+                  ${this.list.map(item => html`
+                    <tr @click="${() => { this.loadDoc(item._id) }}">
+                      <td>${item._id}</td> 
+                      <td>${item.numberOfConflicts}</td>
+                    </tr>
+                  `)}
+                </table>
+              `: ``}
+            </td>
+            <td class="selection" valign="top">
+              ${this.selection.id ? html`
+                <h2 class="title">${this.selection.conflicts.length} Conflict Diff${this.selection.conflicts.length > 1 ? `s`:``} for ${this.selection.id}</h2>
+								<paper-button @click="${() => this.archiveConflictRevisions()}">ARCHIVE CONFLICT REVISIONS</paper-button>
+              `: ``}
+              ${this.selection.conflicts.map(conflict => html`
+                <h3>${conflict.rev}</h3>
+                ${unsafeHTML(Jsondiffpatch.formatters.html.format(conflict.diff, this.selection.doc))}
+              `)}
+            </td>
+            <td valign="top">
+              ${this.selection.doc ? html`
+                <h2>Current Document</h2>
+                <juicy-ace-editor mode="ace/mode/json" .value="${this.selection.JSON}">
+                  ${this.selection.JSON}
+                </juicy-ace-editor>
+                <paper-button @click="${() => this.saveDoc() }">SAVE</paper-button>
+              `: ``}
+
+              
+
+            </td>
+          </table>
+        </div>
+    `
+  }
+
+  async archiveConflictRevisions() {
+    const groupId = window.location.pathname.split('/')[2]
+    const dbPath = `${window.location.protocol}` + '//' + `${window.location.host}/db/${groupId}`
+    const docId = this.selection.id
+    const confirmation = confirm(`Are you sure you want to archive conflict revisions for ${docId}?`)
+    if (!confirmation) return
+    this.selection = { conflicts: []}
+    this.list = []
+    this.ready = false
+    await archiveConflicts(dbPath, docId)
+    alert('Conflicts archived.')
+    this.loadList()
+  }
+
+  async saveDoc() {
+    const groupId = window.location.pathname.split('/')[2]
+    let docToSave
+    try {
+      docToSave = JSON.parse(this.shadowRoot.querySelector('juicy-ace-editor').value)
+    } catch(e) {
+      alert('Invalid JSON. Doc not saved.')
+      return
+    }
+    try {
+      const response = axios.put(`/db/${groupId}/${docToSave._id}`, docToSave)
+    } catch (e) {
+      alert('Error saving')
+      console.error(e)
+      return
+    }
+    await this.loadDoc(docToSave._id)
+    alert('Saved successfully.')
+  }
+
+  async loadDoc(docId) {
+    const groupId = window.location.pathname.split('/')[2]
+    const currentDoc = (await axios.get(`/db/${groupId}/${docId}?conflicts=true`)).data
+    const conflictRevisionIds = [...currentDoc._conflicts] 
+    delete currentDoc._conflicts
+    let conflicts = []
+    if (conflictRevisionIds) {
+      for (const conflictRevisionId of conflictRevisionIds) {
+        const conflictRevisionDoc = (await axios.get(`/db/${groupId}/${docId}?rev=${conflictRevisionId}`)).data
+        const comparison = jsondiffpatch.diff(currentDoc, conflictRevisionDoc)
+        const conflict = {
+          doc: conflictRevisionDoc,
+          rev: conflictRevisionDoc._rev,
+          diff: comparison
+        }
+        conflicts.push(conflict)
+      }
+    }
+    this.selection = {
+      id: currentDoc._id,
+      rev: currentDoc._rev,
+      doc: currentDoc,
+      JSON: JSON.stringify(currentDoc, null, 2),
+      conflicts 
+    }
+  }
+
+}
+
+customElements.define('active-conflicts', ActiveConflicts);
---
+++ content-sets/data-conflict-tools/editor/components/custom-app.js
@@ -0,0 +1,107 @@
+import './archived-conflicts.js'
+import './active-conflicts.js'
+import './search-active-conflicts.js'
+import { LitElement, html } from 'lit-element'
+import { unsafeHTML } from 'lit-html/directives/unsafe-html';
+
+class CustomApp extends LitElement {
+
+  static get properties() {
+    return { 
+      route: { type: String },
+      ready: { type: String }
+    };
+  }
+
+  constructor() {
+    super()
+    window.App = this
+    this.ready = false
+    this.route = window.route || localStorage.getItem('route') || ''
+  }
+
+  async connectedCallback() {
+    super.connectedCallback()
+    this.ready = true
+  }
+
+  setRoute(route) {
+    window.route = route
+    localStorage.setItem('route', route)
+  }
+
+  go(route) {
+    this.setRoute(route)
+    this.route = route
+  }
+
+  set(name = '', value = '') {
+    localStorage.setItem(name, value)
+  }
+
+  get(name) {
+    return localStorage.getItem(name)
+  }
+
+  render() {
+    return html`
+
+      <style type="text/css">
+      	/**
+      	 * These styles just for the style guide
+      	 * The styles for the actual contents are in style.css
+      	 */
+      	body {
+      		background-color: #3f3f43;
+      		padding-top: 15px;
+      	}
+      	.wrapper {
+      		width: 1120px;
+      		padding: 20px;
+      		margin: 0 auto;
+      		background-color: #fff;
+      	}
+      	.tablet {
+      		width: 768px;
+      		margin: 0 auto;
+        }
+        home-button {
+          position: absolute;
+          top: 0px;
+          left: 0px;
+          z-index: 98765456789876545678;
+        }
+      </style>
+      ${!this.ready ? html`Loading...`:``}
+      ${this.ready ? html`
+        ${this.route === '' ? html`
+          <h1>Data Conflicts</h1>
+          <ul>
+            <li @click="${() => this.open('active-conflicts')}">Active Conflicts</li>
+            <li @click="${() => this.open('active-conflicts')}">Search Active Conflicts</li>
+            <li @click="${() => this.open('archived-conflicts')}">Archived Conflicts</li>
+          </ul>
+        `: ``}
+        ${this.route !== '' ? html`
+          <paper-button @click="${() => App.go('')}">< BACK</paper-button> 
+        `: ``}
+        ${this.route === 'archived-conflicts' ? html`
+          <archived-conflicts></archived-conflicts>
+        `: ``}
+        ${this.route === 'active-conflicts' ? html`
+          <active-conflicts></active-conflicts>
+        `: ``}
+        ${this.route === 'search-active-conflicts' ? html`
+          <search-active-conflicts></search-active-conflicts>
+        `: ``}
+      `: ``}
+    `
+  }
+
+  open(route) {
+    this.route = route
+  }
+
+}
+
+customElements.define('custom-app', CustomApp)
