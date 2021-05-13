425871ac4edc22f8e477650c07f11285e6769638
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-05-13 11:06:39 -0400

    Add CaseRoleDataDefinition's to CaseRole definition class

--- client/src/app/case/classes/case-role.class.ts
+++ client/src/app/case/classes/case-role.class.ts
@@ -3,5 +3,11 @@ export interface CaseRole {
   id:string
   label:string
   templateListItem:string
+  dataDefinitions: Array<CaseRoleDataDefinition>
 }
 
+export interface CaseRoleDataDefinition {
+  identifier:boolean
+  name:string
+  type:string
+}
