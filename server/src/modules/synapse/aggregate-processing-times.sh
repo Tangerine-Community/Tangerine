bd6c94ab23b254082057ba36d2110bb2d5553ad3
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-06-02 07:05:50 -0400

    Further aggregate processing times of synapse migrations categorized by tens digit

--- server/src/modules/synapse/aggregate-processing-times.sh
+++ server/src/modules/synapse/aggregate-processing-times.sh
@@ -1,1 +1,1 @@
-docker logs python-tangerine-synapse-connector | grep 'Processed change in' | awk '{print $7}' | sort | uniq -c
+docker logs python-tangerine-synapse-connector | grep 'Processed change in' | awk '{print $7}' | sort | cut -c1-1 | uniq -c | xargs -I {} echo "{}0+ seconds"
