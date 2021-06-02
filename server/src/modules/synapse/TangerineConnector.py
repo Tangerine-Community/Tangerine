ebf5dd9f026230dcdaf736284f42fd12c13a492b
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-06-02 07:55:43 -0400

    Prevent logs from being swallowed by removing try around processing of a synapse migration and prevent keys with whitespace from causing hangups on Synapse column names

--- server/src/modules/synapse/TangerineConnector.py
+++ server/src/modules/synapse/TangerineConnector.py
@@ -36,14 +36,22 @@ def save_entity(doc):
     else :
         data = doc 
     data['id'] = data.get('_id')
+    cleanData = {}
+    for key in data.keys():
+        cleanData[key.strip()] = data[key]
+    data = cleanData
     flexsert_span_table_record(syn, synProjectName, type, data, MAX_NUMBER_OF_COLUMNS)
 
-def save_response(resp_dict):
-    respid = resp_dict.get('_id')
-    response_data = resp_dict.get('data')
-    response_data['id'] = respid
-    formID = response_data.get('formId')
-    flexsert_span_table_record(syn, synProjectName, formID, response_data, MAX_NUMBER_OF_COLUMNS)
+def save_response(doc):
+    id = doc.get('_id')
+    data = doc.get('data')
+    data['id'] = id
+    cleanData = {}
+    for key in data.keys():
+        cleanData[key.strip()] = data[key]
+    data = cleanData
+    formID = data.get('formId')
+    flexsert_span_table_record(syn, synProjectName, formID, data, MAX_NUMBER_OF_COLUMNS)
 
 def update_state(last_change_seq):
     config.set("TANGERINE","lastsequence",last_change_seq)
@@ -57,31 +65,25 @@ def main_job():
     log('Logged into Tangerine database')
     changes = tangerine_database.changes(feed='continuous',include_docs=True,descending=False,since=lastSequence)
     for change in changes:
-        try:
-            start_time = timeit.default_timer()
-            seq = change.get('seq')
-            if seq :
-                log('Processing sequence:' + seq)
-            id = change.get('id')
-            if change.get('deleted'):
-                continue
-            doc = change.get('doc')
-            type = doc.get('type')
-            log("Processing type: " + type + ", id: " + id)
-            if (type.lower() == "response"):
-                save_response(doc)
-            elif type is not None :
-                save_entity(doc)
-            else:
-                log("Unexpected document type")
-            update_state(seq)
-            end_time = timeit.default_timer()
-            log('Processed change in ' + str(int(end_time - start_time)) + ' seconds')
-        except synapseclient.core.exceptions.SynapseHTTPError as e:
-            # probably not our fault -- save the data
-            print('SynapseHTTPError %s' % e)
-        except :
-            log('Something went wrong.')
+        start_time = timeit.default_timer()
+        seq = change.get('seq')
+        if seq :
+            log('Processing sequence:' + seq)
+        id = change.get('id')
+        if change.get('deleted'):
+            continue
+        doc = change.get('doc')
+        type = doc.get('type')
+        log("Processing type: " + type + ", id: " + id)
+        if (type.lower() == "response"):
+            save_response(doc)
+        elif type is not None :
+            save_entity(doc)
+        else:
+            log("Unexpected document type")
+        update_state(seq)
+        end_time = timeit.default_timer()
+        log('Processed change in ' + str(int(end_time - start_time)) + ' seconds')
 
 log('Loading configuration from connector.ini')
 config = configparser.ConfigParser()
