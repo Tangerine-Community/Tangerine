e2418ca04b7a315550968c77e11341fe083bc381
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-06-03 18:52:32 -0400

    Update aggreagate processing time script to grep for new keywords

--- server/src/modules/synapse/aggregate-processing-times.sh
+++ server/src/modules/synapse/aggregate-processing-times.sh
@@ -1,1 +1,1 @@
-docker logs python-tangerine-synapse-connector | grep 'Processed change in' | awk '{print $7}' | sort | sed 's/.$//' | uniq -c | xargs -I {} echo "{}0+ seconds"
+docker logs python-tangerine-synapse-connector | grep 'Processed doc in' | awk '{print $7}' | sort | sed 's/.$//' | uniq -c | xargs -I {} echo "{}0+ seconds"
