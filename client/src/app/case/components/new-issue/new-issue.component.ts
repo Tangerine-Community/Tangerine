37b4e356802c0c0860eab3cea256b87eda969662
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-06-22 13:52:22 -0400

    CLIENT: Add support for templating issue titles/description, automatically accept templated issue title/description

--- client/src/app/case/components/new-issue/new-issue.component.ts
+++ client/src/app/case/components/new-issue/new-issue.component.ts
@@ -4,6 +4,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CaseService } from '../../services/case.service';
 import { AppContext } from 'src/app/app-context.enum';
+import { TangyFormService } from 'src/app/tangy-forms/tangy-form.service';
 
 @Component({
   selector: 'app-new-issue',
@@ -13,23 +14,21 @@ import { AppContext } from 'src/app/app-context.enum';
 export class NewIssueComponent implements OnInit {
 
   @ViewChild('container', { static: true }) container:ElementRef
+  defaultTemplateIssueTitle = `Issue for \${caseDefinition.name}, by \${userName}`
+  defaultTemplateIssueDescription = `` 
+
+  renderedTemplateIssueTitle = ''
+  renderedTemplateIssueDescription = ''
 
   constructor(
     private route:ActivatedRoute,
     private router:Router,
     private caseService: CaseService,
-    private userService:UserService
+    private userService:UserService,
+    private formService:TangyFormService
   ) { }
 
   ngOnInit() {
-    this.container.nativeElement.innerHTML = `
-      <tangy-form id="form" #form>
-        <tangy-form-item id="new-issue" title="New Issue">
-          <tangy-input name="title" label="Title" inner-label=" "></tangy-input>
-          <tangy-input name="description" label="Description" inner-label=" "></tangy-input>
-        </tangy-form-item>
-      </tangy-form>
-    `
     this.route.params.subscribe(async params => {
       const caseId = params.caseId
       const eventId = params.eventId
@@ -37,18 +36,41 @@ export class NewIssueComponent implements OnInit {
       const userName = await this.userService.getCurrentUser()
       const userId = userName
       const groupId = window.location.pathname.split('/')[2]
-      this.container.nativeElement.querySelector('tangy-form').addEventListener('submit', async (event) => {
-        event.preventDefault()
-        const response = new TangyFormResponseModel(event.target.response)
-        const title = response.inputsByName.title.value
-        const description = response.inputsByName.description.value
-        const issue = await this.caseService.createIssue(title, description, caseId, eventId, eventFormId, userId, userName, [AppContext.Editor, AppContext.Client])
-        this.router.navigate(['issue', issue._id, 'form-revision'])
-      })
-
-
+      await this.caseService.load(caseId)
+      const caseInstance = this.caseService.case
+      const caseDefinition = this.caseService.caseDefinition
+      const caseEvent = caseInstance.events.find(caseEvent => caseEvent.id === eventId)
+      const caseEventDefinition = this.caseService.caseDefinition.eventDefinitions.find(eventDefinition => eventDefinition.id === caseEvent.caseEventDefinitionId)
+      const eventForm = caseEvent.eventForms.find(eventForm => eventForm.id === eventFormId)
+      const eventFormDefinition = caseEventDefinition.eventFormDefinitions.find(eventFormDefinition => eventFormDefinition.id === eventForm.eventFormDefinitionId)
+      const formResponse = await this.formService.getResponse(eventForm.formResponseId)
+      eval(`this.renderedTemplateIssueTitle = this.caseService.caseDefinition.templateIssueTitle ? \`${this.caseService.caseDefinition.templateIssueTitle}\` : \`${this.defaultTemplateIssueTitle}\``)
+      eval(`this.renderedTemplateIssueDescription = this.caseService.caseDefinition.templateIssueDescription ? \`${this.caseService.caseDefinition.templateIssueDescription}\` : \`${this.defaultTemplateIssueDescription}\``)
+      if (window.location.hash.split('/').includes('template')) {
+        await this.saveIssueAndRedirect(this.renderedTemplateIssueTitle, this.renderedTemplateIssueDescription, caseId, eventId, eventFormId, userId, userName)
+      } else {
+        this.container.nativeElement.innerHTML = `
+          <tangy-form id="form" #form>
+            <tangy-form-item id="new-issue" title="New Issue">
+              <tangy-input name="title" label="Title" inner-label=" " value="${this.renderedTemplateIssueTitle}"></tangy-input>
+              <tangy-input name="description" label="Description" inner-label=" " value="${this.renderedTemplateIssueDescription}"></tangy-input>
+            </tangy-form-item>
+          </tangy-form>
+        `
+        this.container.nativeElement.querySelector('tangy-form').addEventListener('submit', async (event) => {
+          event.preventDefault()
+          const response = new TangyFormResponseModel(event.target.response)
+          const title = response.inputsByName.title.value
+          const description = response.inputsByName.description.value
+          await this.saveIssueAndRedirect(title, description, caseId, eventId, eventFormId, userId, userName)
+        })
+      }
     })
+  }
 
+  async saveIssueAndRedirect(title, description, caseId, eventId, eventFormId, userId, userName) {
+    const issue = await this.caseService.createIssue(title, description, caseId, eventId, eventFormId, userId, userName, [AppContext.Editor, AppContext.Client])
+    this.router.navigate(['issue', issue._id, 'form-revision'])
   }
 
 }
