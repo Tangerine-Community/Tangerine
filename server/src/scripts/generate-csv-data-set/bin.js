41a3c70ffc9e0d9351b1afd6d3d7c267606ceff3
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-07-07 15:34:03 -0400

    Add mocked up generate-csv-data-set CLI
    (cherry picked from commit a935ef888ee4249f6736c046de33f1847bdd1add)

---
+++ server/src/scripts/generate-csv-data-set/bin.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const generateCsvDataSet = require('./generate-csv-data-set.js')
+
+if (process.argv[2] === '--help') {
+  console.log('Usage:')
+  console.log('  generate-csv <groupId> <formId[,formId]> <outputPath> <year> <month> [--include-pii]  ')
+  console.log('Example:')
+  console.log(`  generate-csv-data-set group-abdc form1,form2 ./output.csv 2018 Jan --include-pii`)
+  process.exit()
+}
+
+const params = {
+  dbName: process.argv[2],
+  formIds: process.argv[3].split(','),
+  outputPath: process.argv[4],
+  year: (process.argv[5]) ? process.argv[5] : null,
+  month: (process.argv[6]) ? process.argv[6] : null,
+  includePii: process.argv[7] ? true : false
+}
+
+async function go(state) {
+  try {
+    await generateCsvDataSet(params.dbName, params.formIds, params.outputPath, params.year, params.month, params.includePii) 
+    process.exit()
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+go(state)
