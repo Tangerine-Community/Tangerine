5d226e04cbe6171f89438518b26267bb21187d09
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-07-08 07:34:54 -0400

    Refactor generate-csv-data-set script to actually generate a batch of CSV files into a zip archive
    (cherry picked from commit 67b4ba9c55d3ca4f55658ed29c41b20a4cd65869)

--- server/src/scripts/generate-csv-data-set/generate-csv-data-set.js
+++ server/src/scripts/generate-csv-data-set/generate-csv-data-set.js
@@ -1,14 +1,33 @@
 const util = require('util');
-const fs = require('fs')
+const exec = util.promisify(require('child_process').exec)
+const fs = require('fs-extra');
+const sanitize = require('sanitize-filename');
 const writeFile = util.promisify(fs.writeFile);
 const writeState = async function (state) {
   await writeFile(state.statePath, JSON.stringify(state, null, 2))
 }
 const sleep = (milliseconds) => new Promise((res) => setTimeout(() => res(true), milliseconds))
 
-async function generateCsvDataSet(dbName = '', formIds = [], outputPath = '', year = '', month = '', includePii = false) {
+function generateCsv(dbName, formId, outputPath, year = '*', month = '*') {
+  return new Promise(function(resolve, reject) {
+    const batchSize = (process.env.T_CSV_BATCH_SIZE) ? process.env.T_CSV_BATCH_SIZE : 5
+    const sleepTimeBetweenBatches = 0
+    let cmd = `cd /tangerine/server/src/scripts/generate-csv/ && ./bin.js ${dbName} ${formId} "${outputPath}" ${batchSize} ${sleepTimeBetweenBatches}`
+    if (year !== '*' && month !== '*') {
+      cmd += ` ${sanitize(year)} ${sanitize(month)}`
+    }
+    exec(cmd).then(status => {
+      resolve(status)
+    }).catch(error => {
+      console.error(error)
+      reject(error)
+    })
+  })
+}
+
+async function generateCsvDataSet(groupId = '', formIds = [], outputPath = '', year = '*', month = '*', includePii = false) {
   let state = {
-    dbName,
+    dbName: groupId + '-reporting',
     formIds,
     outputPath,
     year,
@@ -28,17 +47,33 @@ async function generateCsvDataSet(dbName = '', formIds = [], outputPath = '', ye
   }
   await writeState(state)
   for (let formId of state.formIds) {
-    let complete = false
     state.csvs.find(csv => csv.formId === formId).inProgress = true
     await writeState(state)
-    while (complete === false) {
-      await sleep(10*1000)
-      complete = true
+    const csvOutputPath = `${outputPath.replace('.zip', '')}-${formId}.csv`
+    const csvStatePath = `${outputPath.replace('.zip', '')}-${formId}.state.json`
+    generateCsv(state.dbName, formId, csvOutputPath, year, month)
+    while (!await fs.pathExists(csvStatePath)) {
+      await sleep(1*1000)
     }
-    state.csvs.find(csv => csv.formId === formId).complete = true
+    while (state.csvs.find(csv => csv.formId === formId).complete === false) {
+      const csvState = await fs.readJSON(csvStatePath)
+      state.csvs = state.csvs.map(csvInfo => {
+        return csvInfo.formId === formId
+          ? {
+            ...csvInfo,
+            ...csvState
+          }
+          : csvInfo
+      })
+      await writeState(state)
+      if (state.csvs.find(csv => csv.formId === formId).complete === false) {
+        await sleep(3*1000)
+      }
+    }
     state.csvs.find(csv => csv.formId === formId).inProgress = false
     await writeState(state)
   }
+  await exec(`zip ${outputPath} ${state.csvs.map(csvInfo => csvInfo.outputPath).join(' ')}`)
   state.complete = true
   await writeState(state)
 }
