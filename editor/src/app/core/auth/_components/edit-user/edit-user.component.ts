11ab5ef755efcdd708505ef85876b17d8d0a1841
Chris E. Kelley <ckelley@rti.org>
2021-08-27 12:50:47 -0500

    Added password policy to the admin edit user component
    Clear out statusMessage when policy is adhered to.

--- editor/src/app/core/auth/_components/edit-user/edit-user.component.ts
+++ editor/src/app/core/auth/_components/edit-user/edit-user.component.ts
@@ -4,6 +4,7 @@ import { UserService } from '../../_services/user.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TangyErrorHandler } from 'src/app/shared/_services/tangy-error-handler.service';
 import { _TRANSLATE } from 'src/app/shared/_services/translation-marker';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-edit-user',
@@ -13,17 +14,38 @@ import { _TRANSLATE } from 'src/app/shared/_services/translation-marker';
 export class EditUserComponent implements OnInit {
   user: User;
   updateUserPassword = false;
+  statusMessage = { type: '', message: '' };
+  disableSubmit = false;
+  passwordIsNotStrong = { type: 'error', message: _TRANSLATE('Password is not strong enough.') };
+  incorrectAdminPassword = { type: 'error', message: _TRANSLATE('Incorrect Admin Password') };
+  passwordPolicy: string
+  passwordRecipe: string
   constructor(
     private userService: UserService,
     private route: ActivatedRoute,
+    private errorHandler: TangyErrorHandler,
     private router: Router,
-    private errorHandler: TangyErrorHandler
+    private httpClient: HttpClient
   ) { }
 
   async ngOnInit() {
     this.user = await this.userService.getAUserByUsername(this.route.snapshot.paramMap.get('username')) as User;
     this.user.password = '';
     this.user.confirmPassword = '';
+    try {
+      const result: any = await this.httpClient.get('/configuration/passwordPolicyConfig').toPromise();
+      if(result.T_PASSWORD_POLICY){
+        this.passwordPolicy = result.T_PASSWORD_POLICY;
+      }
+      if(result.T_PASSWORD_RECIPE){
+        this.passwordRecipe = result.T_PASSWORD_RECIPE;
+      }
+    } catch (error) {
+      if (typeof error.status === 'undefined') {
+        this.errorHandler.handleError(_TRANSLATE('Could Not Contact Server.'));
+      }
+    }
+    this.passwordIsNotStrong.message = this.passwordIsNotStrong.message + ' ' + this.passwordRecipe
   }
 
   async editUser() {
@@ -31,6 +53,16 @@ export class EditUserComponent implements OnInit {
       if (!this.updateUserPassword) {
         this.user.password = null;
         this.user.confirmPassword = null;
+      }
+      const policy = new RegExp(this.passwordPolicy)
+      if (!policy.test(this.user.password)) {
+        this.statusMessage = this.passwordIsNotStrong
+        // this.disableSubmit = true
+        // this.errorHandler.handleError(this.passwordIsNotStrong.message);
+        return
+      } else {
+        // Clear out statusMessage is it had been set earlier.
+        this.statusMessage = { type: '', message: '' };
       }
       const data = await this.userService.updateUserDetails({...this.user, updateUserPassword: this.updateUserPassword});
       if (data === 200) {
