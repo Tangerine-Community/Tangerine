1c0ee0ca9423c0a3271989e45562e79e3f2c7afe
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-09-27 13:29:34 -0400

    Harden the syncLocationsDontMatch algorithm to cover more cases

--- client/src/app/sync/sync-couchdb.service.ts
+++ client/src/app/sync/sync-couchdb.service.ts
@@ -41,21 +41,30 @@ export class SyncSessionInfo {
   deviceSyncLocations:Array<LocationConfig> 
 }
 
-function syncLocationsDontMatch(a:Array<LocationConfig>, b:Array<LocationConfig>) {
-  let syncLocationsDontMatch = false
+function syncLocationsDontMatch(locationConfigA:LocationConfig, locationConfigB:LocationConfig) {
+  const lowestANode = locationConfigA.value[locationConfigA.value.length-1]
+  const lowestBNode = locationConfigA.value[locationConfigA.value.length-1]
+  return lowestANode.value !== lowestBNode.value
+    ? true
+    : false
+}
+
+function syncLocationConfigsDontMatch(a:Array<LocationConfig>, b:Array<LocationConfig>) {
+  let syncLocationConfigsDontMatch = false
   for (let locationConfigA of a) {
-    const lowestLevel = locationConfigA.showLevels[locationConfigA.showLevels.length-1] 
-    if (!b.find(locationConfigB => locationConfigB.value.find(node => node.level === lowestLevel).value === locationConfigA.value.find(node => node.level === lowestLevel).value)) {
-      syncLocationsDontMatch = true
+    // If there isn't at least one matching sync location, then sync locations don't match.
+    if (b.some(locationConfigB => syncLocationsDontMatch(locationConfigB, locationConfigA))) {
+      syncLocationConfigsDontMatch = true
     }
   }
+  // And now the same, but the other way around.
   for (let locationConfigB of b) {
-    const lowestLevel = locationConfigB.showLevels[locationConfigB.showLevels.length-1] 
-    if (!a.find(locationConfigA => locationConfigA.value.find(node => node.level === lowestLevel).value === locationConfigB.value.find(node => node.level === lowestLevel).value)) {
-      syncLocationsDontMatch = true
+    // If there isn't at least one matching sync location, then sync locations don't match.
+    if (a.some(locationConfigA => syncLocationsDontMatch(locationConfigB, locationConfigA))) {
+      syncLocationConfigsDontMatch = true
     }
   }
-  return syncLocationsDontMatch
+  return syncLocationConfigsDontMatch
 } 
 
 @Injectable({
