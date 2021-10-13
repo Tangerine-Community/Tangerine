61f4f54358eed74a3bf68873aa0c15b084b9ecb7
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-10-13 10:32:14 -0400

    Fix calculating of "percentile", AKA percent correct grouping

--- client/src/app/class/_services/dashboard.service.ts
+++ client/src/app/class/_services/dashboard.service.ts
@@ -571,7 +571,7 @@ export class DashboardService {
       percentile = 3;
          } else if (score >= 40 && score <= 59) {
       percentile = 2;
-         } else if (score >= 20 && score <= 29) {
+         } else if (score >= 20 && score <= 39) {
       percentile = 1;
          } else {
       percentile = 0;
