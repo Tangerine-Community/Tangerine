e0cc396f75dc63a082904b0c918d449189dfcbde
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-10-26 14:56:29 -0400

    Add support for enabling cyptopouch plugin for encryption

--- client/src/app/shared/_factories/db.factory.ts
+++ client/src/app/shared/_factories/db.factory.ts
@@ -3,6 +3,7 @@ import PouchDB from 'pouchdb';
 // @ts-ignore
 // import * as PouchDBFind from 'pouchdb-find';
 import * as cordovaSqlitePlugin from 'pouchdb-adapter-cordova-sqlite';
+import CryptoPouch from 'crypto-pouch';
 import * as PouchDBUpsert from 'pouchdb-upsert';
 import debugPouch from 'pouchdb-debug';
 PouchDB.plugin(debugPouch);
@@ -17,36 +18,22 @@ PouchDB.plugin({
 });
 PouchDB.adapter('writableStream', window['PouchReplicationStream'].adapters.writableStream);
 PouchDB.defaults({auto_compaction: true, revs_limit: 1});
+PouchDB.plugin(CryptoPouch)
 
-export function DB(name, key = ''):PouchDB {
-
-  function openCallback (connectionId) {
-    console.log('open connection id: ' + connectionId)
+export function connectToSqlCipherDb(name, key = ''):PouchDB {
+  let pouchDBOptions = <any>{
+    adapter: 'cordova-sqlite',
+    location: 'default',
+    androidDatabaseImplementation: 2
+  };
+  if (key) {
+    pouchDBOptions.key = key
   }
-
-  function errorCallback (e) {
-    console.log('UNEXPECTED SQLitePlugin ERROR: ' + e)
-  }
-
-  let pouchDBOptions = <any>{};
-  if (window['isCordovaApp'] && window['sqliteStorageFile'] && !window['turnOffAppLevelEncryption']) {
-    pouchDBOptions = {
-      adapter: 'cordova-sqlite',
-      location: 'default',
-      androidDatabaseImplementation: 2
-    };
-    if (key) {
-      pouchDBOptions.key = key
-    }
-  }
-  // Adding changes_batch_size here so it can be accessed by PWA for testing.
   if (window['changes_batch_size'] && name === 'shared-user-database') {
     pouchDBOptions.changes_batch_size = window['changes_batch_size']
   }
-  let pouch;
-
   try {
-    pouch = new PouchDB(name, pouchDBOptions);
+    const pouch = new PouchDB(name, pouchDBOptions);
     return pouch
   } catch (e) {
     console.log("Database error: " + e);
@@ -54,4 +41,47 @@ export function DB(name, key = ''):PouchDB {
   }
 }
 
+export function connectToCryptoPouchDb(name, key = ''):PouchDB {
+  const pouchDBOptions = <any>{
+    view_adapter: 'cordova-sqlite',
+    location: 'default',
+    androidDatabaseImplementation: 2
+  };
+  if (window['changes_batch_size'] && name === 'shared-user-database') {
+    pouchDBOptions.changes_batch_size = window['changes_batch_size']
+  }
+  try {
+    const pouch = new PouchDB(name, pouchDBOptions);
+    if (key) {
+      pouch.crypto(key)
+    }
+    return pouch
+  } catch (e) {
+    console.log("Database error: " + e);
+    console.trace();
+  }
+}
 
+export function connectToPouchDb(name):PouchDB {
+  const pouchDBOptions = <any>{}
+  if (window['changes_batch_size'] && name === 'shared-user-database') {
+    pouchDBOptions.changes_batch_size = window['changes_batch_size']
+  }
+  try {
+    const pouch = new PouchDB(name, pouchDBOptions);
+    return pouch
+  } catch (e) {
+    console.log("Database error: " + e);
+    console.trace();
+  }
+}
+
+export function DB(name, key = ''):PouchDB {
+  if (window['cryptoPouchRunning']) {
+    return connectToCryptoPouchDb(name, key)
+  } else if (window['sqlCipherRunning']) {
+    return connectToSqlCipherDb(name, key)
+  } else {
+    return connectToPouchDb(name)
+  }
+}
