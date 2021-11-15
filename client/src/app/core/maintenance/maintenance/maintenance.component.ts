d7f436c68d94367a10240213ff9cd58aec76c46e
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2021-11-15 16:23:16 -0500

    Add check permissions code on maintenance page

--- client/src/app/core/maintenance/maintenance/maintenance.component.ts
+++ client/src/app/core/maintenance/maintenance/maintenance.component.ts
@@ -22,6 +22,48 @@ export class MaintenanceComponent implements OnInit {
     this.isCordovaApp = window['isCordovaApp']
   }
 
+  async checkPermissions() {
+    var sleep = function(delay) { return new Promise((resolve, reject) => setTimeout(resolve, delay))}
+    // Notifications API.
+    let process = this.processMonitorService.start('permissionCheck', _TRANSLATE('Checking notifications permission...'))
+    while((await Notification.requestPermission()) === 'denied') {
+      // Do nothing.
+      await sleep(1*1000)
+    }
+    this.processMonitorService.stop(process.id)
+    // Camera and Microphone API.
+    process = this.processMonitorService.start('permissionCheck', _TRANSLATE('Checking camera permission...'))
+    while ((await navigator.permissions.query({name:'camera'})).state !== 'granted') {
+      navigator.getUserMedia(
+        {
+            video: true,
+            audio: false
+        },
+        function() { },
+        function() { }
+      )
+      await sleep(1*1000)
+    }
+    this.processMonitorService.stop(process.id)
+    // Geolocation API.
+    process = this.processMonitorService.start('permissionCheck', _TRANSLATE('Checking geolocation permission...'))
+    while ((await navigator.permissions.query({name:'geolocation'})).state !== 'granted') {
+      try {
+        navigator.geolocation.getCurrentPosition(() => { })
+      } catch (e) {
+        // Do nothing.
+      }
+      await sleep(1*1000)
+    }
+    this.processMonitorService.stop(process.id)
+    // Persistent Storage API.
+    process = this.processMonitorService.start('permissionCheck', _TRANSLATE('Checking permanent storage permission...'))
+    while (!await navigator.storage.persist()) {
+      await sleep(1*1000)
+    }
+    this.processMonitorService.stop(process.id)
+  }
+
   async compact() {
     const process = this.processMonitorService.start('compact', _TRANSLATE('Compacting databases...'))
     const db = await this.userService.getUserDatabase()
