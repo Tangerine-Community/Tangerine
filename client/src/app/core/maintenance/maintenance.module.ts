26a418ab23d4a24799450a69120bbadf757fbdf1
Chris E. Kelley <ckelley@rti.org>
2021-11-16 12:04:14 -0600

    Upgraded UX for maintenance page with mat-cards
    Added confirmation for actions that may have higher risk or take longer time.

--- client/src/app/core/maintenance/maintenance.module.ts
+++ client/src/app/core/maintenance/maintenance.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { MaintenanceRoutingModule } from './maintenance-routing.module';
 import { MaintenanceComponent } from './maintenance/maintenance.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from "@angular/material/divider";
 
 
 @NgModule({
@@ -12,7 +15,10 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     MaintenanceRoutingModule,
-    SharedModule
+    SharedModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule
   ]
 })
 export class MaintenanceModule { }
