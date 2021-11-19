5e776079dd83f9c447ffcb66e30a9356babfa026
Chris E. Kelley <ckelley@rti.org>
2021-11-19 15:19:15 -0600

    backport of device listing fix: chmod the upgrade script
    also fix install of view in -devices db

--- server/src/insert-group-views.js
+++ server/src/insert-group-views.js
@@ -4,10 +4,13 @@ const views = require(`./group-views.js`)
 const dbConnection = require('./db')
 
 module.exports = async function insertGroupViews(databaseName) {
-  let groupDb = new dbConnection(databaseName)
+  let db = new dbConnection(databaseName)
   let designDoc = {}
   for (let prop in views) {
     const view = views[prop]
+    if (view.database) {
+      db = new dbConnection(`${databaseName}-${view.database}`)
+    }
     // Views may just be a map function, else they are an object with a map and a reduce.
     if (typeof view === 'function') {
       designDoc = {
@@ -23,23 +26,27 @@ module.exports = async function insertGroupViews(databaseName) {
         _id: `_design/${prop}`,
         views: {
           [prop]: {
-            map: view.map.toString(),
-            reduce: view.reduce.toString()
+            ...view.map ? { map: view.map.toString() } : {},
+            ...view.reduce ? {reduce: view.reduce.toString() } : {}
           }
         }
       }
     }
     try {
-      const existingDesignDoc = await groupDb.get(`_design/${prop}`)
+      const existingDesignDoc = await db.get(`_design/${prop}`)
       designDoc._rev = existingDesignDoc._rev
     } catch(err) {
       // Do nothing... Assume this is the first time the views have been inserted.
     }
     try {
-      let status = await groupDb.post(designDoc)
-      log.info(`group views inserted into ${databaseName}`)
+      let status = await db.post(designDoc)
+      log.info(`group views inserted into ${db.name}`)
     } catch (error) {
       log.error(error)
+    }
+    // Set the database object back to default.
+    if (view.database) {
+      db = new dbConnection(databaseName)
     }
   }
 }
