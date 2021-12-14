83014a41eee4ab2c7a1f589cda07358668a0d00f
esurface <esurface@rti.org>
2021-12-14 11:38:03 -0500

    updates

--- server/src/modules/mysql/index.js
+++ server/src/modules/mysql/index.js
@@ -65,18 +65,18 @@ module.exports = {
           if (doc.type === 'case') {
             // output case
             await saveFlatResponse(doc, locationList, targetDb, sanitized);
-            let numInf = getItemValue(doc, 'numinf') // Why is this here --- this is very site specific to ARC, no?
-            let participant_id = getItemValue(doc, 'participant_id')
-            if (participant_id && process.env.T_MYSQL_MULTI_PARTICIPANT_SCHEMA) {
-              participant_id = doc._id + '-' + participant_id
-            }
 
             // output participants
             for (const participant of doc.participants) {
+              let participant_id = participant.id
+              if (process.env.T_MYSQL_MULTI_PARTICIPANT_SCHEMA) {
+                participant_id = doc._id + '-' + participant.id
+              }
               await pushResponse({
                 ...participant,
-                _id: uuidv4(),
+                _id: participant_id,
                 caseId: doc._id,
+                participantId: participant.id,
                 type: "participant",
                 archived: doc.archived||''
               }, targetDb);
