1f42ab1f63a21de4efd4c724cc500c9a01bcfa63
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2022-01-07 12:11:38 -0500

    Fix add-generate-csv-datasets, mvp achieved

--- server/src/scripts/generate-csv-data-sets/generate-csv-data-sets.js
+++ server/src/scripts/generate-csv-data-sets/generate-csv-data-sets.js
@@ -2,7 +2,7 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec)
 const fs = require('fs-extra');
 const generateCsvDataSet = require('../generate-csv-data-set/generate-csv-data-set');
-const groupsList = require('src/groups-list');
+const groupsList = require('../../groups-list.js');
 const writeFile = util.promisify(fs.writeFile);
 
 const writeState = async function (state) {
@@ -15,22 +15,22 @@ async function generateCsvDataSets(filename) {
     groups: groupIds.map(groupId => { return { id: groupId } }),
     complete: false,
     startTime: new Date().toISOString(),
-    outputPath: `/csvs/${filename}.zip`,
-    statePath: `/csvs/${filename}.json`
+    outputPath: `/csv/${filename}.zip`,
+    statePath: `/csv/${filename}.json`
   }
   // Fetch all form IDs for each group.
-  for (let group of groups) {
-    group.formIds =  (await fs.readJson(`/tangerine/client/content/groups/${groupId}/forms.json`)).map(form => form.id)
-    group.outputPath = `/csvs/csv-data-sets-${filename}-${group.id}.zip`
+  for (let group of state.groups) {
+    group.formIds =  (await fs.readJson(`/tangerine/client/content/groups/${group.id}/forms.json`)).map(form => form.id)
+    group.outputPath = `/csv/csv-data-sets-${filename}-${group.id}.zip`
     group.complete = false
   }
   for (let group of state.groups) {
     await writeState(state)
-    await generateCsvDataSet(group.id, group.formIds, group.outputPath, '*', '*', true)
+    await generateCsvDataSet(group.id, group.formIds, group.outputPath, '*', '*', false)
     group.complete = true
     await writeState(state)
   }
-  await exec(`zip ${outputPath} ${state.groups.map(group => group.outputPath.replace('.json', '.zip')).join(' ')}`)
+  await exec(`zip ${state.outputPath} ${state.groups.map(group => group.outputPath.replace('.json', '.zip')).join(' ')}`)
   state.complete = true
   await writeState(state)
 }
