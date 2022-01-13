5ddb858c4b7e4ab1a69326eadefdce7dd19df3f1
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2022-01-13 15:26:18 -0500

    Flip naming of includePii to excludePii, how it is actually used and works

--- server/src/scripts/generate-csv-data-set/bin.js
+++ server/src/scripts/generate-csv-data-set/bin.js
@@ -4,10 +4,10 @@ const generateCsvDataSet = require('./generate-csv-data-set.js')
 
 if (process.argv[2] === '--help') {
   console.log('Usage:')
-  console.log('  generate-csv-data-set <groupId> <formId[,formId]> <outputPath> <year> <month> [--include-pii]  ')
+  console.log('  generate-csv-data-set <groupId> <formId[,formId]> <outputPath> <year> <month> [--exclude-pii]  ')
   console.log('Examples:')
-  console.log(`  generate-csv-data-set group-abdc form1,form2 ./output.csv 2018 Jan --include-pii`)
-  console.log(`  generate-csv-data-set group-abdc form1,form2 ./output.csv * * --include-pii`)
+  console.log(`  generate-csv-data-set group-abdc form1,form2 ./output.csv 2018 Jan --exclude-pii`)
+  console.log(`  generate-csv-data-set group-abdc form1,form2 ./output.csv * * --exclude-pii`)
   process.exit()
 }
 
@@ -17,12 +17,12 @@ const params = {
   outputPath: process.argv[4],
   year: (process.argv[5]) ? process.argv[5] : null,
   month: (process.argv[6]) ? process.argv[6] : null,
-  includePii: process.argv[7] ? true : false
+  excludePii: process.argv[7] ? true : false
 }
 
 async function go(params) {
   try {
-    await generateCsvDataSet(params.dbName, params.formIds, params.outputPath, params.year, params.month, params.includePii) 
+    await generateCsvDataSet(params.dbName, params.formIds, params.outputPath, params.year, params.month, params.excludePii) 
     process.exit()
   } catch (error) {
     console.error(error)
