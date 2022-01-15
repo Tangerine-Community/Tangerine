deed6e525cc54ed31a97a92145f607fd547fd9b2
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2022-01-15 16:47:00 -0500

    Fix loading of deploy page to prevent jumping of menu items

--- editor/src/app/groups/group-deploy/group-deploy.component.ts
+++ editor/src/app/groups/group-deploy/group-deploy.component.ts
@@ -2,6 +2,7 @@ import { Breadcrumb } from './../../shared/_components/breadcrumb/breadcrumb.com
 import { _TRANSLATE } from 'src/app/shared/_services/translation-marker';
 import { Component, OnInit } from '@angular/core';
 import { ServerConfigService } from 'src/app/shared/_services/server-config.service';
+import { ProcessMonitorService } from 'src/app/shared/_services/process-monitor.service';
 
 
 @Component({
@@ -15,14 +16,21 @@ export class GroupDeployComponent implements OnInit {
   breadcrumbs:Array<Breadcrumb> = []
   syncProtocol2Enabled: boolean
   groupId:string
+  ready = false
 
-  constructor(private serverConfig:ServerConfigService ) { }
+  constructor(
+    private serverConfig:ServerConfigService,
+    private processMonitor:ProcessMonitorService
+  ) { }
 
   async ngOnInit() {
     this.breadcrumbs = []
     this.groupId = window.location.pathname.split('/')[2]
+    const process = this.processMonitor.start('group-deploy', 'Loading...')
     const config = await this.serverConfig.getServerConfig()
     this.syncProtocol2Enabled = !!(config.enabledModules.find(module=>module==='sync-protocol-2'))
+    this.processMonitor.stop(process.id)
+    this.ready = true
   }
 
 }
