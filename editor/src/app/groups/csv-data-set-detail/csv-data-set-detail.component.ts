c69c7016b064d8a03b62b7051948fe7a7d1026bb
R.J. (Steinert) Corwin <rj@rjsteinert.com>
2022-01-22 12:08:49 -0500

    Fix polling on dataset details page, refactor terminology, add a dynamic download all button

--- editor/src/app/groups/csv-data-set-detail/csv-data-set-detail.component.ts
+++ editor/src/app/groups/csv-data-set-detail/csv-data-set-detail.component.ts
@@ -4,6 +4,8 @@ import { Breadcrumb } from 'src/app/shared/_components/breadcrumb/breadcrumb.com
 import { _TRANSLATE } from 'src/app/shared/_services/translation-marker';
 import { GroupsService } from '../services/groups.service';
 import { TangerineFormsService } from '../services/tangerine-forms.service';
+import { DatePipe } from '@angular/common';
+import { ProcessMonitorService } from 'src/app/shared/_services/process-monitor.service';
 
 @Component({
   selector: 'app-csv-data-set-detail',
@@ -13,54 +15,60 @@ import { TangerineFormsService } from '../services/tangerine-forms.service';
 export class CsvDataSetDetailComponent implements OnInit, OnDestroy {
   breadcrumbs: Array<Breadcrumb> = [
     <Breadcrumb>{
-      label: _TRANSLATE('Download CSV Data Set'),
+      label: _TRANSLATE('Data Downloads'),
       url: 'csv-data-sets'
     }]
-  title = _TRANSLATE('View CSV Data Set Details')
+  title = '' 
   groupId;
   datasetId
   datasetDetail
   displayedColumns = ['formTitle','csvTemplateTitle','inProgress','outputPath']
   detailInterval:any
+  stopPolling = false
   constructor(
     private route: ActivatedRoute,
     private formService: TangerineFormsService,
+    private processMonitor: ProcessMonitorService,
     private groupsService: GroupsService
   ) { 
     this.datasetId = this.route.snapshot.paramMap.get('dataSetId')
     this.groupId = this.route.snapshot.paramMap.get('groupId')
-    this.breadcrumbs = [
-      ...this.breadcrumbs,
-      <Breadcrumb>{
-        label: _TRANSLATE('View CSV Data Set Details'),
-        url: `csv-data-sets/${this.datasetId}`
-      }
-    ]
   }
 
   async ngOnInit(){
+    const process = this.processMonitor.start('csv-data-set-detail', 'Loading details...')
     await this.getDatasetDetail()
-    this.detailInterval = setInterval(() => this.getDatasetDetail, 15*1000)
-    if(this.datasetDetail.complete){
-      clearInterval(this.detailInterval)
-    }
+    this.processMonitor.stop(process.id)
   }
 
-  ngOnDestroy() {
-    clearInterval(this.detailInterval)
+  ngOnDestroy(){
+    this.stopPolling = true
   }
 
   async getDatasetDetail(){
-    const datasetDetail = await this.groupsService.getDatasetDetail(this.datasetId)
+    const datasetDetail = <any>await this.groupsService.getDatasetDetail(this.datasetId)
+    const datePipe = new DatePipe('en-US')
+    if (!this.title) {
+      this.title = `Spreadsheets requested on ${datePipe.transform(datasetDetail.dateCreated, 'medium')}`
+      this.breadcrumbs = [
+        ...this.breadcrumbs,
+        <Breadcrumb>{
+          label: this.title,
+          url: `csv-data-sets/${this.datasetId}`
+        }
+      ]
+    }
     const csvTemplates = await this.formService.listCsvTemplates(this.groupId)
     const formsInfo = await this.formService.getFormsInfo(this.groupId)
-
     datasetDetail['csvs'] = datasetDetail['csvs'].map(csv =>{return {
       ...csv,
       csvTemplateTitle: csvTemplates.find(t => t._id === csv.csvTemplateId)?.title || _TRANSLATE('All data'),
       formTitle: formsInfo.find(f => f.id === csv.formId)?.title || csv.formId
     }})
     this.datasetDetail = datasetDetail
+    if(!this.datasetDetail.complete && !this.stopPolling){
+      setTimeout(() => this.getDatasetDetail(), 5*1000)
+    }
   }
 
 }
