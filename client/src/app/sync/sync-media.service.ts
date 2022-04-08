ee6db13158cb7d028059e725ae135bb5630cf269
Chris E. Kelley <ckelley@rti.org>
2022-04-08 17:10:50 -0500

    Comparing the uploaded files' md5 hash.

--- client/src/app/sync/sync-media.service.ts
+++ client/src/app/sync/sync-media.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpEventType, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse, HttpEventType, HttpHeaders} from "@angular/common/http";
 import {DeviceService} from "../device/services/device.service";
 import {AppConfigService} from "../shared/_services/app-config.service";
 import {UserService} from "../shared/_services/user.service";
@@ -8,12 +8,12 @@ import {CaseService} from "../case/services/case.service";
 import {CaseDefinitionsService} from "../case/services/case-definitions.service";
 import {TangyFormService} from "../tangy-forms/tangy-form.service";
 import {VariableService} from "../shared/_services/variable.service";
-import {Subject, Subscription} from "rxjs";
+import {Subject, Subscription, throwError} from "rxjs";
 import {SyncDirection} from "./sync-direction.enum";
 import {ReplicationStatus} from "./classes/replication-status.class";
 import {UserDatabase} from "../shared/_classes/user-database.class";
 import {_TRANSLATE} from "../shared/translation-marker";
-import {finalize} from "rxjs/operators";
+import {catchError, finalize} from "rxjs/operators";
 import * as SparkMD5 from 'spark-md5';
 import {AppConfig} from "../shared/_classes/app-config.class";
 
@@ -26,10 +26,10 @@ export class SyncMediaService {
 
   constructor(
     private http: HttpClient,
-    private deviceService:DeviceService,
-    private appConfigService:AppConfigService,
-    private userService:UserService,
-    private tangyFormsInfoService:TangyFormsInfoService,
+    private deviceService: DeviceService,
+    private appConfigService: AppConfigService,
+    private userService: UserService,
+    private tangyFormsInfoService: TangyFormsInfoService,
     private tangyFormService: TangyFormService,
     private variableService: VariableService
   ) {
@@ -41,21 +41,21 @@ export class SyncMediaService {
   public readonly syncMessage$: Subject<any> = new Subject();
   public readonly onCancelled$: Subject<any> = new Subject();
   replicationStatus: ReplicationStatus
-  syncMediaServiceStartTime:string
-  syncMediaServiceEndTime:string
+  syncMediaServiceStartTime: string
+  syncMediaServiceEndTime: string
   mediaFilesDir: string = 'Documents/Tangerine/media/'
   mediaFilesDirEntry: any
-  uploadProgress:any = {};
+  uploadProgress: any = {};
   uploadSub: Subscription;
   statusMessage: string;
   private lastEntry: boolean;
 
 
-  async sync():Promise<any> {
+  async sync(): Promise<any> {
     const appConfig = await this.appConfigService.getAppConfig()
     const device = await this.deviceService.getDevice()
     const userDb = new UserDatabase('shared', 'shared', device.key, device._id, true)
-    
+
     this.syncMediaServiceStartTime = new Date().toISOString()
     // this.replicationStatus = new ReplicationStatus()
 
@@ -69,43 +69,68 @@ export class SyncMediaService {
     });
 
     for (var i = 0; i < mediaDirEntries.length; i++) {
-      this.lastEntry = (i+1) === mediaDirEntries.length
+      this.lastEntry = (i + 1) === mediaDirEntries.length
       const entry = mediaDirEntries[i]
       const fileName = entry.name + '.webm'
       console.log("processing file: " + fileName)
-      this.statusMessage =  _TRANSLATE("Uploading file: ") + fileName + "; " + (i+1) + _TRANSLATE(" of ") + mediaDirEntries.length + _TRANSLATE(" to upload")
+      this.statusMessage = _TRANSLATE("Uploading file: ") + fileName + "; " + (i + 1) + _TRANSLATE(" of ") + mediaDirEntries.length + _TRANSLATE(" to upload")
 
       const dirEntry = await new Promise(resolve =>
         this.window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory + this.mediaFilesDir, resolve)
       );
       const fileEntry = await new Promise(resolve => {
-        (dirEntry as DirectoryEntry).getFile(entry.name, {create: true, exclusive: false}, resolve);
+          (dirEntry as DirectoryEntry).getFile(entry.name, {create: true, exclusive: false}, resolve);
         }
       );
-      const file:File = await new Promise(resolve => {
+      const file: File = await new Promise(resolve => {
         (fileEntry as FileEntry).file(resolve)
       });
 
       const md5 = await this.computeChecksumMd5(file)
       console.log("md5: " + md5)
-      
-      const statusMessage = await this.uploadFile(file, appConfig)
-      console.log("statusMessage: " + statusMessage)
+
+      let statusMessage
+      try {
+        statusMessage = await this.uploadFile(file, appConfig, md5)
+        await new Promise(resolve => {
+          (fileEntry as FileEntry).remove(resolve)
+        });
+      } catch (e) {
+        this.statusMessage = _TRANSLATE("ERROR: Upload failed") + " : " + e.error
+        console.log(e)
+      }
+      console.log("statusMessage: " + this.statusMessage)
     }
   }
-  
-  async uploadFile(file: File, appConfig:AppConfig): Promise<any> {
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+
+  async uploadFile(file: File, appConfig: AppConfig, md5: string): Promise<any> {
     return new Promise((resolve, reject) => {
+      let deleteFile = true
       let reader = this.getFileReader();
-      reader.onloadend = (e) => {
+      reader.onloadend = async (e) => {
         // Create a blob based on the FileReader "result", which we asked to be retrieved as an ArrayBuffer
         const blob = new Blob([new Uint8Array(<ArrayBuffer>e.target.result)], {type: "video/webm"});
         const fileName = file.name + '.webm'
         const formData = new FormData();
         formData.append('video', blob, fileName);
+        formData.append('md5', md5);
         // /app/:group/media-upload
         const url = `${appConfig.serverUrl}app/${appConfig.groupId}/client-media-upload`
-        const upload$ = this.http.post(url, formData, {
+        const upload$ = await this.http.post(url, formData, {
           headers: new HttpHeaders({
             'Authorization': appConfig.uploadToken
           }),
@@ -113,7 +138,15 @@ export class SyncMediaService {
           observe: 'events'
         })
           .pipe(
-            finalize(() => this.reset())
+            // catchError(this.handleError()), // then handle the error
+            finalize(() => {
+              if (deleteFile) {
+                // this.deleteFile(file)
+                console.log("deleting file: " + file.name)
+                resolve(this.statusMessage)
+              }
+              this.reset()
+            })
           );
 
         this.uploadSub = upload$.subscribe(event => {
@@ -121,15 +154,28 @@ export class SyncMediaService {
             const progress = Math.round(100 * (event.loaded / event.total));
             if (this.lastEntry && progress === 100) {
               this.statusMessage = _TRANSLATE("Upload complete")
-              resolve(this.statusMessage)
             }
             this.uploadProgress = {
               progress: progress,
-              message:this.statusMessage
+              message: this.statusMessage
             }
             this.syncMessage$.next(this.uploadProgress)
           }
-        })
+        }, error => {
+          deleteFile = false
+          console.log('Error', error)
+          this.statusMessage = _TRANSLATE("ERROR: Upload failed")
+          this.uploadProgress = {
+            progress: null,
+            message: this.statusMessage
+          }
+          this.syncMessage$.next(this.uploadProgress)
+          reject(error)
+        }, () => {
+          console.log('Completed')
+          this.statusMessage = _TRANSLATE("Upload complete")
+          resolve(this.statusMessage)
+        });
       };
       // Read the file as an ArrayBuffer
       reader.readAsArrayBuffer(file);
@@ -148,7 +194,7 @@ export class SyncMediaService {
 
       let cursor = 0; // current cursor in file
 
-      fileReader.onerror = function(): void {
+      fileReader.onerror = function (): void {
         reject('MD5 computation failed - error reading the file');
       };
 
@@ -162,7 +208,7 @@ export class SyncMediaService {
       // If using TS >= 3.6, you can use `FileReaderProgressEvent` type instead 
       // of `any` for `e` variable, otherwise stick with `any`
       // See https://github.com/Microsoft/TypeScript/issues/25510
-      fileReader.onload = function(e: any): void {
+      fileReader.onload = function (e: any): void {
         spark.append(e.target.result); // Accumulate chunk to md5 computation
         cursor += chunkSize; // Move past this chunk
 
@@ -173,7 +219,8 @@ export class SyncMediaService {
           // Computation ended, last chunk has been processed. Return as Promise value.
           // This returns the base64 encoded md5 hash, which is what
           // Rails ActiveStorage or cloud services expect
-          resolve(btoa(spark.end(true)));
+          // resolve(btoa(spark.end(true)));
+          resolve(spark.end());
 
           // If you prefer the hexdigest form (looking like
           // '7cf530335b8547945f1a48880bc421b2'), replace the above line with:
--- server/src/routes/group-client-upload.js
+++ server/src/routes/group-client-upload.js
@@ -1,11 +1,7 @@
-const DB = require('../db.js')
-const clog = require('tangy-log').clog
-const log = require('tangy-log').log
 const util = require('util')
-const readDir = util.promisify(require('fs').readdir)
-const stat = util.promisify(require('fs').stat)
 const access = util.promisify(require('fs').access)
 const exec = util.promisify(require('child_process').exec)
+const md5File = require('md5-file')
 
 module.exports = async (req, res) => {
   try {
@@ -13,9 +9,19 @@ module.exports = async (req, res) => {
   } catch (e) {
     await exec(`mkdir /tangerine/client/content/groups/${req.params.group}/client-uploads`)
   }
-  console.log("Uploads? Anybody???", req.files)
+  const uploadedMd5 = req.body['md5']
   for (let file of req.files) {
-    console.log("File: ", file)
+    const fileMd5 = await md5File(file.path)
+    const fileMd5Base64 = Buffer.from(fileMd5, 'binary').toString('base64')
+    // console.log("uploadedMd5: " + uploadedMd5 + " fileMd5Base64: " + fileMd5Base64 + " file.path: " + file.path)
+    // console.log("uploadedMd5: " + uploadedMd5 + " fileMd5: " + fileMd5 + " file.path: " + file.path)
+    if (fileMd5 !== uploadedMd5) {
+    // if (fileMd5 !== fileMd5Base64) {
+      console.error('md5 mismatch')
+      res.statusMessage = 'File upload error: Invalid MD5';
+      res.status(400).send('Invalid MD5')
+      return
+    }
     await exec(`mv ${file.path} /tangerine/client/content/groups/${req.params.group}/client-uploads/${file.originalname.replace(/(\s+)/g, '\\$1')}`)
   }
   res.send()
