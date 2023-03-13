b8c5858cbd93ebc9f49fd07ad57451224c95d681
Chris E. Kelley <ckelley@rti.org>
2023-03-13 14:48:39 -0500

    Refactored Sync Settings to mat-table from mat-list

--- editor/src/app/groups/group-forms-sync/group-forms-sync.component.ts
+++ editor/src/app/groups/group-forms-sync/group-forms-sync.component.ts
@@ -10,7 +10,23 @@ import { TangerineFormsService } from '../services/tangerine-forms.service';
 import { _TRANSLATE } from 'src/app/shared/_services/translation-marker';
 import { TangyErrorHandler } from 'src/app/shared/_services/tangy-error-handler.service';
 import { ServerConfigService } from 'src/app/shared/_services/server-config.service';
+import {
+  CouchdbSyncSettings,
+  CustomSyncSettings,
+  FormSearchSettings
+} from "../../tangy-forms/classes/form-info.class";
 
+export interface FormSyncSetting {
+  searchSettings: FormSearchSettings;
+  src: string;
+  customSyncSettings: CustomSyncSettings;
+  couchdbSyncSettings: CouchdbSyncSettings;
+  printUrl: string;
+  id: string;
+  title: string;
+  type: string;
+  archived: boolean;
+}
 
 @Component({
   selector: 'app-group-forms-sync',
@@ -20,7 +36,8 @@ import { ServerConfigService } from 'src/app/shared/_services/server-config.serv
 export class GroupFormsSyncComponent implements OnInit, AfterViewInit {
   title = _TRANSLATE("Sync Settings")
   breadcrumbs:Array<Breadcrumb> = []
-  forms;
+  // forms: Array<FormSyncSetting>;
+  forms
   groupId;
   group;
   groupLabel;
@@ -66,6 +83,25 @@ export class GroupFormsSyncComponent implements OnInit, AfterViewInit {
     }
   }
 
+  columns = [
+    {
+      columnDef: 'id',
+      header: 'Form ID',
+      cell: (form: FormSyncSetting) => `${form.id}`,
+    },
+    {
+      columnDef: 'title',
+      header: 'Name',
+      cell: (form: FormSyncSetting) => `${form.title}`,
+    },
+    {
+      columnDef: 'pull',
+      header: '2-way Sync?',
+      cell: (form: FormSyncSetting) => `${form.couchdbSyncSettings.pull}`,
+    }
+  ];
+  dataSource;
+  displayedColumns = this.columns.map(c => c.columnDef);
   async ngAfterViewInit() {
     // This is needed to ensure angular binds to selected Tab. The settimeout does the trick
     const config = await this.serverConfig.getServerConfig()
@@ -73,14 +109,15 @@ export class GroupFormsSyncComponent implements OnInit, AfterViewInit {
   }
 
   async getForms() {
-    this.forms = (await this.tangerineForms.getFormsInfo(this.groupId)).map(formInfo => ({
+    const formInfo = <Array<TangerineFormInfo>> await this.tangerineForms.getFormsInfo(this.groupId)
+    this.forms = formInfo.map(formInfo => ({
       ...formInfo,
       printUrl: `${this.windowRef.nativeWindow.location.origin}${this.windowRef.nativeWindow.location.pathname}/#/tangy-form-editor/${this.groupId}/${formInfo.id}/print`
-    }));;
+    }));
     this.activeForms = this.forms.filter(form => !form.archived);
     this.archivedForms = this.forms.filter(form => form.archived);
   }
-
+  
   async toggleTwoWaySyncOnForm(groupId, formId) {
     const forms = await this.tangerineForms.getFormsInfo(groupId);
     const updatedForms = <Array<TangerineFormInfo>>forms.map(form => {
