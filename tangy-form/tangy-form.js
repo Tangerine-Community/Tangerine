3dcd28ce291143eec73e4ed2fc9e3118408e2f71
Chris E. Kelley <ckelley@rti.org>
2023-04-18 18:14:55 -0500

    Created tangy-ocr custom element to OCR scan forms. Added tesseract worker and wasm to angular scripts array/ Added 'start-using-proxy' npm run target when developing behind a proxy like tunnelto or nginx. Updated client dev section in the README Added support for uppy styles in tangy-form to support the tangy-ocr element.

--- tangy-form/tangy-form.js
+++ tangy-form/tangy-form.js
@@ -230,6 +230,7 @@ export class TangyForm extends PolymerElement {
         paper-progress {
           width: 100%;
         }
+        
         :host(:not([has-summary])) #bar {
           display:none; 
         }
