37652a36d6f3e03fbc2d2d20ef44b4507d4b4a17
esurface <esurface@rti.org>
2023-11-01 18:32:37 -0400

    CSV and MySQL-JS modules: parse data from multiple locaiton lists

--- server/src/modules/index.js
+++ server/src/modules/index.js
@@ -1,3 +1,8 @@
+const path = require('path')
+const fs = require('fs')
+const util = require('util')
+const readDir = util.promisify(require('fs').readdir)
+
 class TangyModules {
 
   constructor() {
@@ -45,6 +50,57 @@ class TangyModules {
     } else {
       flatFormResponse[key] = value
     }
+  }
+
+  async getLocationLists(groupId) {
+    const fileMetadata = []
+
+    function getFileMetadata(fullPath, filePath) {    
+      const fileContents = fs.readFileSync(fullPath, "utf8")
+      const fileData = JSON.parse(fileContents)
+
+      // if it has locationsLevels and locations, it is probably a location list file
+      if (!!fileData.locationsLevels && !!fileData.locations) {
+        const locationName = fileData.name ? fileData.name : path.parse(filePath).name
+        const fileId = fileData.id ? fileData.id : locationName
+        return {
+          id: fileId,
+          name: locationName,
+          path: filePath,
+          ...fileData
+        }
+      }
+    
+    }
+
+    const groupDir = `/tangerine/client/content/groups/${groupId}`
+    const locationListFilePath = 'location-list.json'
+    const locationListFullPath = path.join(groupDir, locationListFilePath)
+    if (fs.existsSync(locationListFullPath)) {
+      const metadata = getFileMetadata(locationListFullPath, locationListFilePath)
+      if (metadata) {
+        fileMetadata.push(metadata)
+      }
+    }
+
+    const locationsDir = path.join(groupDir, 'locations')
+    if (fs.existsSync(locationsDir)) {
+      const list = await readDir(locationsDir, {withFileTypes: true})
+      if (list && list.length > 0) {
+        for (let dirent of list) {
+          const fileName = dirent.name
+          if (path.extname(fileName) == ".json") {
+            const fullPath = path.join(locationsDir, fileName)
+            const filePath = `locations/${fileName}`
+            const metadata = getFileMetadata(fullPath, filePath)
+            if (metadata) {
+              fileMetadata.push(metadata)
+            }
+          }
+        }
+      }
+    }
+    return fileMetadata
   }
 
 }
