3b7465a136bec9b94d0c5b0cf07655910ee8cb3e
Chris E. Kelley <ckelley@rti.org>
2023-11-15 22:43:31 -0800

    Fixed dashboard reports
    Fixed absence display in student detail.
    Updated Luxon.

--- client/src/app/class/attendance/student-details/student-details.component.ts
+++ client/src/app/class/attendance/student-details/student-details.component.ts
@@ -7,7 +7,8 @@ import {MatBottomSheetRef} from "@angular/material/bottom-sheet";
 import {MAT_BOTTOM_SHEET_DATA} from '@angular/material/bottom-sheet';
 import {AppConfigService} from "../../../shared/_services/app-config.service";
 import {DashboardService} from "../../_services/dashboard.service";
-import {DateTime} from "luxon";
+import {DateTime, Interval} from "luxon";
+import {KeyValue} from "@angular/common";
 // import {
 //   CalendarEvent,
 //   CalendarEventAction,
@@ -76,6 +77,7 @@ export class StudentDetailsComponent implements OnInit {
   // refresh = new Subject<void>();
 
   // events: CalendarEvent[] = [];
+  absences: {}
   locale: string = 'es_GT';
   
   async ngOnInit(): Promise<void> {
@@ -97,6 +99,55 @@ export class StudentDetailsComponent implements OnInit {
 
     const records = []
     const events = []
+    const absences = {}
+    
+    // setup calendar
+    // const dt = DateTime.now();
+    // const month = dt.month;
+    // for (let i = 0; i < 6; i++) {
+    //   const monthDate = dt.set({month: month - i, day: 1})
+    //   // events.push({
+    //   //   month: monthDate, 
+    //   // })
+    //   const monthinFutureDate = dt.set({month: (month - i) + 1, day: 1})
+    //   const monthNumber = monthDate.month
+    //   const yearNumber = monthDate.year
+    //   const monthInterval = Interval.fromDateTimes(monthDate, monthinFutureDate);
+    //   const opts = {useLocaleWeeks: true}
+    //   const monthDays = monthInterval.count('days')
+    //  
+    //   if (!absences[yearNumber]) {
+    //     absences[yearNumber] = {}
+    //   }
+    //   if (!absences[yearNumber][monthNumber]) {
+    //     absences[yearNumber][monthNumber] = {
+    //       monthName: monthDate.toLocaleString({month: 'long'}),
+    //       days:[]
+    //     }
+    //   }
+    //   let currentWeekNumber = 0
+    //   let dayOfWeek = 0
+    //   let isEndOfWeek = false
+    //   for (let j = 0; j < (monthDays - 1); j++) {
+    //     const dayNumber = j+1
+    //     const currentDate = dt.set({month: month - i, day: dayNumber})
+    //     const endOfWeek = currentDate.endOf('week')
+    //     isEndOfWeek = currentDate.day === endOfWeek.day;
+    //     const weekNumber = currentDate.weekNumber
+    //     let newWeek = false
+    //     if (currentWeekNumber !== weekNumber) {
+    //       newWeek = true
+    //       currentWeekNumber = weekNumber
+    //     }
+    //     absences[yearNumber][monthNumber]['days'][dayNumber] = {
+    //       weekNumber: weekNumber,
+    //       newWeek: newWeek,
+    //       isEndOfWeek: isEndOfWeek,
+    //       absenceList: []
+    //     }
+    //   }
+    // }
+    
     const attendanceReports = await this.dashboardService.searchDocs('attendance', this.data.currentClass, null, curriculumLabel, randomId, true)
     for (let i = 0; i < attendanceReports.length; i++) {
       const attendanceReport = attendanceReports[i].doc
@@ -107,26 +158,47 @@ export class StudentDetailsComponent implements OnInit {
       const reportLocaltime = timestampFormatted.toLocaleString(DateTime.DATE_FULL)
       const attendanceList = attendanceReport.attendanceList
       // To get events into the calendar display
-      // for (let j = 0; j < attendanceList.length; j++) {
-      //   const attendance = attendanceList[j]
-      //   if (attendance.id === studentId) {
-      //     if (attendance.absent === true) {
-      //       attendance.reportLocaltime = reportLocaltime
-      //       records.push(attendance)
-      //       events.push({
-      //         start: startOfDay(timestampDate), 
-      //         title: _TRANSLATE('Absent'),
-      //         color: { ...colors.red },
-      //         allDay: true,
-      //       })
-      //     }
-      //   }
-      // }
-      // this.events = events
+      for (let j = 0; j < attendanceList.length; j++) {
+        const attendance = attendanceList[j]
+        if (attendance.id === studentId) {
+          if (attendance.absent === true) {
+            attendance.reportLocaltime = reportLocaltime
+            records.push(attendance)
+
+            // const dayNumber = timestampFormatted.day
+            // const monthNumber = timestampFormatted.month
+            // const yearNumber = timestampFormatted.year
+            //
+            // const absence = {
+            //   month: timestampDate,
+            //   monthString: reportLocaltime
+            // }
+            // if (absences[yearNumber] && absences[yearNumber][monthNumber]['days'] && absences[yearNumber][monthNumber]['days'][dayNumber]) {
+            //   absences[yearNumber][monthNumber]['days'][dayNumber]['absenceList'].push(absence)
+            // }
+            
+            // var diffInMonths = end.diff(start, 'months');
+            
+            // events.push({
+            //   start: startOfDay(timestampDate), 
+            //   title: _TRANSLATE('Absent'),
+            //   color: { ...colors.red },
+            //   allDay: true,
+            // })
+            
+            
+          }
+        }
+      }
+      // this.absences = absences
     }
     
     this.absentRecords = records
     
+  }
+
+  private onCompare(_left: KeyValue<any, any>, _right: KeyValue<any, any>): number {
+    return 1;
   }
 
   openLink(event: MouseEvent): void {
