867510e640fa3f7a6dbcec3b227965dd4d2391f8
esurface <esurface@rti.org>
2023-12-19 15:34:50 -0500

    Add Custom Date Range selection in Attendance report

---
+++ client/src/app/shared/_services/tangy-date-adapter.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { NativeDateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
+import { Platform, PlatformModule } from '@angular/cdk/platform';
+import { LanguagesService } from './languages.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TangyDateAdapterService extends NativeDateAdapter {
+
+  constructor(
+    private languagesService: LanguagesService
+  ) {
+    super('en-US', new Platform);
+
+    this.languagesService.getCurrentLanguageLocale().then(locale => {
+      this.setLocale(locale);
+    });
+  }
+
+  getFirstDayOfWeek(): number {
+    if (this.locale != 'en') {
+      return 1; // Monday
+    }
+    return 0; // Sunday
+ }
+}
