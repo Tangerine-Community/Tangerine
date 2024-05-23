b803b25c2d125c15c8b8f585d75a57067fc1b8cf
Evans Dianga <evans@itsevans.com>
2024-05-23 17:42:29 +0300

    build(import-profile): Add views and remove redundant retry button
    Refs Tangerine-Community/Tangerine#3696

--- server/src/routes/group-responses-by-user-profile-short-code.js
+++ server/src/routes/group-responses-by-user-profile-short-code.js
@@ -5,24 +5,31 @@ const log = require('tangy-log').log
 module.exports = async (req, res) => {
   try {
     const groupDb = new DB(req.params.groupId)
-    let options = {key: req.params.userProfileShortCode, include_docs: true}
+    const userProfileShortCode = req.params.userProfileShortCode
+    let options = { key: userProfileShortCode, include_docs: true }
     if (req.params.limit) {
       options.limit = req.params.limit
     }
     if (req.params.skip) {
       options.skip = req.params.skip
     }
-    if(req.query.totalRows){
+    if (req.query.totalRows) {
       options.limit = 1
       options.skip = 0
       const results = await groupDb.query('responsesByUserProfileShortCode', options);
-      res.send({totalRows:results.total_rows})
-    }else{
+      res.send({ totalRows: results.total_rows })
+    } else if (req.query.userProfile) {
+     
+      await groupDb.query("userProfileByUserProfileShortCode", { limit: 0 });
+      const profile = await groupDb.query("userProfileByUserProfileShortCode", { key: userProfileShortCode, limit: 1, include_docs: true });
+      res.send(profile.rows[0])
+    } else {
       const results = await groupDb.query('responsesByUserProfileShortCode', options);
       const docs = results.rows.map(row => row.doc)
       res.send(docs)
     }
   } catch (error) {
+    console.log(error)
     log.error(error);
     res.status(500).send(error);
   }
