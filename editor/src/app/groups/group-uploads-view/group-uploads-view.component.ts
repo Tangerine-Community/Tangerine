c062d8f59a5e30b32c7f078494ecf44e68ecbbb6
Evans Dianga <evans@itsevans.com>
2024-06-05 13:35:12 +0300

    feat(group-uploads-view): Lock form responses when in uploads view
    - Lock form responses when in uploads view
    - Allow users to delete response when viewing upload
    - Add groups edit component
    -
    
    Refs Tangerine-Community/Tangerine#3703

--- editor/src/app/groups/group-uploads-view/group-uploads-view.component.ts
+++ editor/src/app/groups/group-uploads-view/group-uploads-view.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Breadcrumb } from './../../shared/_components/breadcrumb/breadcrumb.component';
 import { _TRANSLATE } from 'src/app/shared/_services/translation-marker';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-group-uploads-view',
@@ -14,14 +15,19 @@ export class GroupUploadsViewComponent implements OnInit {
   title = _TRANSLATE('View Upload')
   breadcrumbs:Array<Breadcrumb> = []
   @ViewChild('formPlayer', {static: true}) formPlayer: TangyFormsPlayerComponent
+  responseId
+  groupId
  
   constructor(
     private route:ActivatedRoute,
-    private router:Router
+    private router:Router,
+    private http: HttpClient,
   ) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
+      this.responseId = params.responseId
+      this.groupId = params.groupId
       this.breadcrumbs = [
         <Breadcrumb>{
           label: _TRANSLATE('Uploads'),
@@ -29,17 +35,27 @@ export class GroupUploadsViewComponent implements OnInit {
         },
         <Breadcrumb>{
           label: this.title,
-          url: `uploads/view/${params.responseId}` 
+          url: `uploads/${params.responseId}`
         }
       ]
       this.formPlayer.formResponseId = params.responseId
-      this.formPlayer.unlockFormResponses = true
       this.formPlayer.render()
       this.formPlayer.$submit.subscribe(async () => {
         this.formPlayer.saveResponse(this.formPlayer.formEl.store.getState())
         this.router.navigate([`../`], { relativeTo: this.route })
       })
     })
+  }
+
+  async delete(){
+    if(confirm('Are you sure you want to delete this form response?')) {
+      await this.http.delete(`/api/${this.groupId}/${this.responseId}`).toPromise()
+      this.router.navigate([`../`], { relativeTo: this.route })
+    }
+  }
+
+  async verify(){
+
   }
 
 }
