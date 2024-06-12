dd37629099e2f02b21eec4e591eabfe0df1724f9
esurface <esurface@rti.org>
2024-06-12 13:29:49 -0400

    Revert addition of all client/app-config.json; only set helpLink

--- server/src/scripts/release-online-survey-app.sh
+++ server/src/scripts/release-online-survey-app.sh
@@ -41,7 +41,6 @@ MEDIA_DIRECTORY="$FORM_CLIENT_DIRECTORY/media"
 CUSTOM_SCRIPTS="$FORM_CLIENT_DIRECTORY/custom-scripts.js"
 CUSTOM_SCRIPTS_MAP="$FORM_CLIENT_DIRECTORY/custom-scripts.js.map"
 CUSTOM_LOGIN_MARKUP="$FORM_CLIENT_DIRECTORY/custom-login-markup.html"
-APP_CONFIG="$FORM_CLIENT_DIRECTORY/app-config.json"
 
 # Set up the release dir from the dist
 cp -r /tangerine/online-survey-app/dist/online-survey-app/ $RELEASE_DIRECTORY
@@ -95,25 +94,18 @@ else
   REQUIRE_ACCESS_CODE="false"
 fi
 
-# Copy the app-config.json to the release directory
-cp $APP_CONFIG $RELEASE_DIRECTORY/assets/
+# NOTE: App Config does NOT come from the app-config.json file in the release directory
+sed -i -e "s#GROUP_ID#"$GROUP_ID"#g" $RELEASE_DIRECTORY/assets/app-config.json
+sed -i -e "s#FORM_UPLOAD_URL#"$FORM_UPLOAD_URL"#g" $RELEASE_DIRECTORY/assets/app-config.json
+sed -i -e "s#UPLOAD_KEY#"$UPLOAD_KEY"#g" $RELEASE_DIRECTORY/assets/app-config.json
+sed -i -e "s#APP_NAME#"$APP_NAME"#g" $RELEASE_DIRECTORY/assets/app-config.json
+sed -i -e "s#REQUIRE_ACCESS_CODE#"$REQUIRE_ACCESS_CODE"#g" $RELEASE_DIRECTORY/assets/app-config.json
 
-# first delete the last line of the file with the closing bracket
-sed -i '$ d' $RELEASE_DIRECTORY/assets/app-config.json
-
-#append a comma to the end of the last line
-sed -i '$ s/$/,/' $RELEASE_DIRECTORY/assets/app-config.json
-
-#then append the online survey configs
-echo "\"formUploadURL\": \"$FORM_UPLOAD_URL\"," >> $RELEASE_DIRECTORY/assets/app-config.json
-echo "\"uploadKey\": \"$UPLOAD_KEY\"," >> $RELEASE_DIRECTORY/assets/app-config.json
-echo "\"groupId\": \"$GROUP_ID\"," >> $RELEASE_DIRECTORY/assets/app-config.json
-echo "\"languageDirection\": \"ltr\"," >> $RELEASE_DIRECTORY/assets/app-config.json
-echo "\"languageCode\": \"en\"," >> $RELEASE_DIRECTORY/assets/app-config.json
-echo "\"appName\": \"$APP_NAME\"," >> $RELEASE_DIRECTORY/assets/app-config.json
-echo "\"requireAccessCode\": $REQUIRE_ACCESS_CODE" >> $RELEASE_DIRECTORY/assets/app-config.json
-
-# finaly, add the closing bracket
-echo "}" >> $RELEASE_DIRECTORY/assets/app-config.json
+# copy the HELP_LINK from client/app-config.json to assets/app-config.json
+HELP_LINK=$(jq -r '.helpLink' $FORM_CLIENT_DIRECTORY/app-config.json)
+if [ "$HELP_LINK" = "null" ]; then
+  HELP_LINK=""
+fi
+jq '.helpLink = "'$HELP_LINK'"' $RELEASE_DIRECTORY/assets/app-config.json > tmp.$$.json && mv tmp.$$.json $RELEASE_DIRECTORY/assets/app-config.json
 
 echo "Release with UUID of $UUID to $RELEASE_DIRECTORY with Build ID of $BUILD_ID, channel of $RELEASE_TYPE"
