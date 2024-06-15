88071e57e1f8e4f6c112ae369f378e340754270f
esurface <esurface@rti.org>
2024-06-15 15:40:16 -0400

    Add process monitor dialogs to publish/unpublish online survey

--- editor/src/app/groups/release-online-survey/release-online-survey.component.ts
+++ editor/src/app/groups/release-online-survey/release-online-survey.component.ts
@@ -5,6 +5,9 @@ import { TangyErrorHandler } from 'src/app/shared/_services/tangy-error-handler.
 import { _TRANSLATE } from 'src/app/shared/_services/translation-marker';
 import { GroupsService } from '../services/groups.service';
 import { TangerineFormsService } from '../services/tangerine-forms.service';
+import { ProcessMonitorService } from 'src/app/shared/_services/process-monitor.service';
+import { ProcessMonitorDialogComponent } from 'src/app/shared/_components/process-monitor-dialog/process-monitor-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-release-online-survey',
@@ -19,10 +22,15 @@ export class ReleaseOnlineSurveyComponent implements OnInit {
   group;
   publishedSurveys;
   unPublishedSurveys;
+  dialogRef:any
+
   constructor(private route: ActivatedRoute,
     private groupService: GroupsService,
     private errorHandler: TangyErrorHandler,
-    private tangyFormService: TangerineFormsService) { }
+    private tangyFormService: TangerineFormsService,
+    private processMonitorService: ProcessMonitorService,
+    private dialog: MatDialog,
+  ) { }
 
   async ngOnInit() {
     this.groupId = this.route.snapshot.paramMap.get('groupId');
@@ -32,6 +40,20 @@ export class ReleaseOnlineSurveyComponent implements OnInit {
         url: 'onlineSurvey'
       }
     ];
+    this.processMonitorService.change.subscribe((isDone) => {
+      if (this.processMonitorService.processes.length === 0) {
+        this.dialog.closeAll()
+      } else {
+        this.dialog.closeAll()
+        this.dialogRef = this.dialog.open(ProcessMonitorDialogComponent, {
+          data: {
+            messages: this.processMonitorService.processes.map(process => process.description).reverse()
+          },
+          disableClose: true
+        })
+      }
+    })
+
     await this.getForms();
   }
 
@@ -47,6 +69,8 @@ export class ReleaseOnlineSurveyComponent implements OnInit {
     this.unPublishedSurveys = surveyData.filter(e => !e.published);
   }
   async publishSurvey(formId, appName, locked) {
+    const process = this.processMonitorService.start('publishSurvey', 'Publishing Survey');
+
     try {
       await this.groupService.publishSurvey(this.groupId, formId, 'prod', appName, locked);
       await this.getForms();
@@ -54,16 +78,22 @@ export class ReleaseOnlineSurveyComponent implements OnInit {
     } catch (error) {
       console.error(error);
       this.errorHandler.handleError(_TRANSLATE('Could Not Contact Server.'));
+    } finally {
+      this.processMonitorService.stop(process.id);
     }
   }
   async unPublishSurvey(formId) {
+    const process = this.processMonitorService.start('unpublishSurvey', 'Un-publishing Survey');
+
     try {
       await this.groupService.unPublishSurvey(this.groupId, formId);
       await this.getForms();
-      this.errorHandler.handleError(_TRANSLATE('Survey UnPublished Successfully.'));
+      this.errorHandler.handleError(_TRANSLATE('Survey Un-published Successfully.'));
     } catch (error) {
       console.error(error);
       this.errorHandler.handleError(_TRANSLATE('Could Not Contact Server.'));
+    } finally {
+      this.processMonitorService.stop(process.id);
     }
   }
 
