d8fa95db5df6ff33bba5ac0e06b2048b8b4368f6
esurface <esurface@rti.org>
2024-06-15 14:05:26 -0400

    Add timeout and logout to online survey

--- online-survey-app/src/app/app.component.ts
+++ online-survey-app/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppConfigService } from './shared/_services/app-config.service';
-import { CaseService } from './case/services/case.service';
+import { _TRANSLATE } from './shared/_services/translation-marker';
+import { AuthenticationService } from './core/auth/_services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -12,17 +14,34 @@ export class AppComponent implements OnInit{
   appName: string;
   hasHelpLink: boolean = false;
   helpLink: string;
-  window: any;
+  sessionTimeoutCheckTimerID;
+  isConfirmDialogActive = false;
+  loggedIn = false;
 
-  constructor(private appConfigService: AppConfigService, private caseService: CaseService){
+  constructor(
+    private appConfigService: AppConfigService, 
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ){
   }
 
   async ngOnInit(): Promise<any>{
+    this.authenticationService.currentUserLoggedIn$.subscribe(async isLoggedIn => {
+      if (isLoggedIn) {
+        this.loggedIn = isLoggedIn;
+        this.sessionTimeoutCheck();
+        this.sessionTimeoutCheckTimerID =
+        setInterval(await this.sessionTimeoutCheck.bind(this), 10 * 60 * 1000); // check every 10 minutes
+      } else {
+        this.loggedIn = false;
+        this.router.navigate(['/survey-login']);
+      }
+    });
+
     try {
       const appConfig = await this.appConfigService.getAppConfig();
       this.appName = appConfig.appName;
       this.languageDirection = appConfig.languageDirection;
-
       if (appConfig.helpLink) {
         this.hasHelpLink = true;
         this.helpLink = appConfig.helpLink;
@@ -31,6 +50,31 @@ export class AppComponent implements OnInit{
     } catch (error) {
       this.appName = '';
       this.languageDirection = 'ltr';
+      this.helpLink = '';
     }
+  }
+  
+  async sessionTimeoutCheck() {
+    const token = localStorage.getItem('token');
+    const claims = JSON.parse(atob(token.split('.')[1]));
+    const expiryTimeInMs = claims['exp'] * 1000;
+    const minutesBeforeExpiry = expiryTimeInMs - (15 * 60 * 1000); // warn 15 minutes before expiry of token
+    if (Date.now() >= minutesBeforeExpiry && !this.isConfirmDialogActive) {
+      this.isConfirmDialogActive = true;
+      const extendSession = confirm(_TRANSLATE('You are about to be logged out. Should we extend your session?'));
+      if (extendSession) {
+        await this.authenticationService.extendUserSession();
+        this.isConfirmDialogActive = false;
+      } else {
+        await this.logout();
+      }
+    }
+  }
+
+  async logout() {
+    clearInterval(this.sessionTimeoutCheckTimerID);
+    await this.authenticationService.logout();
+    this.loggedIn = false;
+    this.router.navigate(['/survey-login']);
   }
 }
