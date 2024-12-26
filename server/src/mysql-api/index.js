64bd60cc27520f93be6de92e79e25bd71016a22e
Erik Surface <esurface@rti.org>
2024-12-26 14:08:02 -0500

    Add group permission can_access_mysql_api to control access to mysql api

--- server/src/mysql-api/index.js
+++ server/src/mysql-api/index.js
@@ -2,18 +2,6 @@ var express = require('express');
 var router = express.Router();
 const dataGenerator = require('./data-generator.js')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send(`
-    <h2>API Index:</h2>
-    <div>
-    <div>
-      <p><a href="/get-table?groupId=groupc95a711880df4875a46646e4b40af512&formId=form_ee17ac29_a21b_4ef8_a62f_af0f314d2c6c">Get Table Test</a></p>
-      <p><a href="/get-view?groupId=groupf4881f8ad4964a939d3eb903cd0d63bf&viewId=attendance_view">Get View Test</a></p>
-    </div>
-  `);
-});
-
 router.get('/get-table', async function(req, res) {
   try {
     if (req.query.groupId && req.query.formId) {
