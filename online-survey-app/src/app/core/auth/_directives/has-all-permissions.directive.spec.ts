0cd27c1b42fbaa2c2551007cb6a8a8d6f4b8f4cd
Erik Surface <esurface@rti.org>
2025-01-10 15:24:06 -0500

    Remove auth directives from online-survey-app

--- online-survey-app/src/app/core/auth/_directives/has-all-permissions.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HasAllPermissionsDirective } from './has-all-permissions.directive';
-
-describe('HasAllPermissionsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HasAllPermissionsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
