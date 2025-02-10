5112ac6e27e336934e62232cec2e3603c1dfa570
Erik Surface <esurface@rti.org>
2025-02-10 11:57:32 -0400

    Add shared csv template parameter to generate csv data sets script

--- server/src/scripts/generate-csv-data-sets/bin.js
+++ server/src/scripts/generate-csv-data-sets/bin.js
@@ -4,19 +4,21 @@ const generateCsvDataSets = require('./generate-csv-data-sets.js')
 
 if (process.argv[2] === '--help') {
   console.log('Usage:')
-  console.log('  generate-csv-data-sets <filename>')
+  console.log('  generate-csv-data-sets <filename> [sharedCsvTemplateId]')
   console.log('Examples:')
   console.log(`  generate-csv-data-sets 2022-01-03`)
+  console.log(`  generate-csv-data-sets 2022-01-03 shared-csv-template-id`)
   process.exit()
 }
 
 const params = {
-  filename: process.argv[2]
+  filename: process.argv[2],
+  sharedCsvTemplateId: process.argv[3] || undefined
 }
 
 async function go(params) {
   try {
-    await generateCsvDataSets(params.filename) 
+    await generateCsvDataSets(params.filename, params.sharedCsvTemplateId)
     process.exit()
   } catch (error) {
     console.error(error)
