d4872eb64df403c2eb2c98d34fa89a32666fe13a
Erik Surface <esurface@rti.org>
2025-02-12 12:32:34 -0400

    Updates and fixes

--- editor/src/app/pipes/format-form-title-from-info.pipe.ts
+++ editor/src/app/pipes/format-form-title-from-info.pipe.ts
@@ -2,31 +2,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'formTitleFromInfo',
 })
+/*
+ * Angular Pipe to format form title
+ * If the title doesnt contain t-lang tag, return the text.
+ * Otherwise, use the English translation if it exists or the first translation if there's no English version of the title, followed by the form Id
+ */
 export class FormTitleFromInfoPipe implements PipeTransform {
   transform(formInfo) {
-    if(formInfo.id){
-        if(!formInfo.title){
-            return formInfo.id
-          }
-          if(!formInfo.title.includes('t-lang')) return formInfo.title;
-          if(formInfo.title.includes('t-lang')) {
-            const titleDomString = new DOMParser().parseFromString(formInfo.title, 'text/html')
-            const formTitleEnglish = titleDomString.querySelector('t-lang[en]')
-            formInfo.title = formTitleEnglish ? formTitleEnglish.textContent : titleDomString.querySelector('t-lang').textContent
-            return formInfo.title
-          }
-    }
-    if(formInfo.formId){
-        if(!formInfo.formTitle){
-            return formInfo.formId
-          }
-          if(!formInfo.formTitle.includes('t-lang')) return formInfo.formTitle;
-          if(formInfo.formTitle.includes('t-lang')) {
-            const titleDomString = new DOMParser().parseFromString(formInfo.formTitle, 'text/html')
-            const formTitleEnglish = titleDomString.querySelector('t-lang[en]')
-            formInfo.formTitle = formTitleEnglish ? formTitleEnglish.textContent : titleDomString.querySelector('t-lang').textContent
-            return formInfo.formTitle
-          }
+    const formId = formInfo.id || formInfo.formId
+    if (formId) {
+      if (!formInfo.title) return formId
+      if (!formInfo.title.includes('t-lang')) return formInfo.title;
+      if (formInfo.title.includes('t-lang')) {
+        const titleDomString = new DOMParser().parseFromString(formInfo.title, 'text/html')
+        const formTitleEnglish = titleDomString.querySelector('t-lang[en]')
+        formInfo.title = formTitleEnglish ? formTitleEnglish.textContent : titleDomString.querySelector('t-lang').textContent
+        return formInfo.title
+      }
     }
   }
 }
