29e6264eafb653c502b27f1f47b0c647199d1f99
Erik Surface <esurface@rti.org>
2025-02-12 12:02:35 -0400

    Fix missing references in html and move comments out of csv/views.js

--- server/src/modules/csv/views.js
+++ server/src/modules/csv/views.js
@@ -1,3 +1,5 @@
+// getMonth returns 0-based index i.e 0,1,2,3,4,5,6,7,8,9,10,11
+//The emitted value is in the form "formId" i.e `formId` and also "formId_2018_0" for Jan i.e `formId_Year_Month` since Javascript uses 0-based indexing for months
 module.exports = {
   _id: '_design/tangy-reporting',
   version: '1',
@@ -6,8 +8,7 @@ module.exports = {
       map: function (doc) {
         if (doc.formId) {
           const startUnixtime = new Date(doc.startUnixtime);
-          const key = doc.formId + '_' + startUnixtime.getFullYear() + '_' + startUnixtime.getMonth();// getMonth returns 0-based index i.e 0,1,2,3,4,5,6,7,8,9,10,11
-          //The emitted value is in the form "formId" i.e `formId` and also "formId_2018_0" for Jan i.e `formId_Year_Month` Remember Javascript uses 0-based indexing for months
+          const key = doc.formId + '_' + startUnixtime.getFullYear() + '_' + startUnixtime.getMonth(); 
           emit(doc.formId);
           emit(key);
         }
