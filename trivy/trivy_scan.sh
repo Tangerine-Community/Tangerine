70fef8b8721e23be99df5df0d99466bf28467f78
ajc5 <3946534+ajc5@users.noreply.github.com>
2026-06-22 16:49:15 +0200

    Fixed critical CVEs

--- trivy/trivy_scan.sh
+++ trivy/trivy_scan.sh
@@ -1,6 +1,7 @@
 TIMEOUT="20m"
 
-trivy image --scanners vuln tangerine-server --timeout $TIMEOUT --format template --template "@contrib/html.tpl" -o ../data/trivy-reports/tangerine-server.html
-trivy image --scanners vuln tangerine-server-ui --timeout $TIMEOUT --format template --template "@contrib/html.tpl" -o ../data/trivy-reports/tangerine-server-ui.html
-trivy image --scanners vuln tangerine-couchdb --timeout $TIMEOUT --format template --template "@contrib/html.tpl" -o ../data/trivy-reports/tangerine-couchdb.html
-trivy image --scanners vuln tangerine-apk-generator --timeout $TIMEOUT --format template --template "@contrib/html.tpl" -o ../data/trivy-reports/tangerine-apk-generator.html
+trivy image --scanners vuln tangerine/server:latest --timeout $TIMEOUT --format template --template "@contrib/html.tpl" -o ../data/trivy-reports/tangerine-server.html
+trivy image --scanners vuln tangerine/server-ui:latest --timeout $TIMEOUT --format template --template "@contrib/html.tpl" -o ../data/trivy-reports/tangerine-server-ui.html
+trivy image --scanners vuln tangerine-couchdb:latest --timeout $TIMEOUT --format template --template "@contrib/html.tpl" -o ../data/trivy-reports/tangerine-couchdb.html
+trivy image --scanners vuln tangerine/apk-generator:latest --timeout $TIMEOUT --format template --template "@contrib/html.tpl" -o ../data/trivy-reports/tangerine-apk-generator.html
+trivy image --scanners vuln nginx:1.27-alpine --timeout $TIMEOUT --format template --template "@contrib/html.tpl" -o ../data/trivy-reports/nginx.html
